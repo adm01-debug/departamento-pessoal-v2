1e8ffd0e2d2d26fcb91ebc3b8018e5e9fc8690d8
adm01-debug <adm01@promobrindes.com.br>
2025-12-22 20:30:37 -0300

    feat: Criar barrel export services

---
+++ src/services/index.ts
@@ -0,0 +1,7 @@
+export { colaboradoresService } from './colaboradoresService';
+export { feriasService } from './feriasService';
+export { folhaService } from './folhaService';
+export { pontoService } from './pontoService';
+export { auditoriaService } from './auditoriaService';
+export { esocialService } from './esocialService';
+export { notificacoesService } from './notificacoesService';
