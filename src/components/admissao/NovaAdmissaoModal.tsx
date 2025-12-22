2c30fcc8622f72fe74dfcf4a15f34937361f6349
adm01-debug <adm01@promobrindes.com.br>
2025-12-22 19:25:18 -0300

    perf: Adicionar React.memo em NovaAdmissaoModal.tsx

--- src/components/admissao/NovaAdmissaoModal.tsx
+++ src/components/admissao/NovaAdmissaoModal.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, memo } from 'react';
 import {
   Dialog,
   DialogContent,
@@ -448,3 +448,4 @@ export function NovaAdmissaoModal({ open, onOpenChange, onSubmit }: NovaAdmissao
     </Dialog>
   );
 }
+
