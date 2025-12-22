15cc138ab13aa26bd85f420655991db828d2cbbe
adm01-debug <adm01@promobrindes.com.br>
2025-12-22 20:30:48 -0300

    feat: Criar barrel export lib

---
+++ src/lib/index.ts
@@ -0,0 +1,7 @@
+export * from './utils';
+export * from './formatters';
+export * from './masks';
+export * from './validators';
+export * from './calculosTrabalhistas';
+export * from './constants';
+export * from './logger';
