f96f606a0b7709021ac2e5a843344266bca12c76
adm01-debug <adm01@promobrindes.com.br>
2025-12-23 10:57:10 -0300

    feat: Barrel export form components

---
+++ src/components/form/index.ts
@@ -0,0 +1,6 @@
+export { FormField } from './FormField';
+export { CpfInput } from './CpfInput';
+export { CnpjInput } from './CnpjInput';
+export { MoneyInput } from './MoneyInput';
+export { TelefoneInput } from './TelefoneInput';
+export { CepInput } from './CepInput';
