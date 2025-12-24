eefb753c7cb7ef0e28c20b49bb181c8b97d2804f
adm01-debug <adm01@promobrindes.com.br>
2025-12-24 15:26:21 -0300

    fix(dropdown): rebuild DropdownMenu [325/450]

--- src/components/dropdown/DropdownMenu.tsx
+++ src/components/dropdown/DropdownMenu.tsx
@@ -1,1 +1,6 @@
-import { memo } from 'react';
+import { memo } from "react";
+import { DropdownMenu as DM } from "@/components/ui/dropdown-menu";
+interface DropdownMenuProps { children: React.ReactNode; }
+export const DropdownMenu = memo(function DropdownMenu({ children }: DropdownMenuProps) {
+  return <DM>{children}</DM>;
+});
