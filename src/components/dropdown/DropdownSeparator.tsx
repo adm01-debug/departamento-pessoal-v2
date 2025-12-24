a12421fe725f0f316aa72e8f639b0cad8fa471f3
adm01-debug <adm01@promobrindes.com.br>
2025-12-24 15:26:28 -0300

    fix(dropdown): rebuild DropdownSeparator [326/450]

--- src/components/dropdown/DropdownSeparator.tsx
+++ src/components/dropdown/DropdownSeparator.tsx
@@ -1,1 +1,5 @@
-import { memo } from 'react';
+import { memo } from "react";
+import { DropdownMenuSeparator } from "@/components/ui/dropdown-menu";
+export const DropdownSeparator = memo(function DropdownSeparator() {
+  return <DropdownMenuSeparator />;
+});
