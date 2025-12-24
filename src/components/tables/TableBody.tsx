e0257aa17f18c4af260ef108904e2bab4aade492
adm01-debug <adm01@promobrindes.com.br>
2025-12-24 15:37:25 -0300

    fix(tables): rebuild TableBody [352/450]

--- src/components/tables/TableBody.tsx
+++ src/components/tables/TableBody.tsx
@@ -1,3 +1,6 @@
-import { memo } from 'react';
-
-
+import { memo } from "react";
+import { TableBody as TB } from "@/components/ui/table";
+interface TableBodyProps { children: React.ReactNode; }
+export const TableBody = memo(function TableBody({ children }: TableBodyProps) {
+  return <TB>{children}</TB>;
+});
