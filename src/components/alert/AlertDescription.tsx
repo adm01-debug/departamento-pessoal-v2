29b3699098a3c95285f1d7f3185bbce0e4f06407
adm01-debug <adm01@promobrindes.com.br>
2025-12-24 15:23:20 -0300

    fix(alert): rebuild AlertDescription [302/450]

--- src/components/alert/AlertDescription.tsx
+++ src/components/alert/AlertDescription.tsx
@@ -1,1 +1,6 @@
-import { memo } from 'react';
+import { memo } from "react";
+import { AlertDescription as AD } from "@/components/ui/alert";
+interface AlertDescriptionProps { children: React.ReactNode; }
+export const AlertDescription = memo(function AlertDescription({ children }: AlertDescriptionProps) {
+  return <AD>{children}</AD>;
+});
