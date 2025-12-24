763745dc65a2b9e82651269294426fe9c1b9a243
adm01-debug <adm01@promobrindes.com.br>
2025-12-24 15:37:40 -0300

    fix(tables): rebuild TableHead [354/450]

--- src/components/tables/TableHead.tsx
+++ src/components/tables/TableHead.tsx
@@ -1,3 +1,6 @@
-import { memo } from 'react';
-
-
+import { memo } from "react";
+import { TableHead as TH } from "@/components/ui/table";
+interface TableHeadProps { children: React.ReactNode; className?: string; }
+export const TableHead = memo(function TableHead({ children, className }: TableHeadProps) {
+  return <TH className={className}>{children}</TH>;
+});
