ab5305b8a1edf4833dbde000e3259036728a83c7
adm01-debug <adm01@promobrindes.com.br>
2025-12-24 15:38:02 -0300

    fix(tooltip): rebuild TooltipProvider [357/450]

--- src/components/tooltip/TooltipProvider.tsx
+++ src/components/tooltip/TooltipProvider.tsx
@@ -1,1 +1,6 @@
-import { memo } from 'react';
+import { memo } from "react";
+import { TooltipProvider as TP, Tooltip } from "@/components/ui/tooltip";
+interface TooltipProviderProps { children: React.ReactNode; }
+export const TooltipProvider = memo(function TooltipProvider({ children }: TooltipProviderProps) {
+  return <TP><Tooltip>{children}</Tooltip></TP>;
+});
