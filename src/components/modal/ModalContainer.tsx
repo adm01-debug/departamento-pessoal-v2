8795d95b9fe708b3cb1ab3c767dd24cec94f6e3c
adm01-debug <adm01@promobrindes.com.br>
2025-12-24 15:36:16 -0300

    fix(modal): rebuild ModalContainer [345/450]

--- src/components/modal/ModalContainer.tsx
+++ src/components/modal/ModalContainer.tsx
@@ -1,1 +1,6 @@
-import { memo } from 'react';
+import { memo } from "react";
+import { DialogContent } from "@/components/ui/dialog";
+interface ModalContainerProps { children: React.ReactNode; className?: string; }
+export const ModalContainer = memo(function ModalContainer({ children, className }: ModalContainerProps) {
+  return <DialogContent className={className}>{children}</DialogContent>;
+});
