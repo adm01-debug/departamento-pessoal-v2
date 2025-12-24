3ab1f021bd373ac016f680e6f8206c2fe1ac7f99
adm01-debug <adm01@promobrindes.com.br>
2025-12-24 16:04:11 -0300

    fix(tables): rebuild TableBody [260/450]

--- src/components/tables/TableBody.tsx
+++ src/components/tables/TableBody.tsx
@@ -1,6 +1,31 @@
-import { memo } from "react";
-import { TableBody as TB } from "@/components/ui/table";
-interface TableBodyProps { children: React.ReactNode; }
-export const TableBody = memo(function TableBody({ children }: TableBodyProps) {
-  return <TB>{children}</TB>;
+/**
+ * @fileoverview Corpo da tabela
+ * @module components/tables/TableBody
+ */
+import { memo, type ReactNode } from 'react';
+import { cn } from '@/lib/utils';
+import { TableBody as UITableBody } from '@/components/ui/table';
+
+/** Props do TableBody */
+interface TableBodyProps {
+  /** Linhas da tabela */
+  children: ReactNode;
+  /** Classes CSS adicionais */
+  className?: string;
+}
+
+/**
+ * Corpo da tabela para exibir dados
+ * @param props - Propriedades do componente
+ * @returns Elemento JSX
+ */
+export const TableBody = memo(function TableBody({
+  children,
+  className,
+}: TableBodyProps) {
+  return (
+    <UITableBody className={cn('[&_tr:last-child]:border-0', className)}>
+      {children}
+    </UITableBody>
+  );
 });
