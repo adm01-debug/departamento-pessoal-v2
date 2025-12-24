461ae0a5c1eb4e6cacf6c6596ebb3ffa1628d39c
adm01-debug <adm01@promobrindes.com.br>
2025-12-24 15:26:13 -0300

    fix(dropdown): rebuild DropdownItem [324/450]

--- src/components/dropdown/DropdownItem.tsx
+++ src/components/dropdown/DropdownItem.tsx
@@ -1,1 +1,6 @@
-import { memo } from 'react';
+import { memo } from "react";
+import { DropdownMenuItem } from "@/components/ui/dropdown-menu";
+interface DropdownItemProps { children: React.ReactNode; onClick?: () => void; disabled?: boolean; }
+export const DropdownItem = memo(function DropdownItem({ children, onClick, disabled }: DropdownItemProps) {
+  return <DropdownMenuItem onClick={onClick} disabled={disabled}>{children}</DropdownMenuItem>;
+});
