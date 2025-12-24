0c1c964a06778fc6b82d9ac9202d1a8d5d455550
adm01-debug <adm01@promobrindes.com.br>
2025-12-24 15:36:01 -0300

    fix(modal): rebuild Modal [343/450]

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,1 +1,6 @@
-import { memo } from 'react';
+import { memo } from "react";
+import { Dialog, DialogContent } from "@/components/ui/dialog";
+interface ModalProps { open: boolean; onOpenChange: (o: boolean) => void; children: React.ReactNode; }
+export const Modal = memo(function Modal({ open, onOpenChange, children }: ModalProps) {
+  return <Dialog open={open} onOpenChange={onOpenChange}><DialogContent>{children}</DialogContent></Dialog>;
+});
