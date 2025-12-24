a3eea80c03e8f4e07eabe2962bb3e25680a25ce8
adm01-debug <adm01@promobrindes.com.br>
2025-12-24 15:25:30 -0300

    fix(card): rebuild CardDescription [318/450]

--- src/components/card/CardDescription.tsx
+++ src/components/card/CardDescription.tsx
@@ -1,1 +1,6 @@
-import { memo } from 'react';
+import { memo } from "react";
+import { CardDescription as CD } from "@/components/ui/card";
+interface CardDescriptionProps { children: React.ReactNode; }
+export const CardDescription = memo(function CardDescription({ children }: CardDescriptionProps) {
+  return <CD>{children}</CD>;
+});
