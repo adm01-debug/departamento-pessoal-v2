90ab31e0bf199cf7af845cc6f5903d4ea5fe9bca
adm01-debug <adm01@promobrindes.com.br>
2025-12-24 15:38:09 -0300

    fix(tooltip): rebuild TooltipTrigger [358/450]

--- src/components/tooltip/TooltipTrigger.tsx
+++ src/components/tooltip/TooltipTrigger.tsx
@@ -1,1 +1,6 @@
-import { memo } from 'react';
+import { memo } from "react";
+import { TooltipTrigger as TT } from "@/components/ui/tooltip";
+interface TooltipTriggerProps { children: React.ReactNode; asChild?: boolean; }
+export const TooltipTrigger = memo(function TooltipTrigger({ children, asChild = true }: TooltipTriggerProps) {
+  return <TT asChild={asChild}>{children}</TT>;
+});
