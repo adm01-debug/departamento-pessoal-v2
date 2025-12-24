99b6095c3521df6bdc6bc5dba7bad2d9a6933c9f
adm01-debug <adm01@promobrindes.com.br>
2025-12-24 15:19:58 -0300

    fix(timeline): rebuild TimelineList [289/450]

--- src/components/timeline/TimelineList.tsx
+++ src/components/timeline/TimelineList.tsx
@@ -1,1 +1,5 @@
-import { memo } from 'react';
+import { memo } from "react";
+interface TimelineListProps { children: React.ReactNode; }
+export const TimelineList = memo(function TimelineList({ children }: TimelineListProps) {
+  return <div className="space-y-0">{children}</div>;
+});
