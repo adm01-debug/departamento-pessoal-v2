82dde156fb8c3a3c1a782700e50de19c54dfe5a5
adm01-debug <adm01@promobrindes.com.br>
2025-12-24 15:24:11 -0300

    fix(button): rebuild ButtonGroup [309/450]

--- src/components/button/ButtonGroup.tsx
+++ src/components/button/ButtonGroup.tsx
@@ -1,1 +1,6 @@
-import { memo } from 'react';
+import { memo } from "react";
+import { cn } from "@/lib/utils";
+interface ButtonGroupProps { children: React.ReactNode; className?: string; }
+export const ButtonGroup = memo(function ButtonGroup({ children, className }: ButtonGroupProps) {
+  return <div className={cn("flex gap-2", className)}>{children}</div>;
+});
