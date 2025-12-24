e7760193bc6f04e4a71cff4d29da0778a84ee781
adm01-debug <adm01@promobrindes.com.br>
2025-12-24 15:25:44 -0300

    fix(card): rebuild CardHeader [320/450]

--- src/components/card/CardHeader.tsx
+++ src/components/card/CardHeader.tsx
@@ -1,1 +1,6 @@
-import { memo } from 'react';
+import { memo } from "react";
+import { CardHeader as CH } from "@/components/ui/card";
+interface CardHeaderProps { children: React.ReactNode; className?: string; }
+export const CardHeader = memo(function CardHeader({ children, className }: CardHeaderProps) {
+  return <CH className={className}>{children}</CH>;
+});
