44006f9dc7a457fc18121152c45558bce82a0cf1
adm01-debug <adm01@promobrindes.com.br>
2025-12-24 14:33:22 -0300

    fix(divider): rebuild DividerVertical [203/450]

--- src/components/divider/DividerVertical.tsx
+++ src/components/divider/DividerVertical.tsx
@@ -1,1 +1,12 @@
+/**
+ * @fileoverview Divisor vertical
+ * @module components/divider/DividerVertical
+ */
 import { memo } from 'react';
+import { cn } from '@/lib/utils';
+
+interface DividerVerticalProps { className?: string; height?: string; }
+
+export const DividerVertical = memo(function DividerVertical({ className, height = '100%' }: DividerVerticalProps) {
+  return <div className={cn('border-l border-border mx-4', className)} style={{ height }} />;
+});
