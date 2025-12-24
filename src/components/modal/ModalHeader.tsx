1f45bf90e617414c655710d2bde2e8ec75708d6b
adm01-debug <adm01@promobrindes.com.br>
2025-12-24 15:36:32 -0300

    fix(modal): rebuild ModalHeader [347/450]

--- src/components/modal/ModalHeader.tsx
+++ src/components/modal/ModalHeader.tsx
@@ -1,2 +1,6 @@
-import { memo } from 'react';
-import { memo } from 'react';
+import { memo } from "react";
+import { DialogHeader, DialogTitle } from "@/components/ui/dialog";
+interface ModalHeaderProps { title: string; }
+export const ModalHeader = memo(function ModalHeader({ title }: ModalHeaderProps) {
+  return <DialogHeader><DialogTitle>{title}</DialogTitle></DialogHeader>;
+});
