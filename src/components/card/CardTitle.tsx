0d1f91cba8d5ab998e2a55071f89558e2927fa8a
adm01-debug <adm01@promobrindes.com.br>
2025-12-24 15:25:58 -0300

    fix(card): rebuild CardTitle [322/450]

--- src/components/card/CardTitle.tsx
+++ src/components/card/CardTitle.tsx
@@ -1,1 +1,6 @@
-import { memo } from 'react';
+import { memo } from "react";
+import { CardTitle as CT } from "@/components/ui/card";
+interface CardTitleProps { children: React.ReactNode; }
+export const CardTitle = memo(function CardTitle({ children }: CardTitleProps) {
+  return <CT>{children}</CT>;
+});
