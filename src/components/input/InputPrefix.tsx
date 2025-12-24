6cae5c812802b2613b345df9eaff507e74f6e912
adm01-debug <adm01@promobrindes.com.br>
2025-12-24 15:33:50 -0300

    fix(input): rebuild InputPrefix [328/450]

--- src/components/input/InputPrefix.tsx
+++ src/components/input/InputPrefix.tsx
@@ -1,1 +1,5 @@
-import { memo } from 'react';
+import { memo } from "react";
+interface InputPrefixProps { children: React.ReactNode; }
+export const InputPrefix = memo(function InputPrefix({ children }: InputPrefixProps) {
+  return <span className="absolute left-3 top-1/2 -translate-y-1/2 text-muted-foreground">{children}</span>;
+});
