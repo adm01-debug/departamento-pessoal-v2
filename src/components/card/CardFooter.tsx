9bea52dbd8237ee2470834383ebc987ac9c8a036
adm01-debug <adm01@promobrindes.com.br>
2025-12-24 15:25:37 -0300

    fix(card): rebuild CardFooter [319/450]

--- src/components/card/CardFooter.tsx
+++ src/components/card/CardFooter.tsx
@@ -1,1 +1,6 @@
-import { memo } from 'react';
+import { memo } from "react";
+import { CardFooter as CF } from "@/components/ui/card";
+interface CardFooterProps { children: React.ReactNode; className?: string; }
+export const CardFooter = memo(function CardFooter({ children, className }: CardFooterProps) {
+  return <CF className={className}>{children}</CF>;
+});
