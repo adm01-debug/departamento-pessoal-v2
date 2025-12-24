03b227e35e11bfdc6f283e00d18cf5eb6ea7652e
adm01-debug <adm01@promobrindes.com.br>
2025-12-24 15:36:42 -0300

    fix(modal): rebuild ModalOverlay [348/450]

--- src/components/modal/ModalOverlay.tsx
+++ src/components/modal/ModalOverlay.tsx
@@ -1,1 +1,5 @@
-import { memo } from 'react';
+import { memo } from "react";
+interface ModalOverlayProps { onClick?: () => void; }
+export const ModalOverlay = memo(function ModalOverlay({ onClick }: ModalOverlayProps) {
+  return <div className="fixed inset-0 bg-black/50 z-40" onClick={onClick} />;
+});
