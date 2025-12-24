da6b2ecbe55904e8a07e65dac76f1a2faef6d7f0
adm01-debug <adm01@promobrindes.com.br>
2025-12-24 15:34:24 -0300

    fix(layout): rebuild Container [331/450]

--- src/components/layout/Container.tsx
+++ src/components/layout/Container.tsx
@@ -1,4 +1,6 @@
-import { memo, useMemo } from 'react';
-
-
-
+import { memo } from "react";
+import { cn } from "@/lib/utils";
+interface ContainerProps { children: React.ReactNode; className?: string; }
+export const Container = memo(function Container({ children, className }: ContainerProps) {
+  return <div className={cn("container mx-auto px-4", className)}>{children}</div>;
+});
