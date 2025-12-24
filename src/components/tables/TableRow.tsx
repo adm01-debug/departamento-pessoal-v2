72c6dfc2fa5d6a7db0ad62e491a1ec90ce5a2f64
adm01-debug <adm01@promobrindes.com.br>
2025-12-24 15:37:48 -0300

    fix(tables): rebuild TableRow [355/450]

--- src/components/tables/TableRow.tsx
+++ src/components/tables/TableRow.tsx
@@ -1,1 +1,6 @@
-import { memo } from 'react';
+import { memo } from "react";
+import { TableRow as TR } from "@/components/ui/table";
+interface TableRowProps { children: React.ReactNode; className?: string; }
+export const TableRow = memo(function TableRow({ children, className }: TableRowProps) {
+  return <TR className={className}>{children}</TR>;
+});
