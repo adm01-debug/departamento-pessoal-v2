7c851da62b6f28d24013b2e71cebdb05fcddd553
adm01-debug <adm01@promobrindes.com.br>
2025-12-24 15:36:25 -0300

    fix(modal): rebuild ModalFooter [346/450]

--- src/components/modal/ModalFooter.tsx
+++ src/components/modal/ModalFooter.tsx
@@ -1,2 +1,6 @@
-import { memo } from 'react';
-import { memo } from 'react';
+import { memo } from "react";
+import { DialogFooter } from "@/components/ui/dialog";
+interface ModalFooterProps { children: React.ReactNode; }
+export const ModalFooter = memo(function ModalFooter({ children }: ModalFooterProps) {
+  return <DialogFooter>{children}</DialogFooter>;
+});
