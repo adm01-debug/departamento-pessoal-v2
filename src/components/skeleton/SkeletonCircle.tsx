5b647ad05e69b1b5b62c145248b4dd21cfb0fb9c
adm01-debug <adm01@promobrindes.com.br>
2025-12-24 15:17:56 -0300

    fix(skeleton): rebuild SkeletonCircle [274/450]

--- src/components/skeleton/SkeletonCircle.tsx
+++ src/components/skeleton/SkeletonCircle.tsx
@@ -1,1 +1,6 @@
-import { memo } from 'react';
+import { memo } from "react";
+import { Skeleton } from "@/components/ui/skeleton";
+interface SkeletonCircleProps { size?: number; }
+export const SkeletonCircle = memo(function SkeletonCircle({ size = 40 }: SkeletonCircleProps) {
+  return <Skeleton className="rounded-full" style={{ width: size, height: size }} />;
+});
