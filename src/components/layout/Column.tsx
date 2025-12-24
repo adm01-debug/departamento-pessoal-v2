78feb214e0d1b037b18284aebd85d335d5a98934
adm01-debug <adm01@promobrindes.com.br>
2025-12-24 15:34:17 -0300

    fix(layout): rebuild Column [330/450]

--- src/components/layout/Column.tsx
+++ src/components/layout/Column.tsx
@@ -1,4 +1,6 @@
-import { memo, useMemo } from 'react';
-
-
-
+import { memo } from "react";
+import { cn } from "@/lib/utils";
+interface ColumnProps { children: React.ReactNode; span?: number; className?: string; }
+export const Column = memo(function Column({ children, span = 1, className }: ColumnProps) {
+  return <div className={cn(className)} style={{ gridColumn: `span ${span}` }}>{children}</div>;
+});
