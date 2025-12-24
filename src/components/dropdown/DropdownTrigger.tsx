fda6ce78dca9147904e4aadb7245bb8a48fd96f6
adm01-debug <adm01@promobrindes.com.br>
2025-12-24 15:26:35 -0300

    fix(dropdown): rebuild DropdownTrigger [327/450]

--- src/components/dropdown/DropdownTrigger.tsx
+++ src/components/dropdown/DropdownTrigger.tsx
@@ -1,1 +1,6 @@
-import { memo } from 'react';
+import { memo } from "react";
+import { DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+interface DropdownTriggerProps { children: React.ReactNode; asChild?: boolean; }
+export const DropdownTrigger = memo(function DropdownTrigger({ children, asChild }: DropdownTriggerProps) {
+  return <DropdownMenuTrigger asChild={asChild}>{children}</DropdownMenuTrigger>;
+});
