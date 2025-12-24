311bb8c58d35407e4846c1bea065364d2d265741
adm01-debug <adm01@promobrindes.com.br>
2025-12-24 15:26:05 -0300

    fix(dropdown): rebuild DropdownContent [323/450]

--- src/components/dropdown/DropdownContent.tsx
+++ src/components/dropdown/DropdownContent.tsx
@@ -1,1 +1,6 @@
-import { memo } from 'react';
+import { memo } from "react";
+import { DropdownMenuContent } from "@/components/ui/dropdown-menu";
+interface DropdownContentProps { children: React.ReactNode; align?: "start" | "center" | "end"; }
+export const DropdownContent = memo(function DropdownContent({ children, align = "end" }: DropdownContentProps) {
+  return <DropdownMenuContent align={align}>{children}</DropdownMenuContent>;
+});
