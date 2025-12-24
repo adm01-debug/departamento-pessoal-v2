7ebe0f60ba709ddfbedca72c1bd632956601bb45
adm01-debug <adm01@promobrindes.com.br>
2025-12-24 15:37:55 -0300

    fix(tooltip): rebuild TooltipContent [356/450]

--- src/components/tooltip/TooltipContent.tsx
+++ src/components/tooltip/TooltipContent.tsx
@@ -1,1 +1,6 @@
-import { memo } from 'react';
+import { memo } from "react";
+import { TooltipContent as TC } from "@/components/ui/tooltip";
+interface TooltipContentProps { children: React.ReactNode; side?: "top" | "right" | "bottom" | "left"; }
+export const TooltipContent = memo(function TooltipContent({ children, side = "top" }: TooltipContentProps) {
+  return <TC side={side}>{children}</TC>;
+});
