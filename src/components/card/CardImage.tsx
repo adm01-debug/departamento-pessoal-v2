8b4ada2cfa6c374abc36c798cb7af2f14bf0518d
adm01-debug <adm01@promobrindes.com.br>
2025-12-24 15:25:51 -0300

    fix(card): rebuild CardImage [321/450]

--- src/components/card/CardImage.tsx
+++ src/components/card/CardImage.tsx
@@ -1,1 +1,5 @@
-import { memo } from 'react';
+import { memo } from "react";
+interface CardImageProps { src: string; alt: string; className?: string; }
+export const CardImage = memo(function CardImage({ src, alt, className }: CardImageProps) {
+  return <img src={src} alt={alt} className={`w-full object-cover ${className || ""}`} />;
+});
