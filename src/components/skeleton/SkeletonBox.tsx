b48192445eb01ccf12e85f711d251c356a67110a
adm01-debug <adm01@promobrindes.com.br>
2025-12-24 15:17:42 -0300

    fix(skeleton): rebuild SkeletonBox [272/450]

--- src/components/skeleton/SkeletonBox.tsx
+++ src/components/skeleton/SkeletonBox.tsx
@@ -1,1 +1,7 @@
-import { memo } from 'react';
+import { memo } from "react";
+import { Skeleton } from "@/components/ui/skeleton";
+import { cn } from "@/lib/utils";
+interface SkeletonBoxProps { width?: string; height?: string; className?: string; }
+export const SkeletonBox = memo(function SkeletonBox({ width = "100%", height = "100px", className }: SkeletonBoxProps) {
+  return <Skeleton className={cn("rounded-lg", className)} style={{ width, height }} />;
+});
