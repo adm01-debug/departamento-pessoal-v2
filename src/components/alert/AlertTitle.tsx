6e1fc0ba882fa9167264330040a7f1cf27095d65
adm01-debug <adm01@promobrindes.com.br>
2025-12-24 15:23:36 -0300

    fix(alert): rebuild AlertTitle [304/450]

--- src/components/alert/AlertTitle.tsx
+++ src/components/alert/AlertTitle.tsx
@@ -1,1 +1,6 @@
-import { memo } from 'react';
+import { memo } from "react";
+import { AlertTitle as AT } from "@/components/ui/alert";
+interface AlertTitleProps { children: React.ReactNode; }
+export const AlertTitle = memo(function AlertTitle({ children }: AlertTitleProps) {
+  return <AT>{children}</AT>;
+});
