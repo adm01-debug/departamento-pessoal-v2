53f20d21dd9fccf94fbd75f3f4320fcc06b9ee2a
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 20:55:34 -0300

    test: AlertTitle

---
+++ src/components/alert/__tests__/AlertTitle.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { AlertTitle } from '../AlertTitle';
+describe('AlertTitle', () => { it('defined', () => { expect(AlertTitle).toBeDefined(); }); });
