1460c77bc8994f8111a8f341f973d733b6fed055
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:12:49 -0300

    test: add ESocialEventoForm test [253/3211]

---
+++ src/components/esocial/__tests__/ESocialEventoForm.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { ESocialEventoForm } from '../ESocialEventoForm';
+describe('ESocialEventoForm', () => { it('should be defined', () => { expect(ESocialEventoForm).toBeDefined(); }); });
