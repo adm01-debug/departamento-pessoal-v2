1dfee393ccce5609ceaa3b622125efdb1a8211b5
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:39:55 -0300

    test: VisuallyHidden

---
+++ src/components/common/__tests__/VisuallyHidden.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { VisuallyHidden } from '../VisuallyHidden';
+describe('VisuallyHidden', () => { it('defined', () => { expect(VisuallyHidden).toBeDefined(); }); });
