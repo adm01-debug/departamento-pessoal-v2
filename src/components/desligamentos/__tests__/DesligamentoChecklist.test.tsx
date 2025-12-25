49e9680d66744ad22e17663d4e8b4bf46f5f3a8b
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:22:43 -0300

    test: add DesligamentoChecklist [358/3211]

---
+++ src/components/desligamentos/__tests__/DesligamentoChecklist.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { DesligamentoChecklist } from '../DesligamentoChecklist';
+describe('DesligamentoChecklist', () => { it('should be defined', () => { expect(DesligamentoChecklist).toBeDefined(); }); });
