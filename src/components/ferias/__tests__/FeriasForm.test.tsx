2c4c7ccd68c986f79846c189391f7d7b4acb584e
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:11:55 -0300

    test: add FeriasForm test [231/3211]

---
+++ src/components/ferias/__tests__/FeriasForm.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { FeriasForm } from '../FeriasForm';
+describe('FeriasForm', () => { it('should be defined', () => { expect(FeriasForm).toBeDefined(); }); });
