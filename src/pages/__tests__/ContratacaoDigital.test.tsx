7d0610ec3c62618a561e19624e8e6b02b3620e15
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:42:05 -0300

    test: ContratacaoDigital page

---
+++ src/pages/__tests__/ContratacaoDigital.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import ContratacaoDigital from '../ContratacaoDigital';
+describe('ContratacaoDigital Page', () => { it('should be defined', () => { expect(ContratacaoDigital).toBeDefined(); }); });
