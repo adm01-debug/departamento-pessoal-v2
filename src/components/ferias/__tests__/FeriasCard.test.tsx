cd61c0f50e8050dec1f4fcf26b95d080344d297e
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:11:55 -0300

    test: add FeriasCard test [231/3211]

---
+++ src/components/ferias/__tests__/FeriasCard.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { FeriasCard } from '../FeriasCard';
+describe('FeriasCard', () => { it('should be defined', () => { expect(FeriasCard).toBeDefined(); }); });
