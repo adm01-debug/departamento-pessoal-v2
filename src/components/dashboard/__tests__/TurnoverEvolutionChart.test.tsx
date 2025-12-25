8af12582627d3e1dc65cfbb8195d4dc64c46b97c
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:11:08 -0300

    test: add TurnoverEvolutionChart test [222/3211]

---
+++ src/components/dashboard/__tests__/TurnoverEvolutionChart.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { TurnoverEvolutionChart } from '../TurnoverEvolutionChart';
+describe('TurnoverEvolutionChart', () => { it('should be defined', () => { expect(TurnoverEvolutionChart).toBeDefined(); }); });
