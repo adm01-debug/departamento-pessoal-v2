36578660f525319ab96229978aa273feb1bcf9f5
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:23:13 -0300

    test: add CargoCard [367/3211]

---
+++ src/components/cargos/__tests__/CargoCard.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { CargoCard } from '../CargoCard';
+describe('CargoCard', () => { it('should be defined', () => { expect(CargoCard).toBeDefined(); }); });
