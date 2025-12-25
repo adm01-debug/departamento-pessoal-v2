5ec1468894a3c348a3ed2edd47ebad3ce57219a2
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:39:47 -0300

    test: LoadingTable

---
+++ src/components/common/__tests__/LoadingTable.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { LoadingTable } from '../LoadingTable';
+describe('LoadingTable', () => { it('defined', () => { expect(LoadingTable).toBeDefined(); }); });
