db896f0ede0932741c298b8eddfe5aae622e7227
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:45:27 -0300

    test: AuditoriaList

---
+++ src/components/auditoria/__tests__/AuditoriaList.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { AuditoriaList } from '../AuditoriaList';
+describe('AuditoriaList', () => { it('defined', () => { expect(AuditoriaList).toBeDefined(); }); });
