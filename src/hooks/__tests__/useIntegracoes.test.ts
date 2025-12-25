892f51019bfccd3ffa8971c7f47799b3b768a354
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:40:08 -0300

    test: useIntegracoes

---
+++ src/hooks/__tests__/useIntegracoes.test.ts
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { useIntegracoes } from '../useIntegracoes';
+describe('useIntegracoes', () => { it('defined', () => { expect(useIntegracoes).toBeDefined(); }); });
