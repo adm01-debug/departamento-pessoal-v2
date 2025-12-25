4aeea903b53609e05d103de772f611b0c155ad7e
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:17:31 -0300

    test: add esocialEventosTrabalhadores test [311/3211]

---
+++ src/lib/__tests__/esocialEventosTrabalhadores.test.ts
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import * as esocialEventosTrabalhadores from '../esocialEventosTrabalhadores';
+describe('esocialEventosTrabalhadores', () => { it('should export events', () => { expect(esocialEventosTrabalhadores).toBeDefined(); }); });
