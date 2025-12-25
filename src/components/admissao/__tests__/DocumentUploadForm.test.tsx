58d048cc11b0118987779824a918629fdc1bf1b4
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:43:49 -0300

    test: DocumentUploadForm

---
+++ src/components/admissao/__tests__/DocumentUploadForm.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { DocumentUploadForm } from '../DocumentUploadForm';
+describe('DocumentUploadForm', () => { it('defined', () => { expect(DocumentUploadForm).toBeDefined(); }); });
