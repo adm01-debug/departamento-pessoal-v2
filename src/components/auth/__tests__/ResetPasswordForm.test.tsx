bd07113bd66872bab2ccac530ad3aefe31dda061
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:25:06 -0300

    test: ResetPasswordForm [381]

---
+++ src/components/auth/__tests__/ResetPasswordForm.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { ResetPasswordForm } from '../ResetPasswordForm';
+describe('ResetPasswordForm', () => { it('defined', () => { expect(ResetPasswordForm).toBeDefined(); }); });
