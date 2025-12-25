efccc418974032d3eaa482eb694e4156926c7204
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:33:47 -0300

    test: useScrollLock [462]

---
+++ src/hooks/__tests__/useScrollLock.test.ts
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { useScrollLock } from '../useScrollLock';
+describe('useScrollLock', () => { it('defined', () => { expect(useScrollLock).toBeDefined(); }); });
