44322a374b7c50ad21a05817195719fd51266e64
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:12:50 -0300

    test: add ESocialLote test [253/3211]

---
+++ src/components/esocial/__tests__/ESocialLote.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { ESocialLote } from '../ESocialLote';
+describe('ESocialLote', () => { it('should be defined', () => { expect(ESocialLote).toBeDefined(); }); });
