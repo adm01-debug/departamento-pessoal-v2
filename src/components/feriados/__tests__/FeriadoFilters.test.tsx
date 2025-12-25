a294330b055ee1d5a78eb39a8e051d2ab4d4428e
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:45:41 -0300

    test: FeriadoFilters

---
+++ src/components/feriados/__tests__/FeriadoFilters.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { FeriadoFilters } from '../FeriadoFilters';
+describe('FeriadoFilters', () => { it('defined', () => { expect(FeriadoFilters).toBeDefined(); }); });
