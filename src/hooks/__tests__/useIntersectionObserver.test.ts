62cfe7a944d46de05cfdefb2565fa7c9767b101f
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:22:00 -0300

    test: add useIntersectionObserver [340/3211]

---
+++ src/hooks/__tests__/useIntersectionObserver.test.ts
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { useIntersectionObserver } from '../useIntersectionObserver';
+describe('useIntersectionObserver', () => { it('should be defined', () => { expect(useIntersectionObserver).toBeDefined(); }); });
