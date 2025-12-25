8c214176bee4266b9b198fbc495f8c29d9b239d5
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 15:45:05 -0300

    feat(dashboard): create barrel export [126/3211]

---
+++ src/components/dashboard/index.ts
@@ -0,0 +1,30 @@
+/**
+ * @fileoverview Exportações de componentes de dashboard
+ * @module components/dashboard
+ */
+export { AbsenteeismChart } from './AbsenteeismChart';
+export { AdmissionsLineChart } from './AdmissionsLineChart';
+export { AlertHistoryModal } from './AlertHistoryModal';
+export { AlertsList } from './AlertsList';
+export { BirthdayCard } from './BirthdayCard';
+export { ComplianceCard } from './ComplianceCard';
+export { DashboardCustomizer } from './DashboardCustomizer';
+export { DepartmentBarChart } from './DepartmentBarChart';
+export { DepartmentChart } from './DepartmentChart';
+export { EmployeeStatusCard } from './EmployeeStatusCard';
+export { IndicatorAlertsCard } from './IndicatorAlertsCard';
+export { KPICard } from './KPICard';
+export { MetricsGrid } from './MetricsGrid';
+export { MiniCalendar } from './MiniCalendar';
+export { NotificationsPanel } from './NotificationsPanel';
+export { PayrollCostChart } from './PayrollCostChart';
+export { QuickActions } from './QuickActions';
+export { RecentActivities } from './RecentActivities';
+export { StatusPieChart } from './StatusPieChart';
+export { TasksPanel } from './TasksPanel';
+export { TurnoverDetailModal } from './TurnoverDetailModal';
+export { TurnoverEvolutionChart } from './TurnoverEvolutionChart';
+export { TurnoverGauge } from './TurnoverGauge';
+export { TurnoverYearComparisonChart } from './TurnoverYearComparisonChart';
+export { VacationCalendar } from './VacationCalendar';
+export { WelcomeCard } from './WelcomeCard';
