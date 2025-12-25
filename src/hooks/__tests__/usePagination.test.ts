3c254c660082dd98b01b3827f616fedc5d22ffe5
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:40:09 -0300

    test: usePagination

---
+++ src/hooks/__tests__/usePagination.test.ts
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { usePagination } from '../usePagination';
+describe('usePagination', () => { it('defined', () => { expect(usePagination).toBeDefined(); }); });
