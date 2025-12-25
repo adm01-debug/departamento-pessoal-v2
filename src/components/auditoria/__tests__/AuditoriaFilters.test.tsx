2bc04060cab4c45279efecb7625849251c331118
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:45:27 -0300

    test: AuditoriaFilters

---
+++ src/components/auditoria/__tests__/AuditoriaFilters.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { AuditoriaFilters } from '../AuditoriaFilters';
+describe('AuditoriaFilters', () => { it('defined', () => { expect(AuditoriaFilters).toBeDefined(); }); });
