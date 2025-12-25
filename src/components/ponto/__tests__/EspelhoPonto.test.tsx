20f556bd1f9e7c1b620934e7a2be11d71229c26c
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:12:40 -0300

    test: add EspelhoPonto test [250/3211]

---
+++ src/components/ponto/__tests__/EspelhoPonto.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { EspelhoPonto } from '../EspelhoPonto';
+describe('EspelhoPonto', () => { it('should be defined', () => { expect(EspelhoPonto).toBeDefined(); }); });
