f5cfe565c2339268fa026538b3e52db231b780f6
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:21:52 -0300

    test: add useFormPersist [335/3211]

---
+++ src/hooks/__tests__/useFormPersist.test.ts
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { useFormPersist } from '../useFormPersist';
+describe('useFormPersist', () => { it('should be defined', () => { expect(useFormPersist).toBeDefined(); }); });
