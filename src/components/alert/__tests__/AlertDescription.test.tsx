4ebb44d220b4c5519c4c4f27df7d3fb15abc489a
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 20:55:33 -0300

    test: AlertDescription

---
+++ src/components/alert/__tests__/AlertDescription.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { AlertDescription } from '../AlertDescription';
+describe('AlertDescription', () => { it('defined', () => { expect(AlertDescription).toBeDefined(); }); });
