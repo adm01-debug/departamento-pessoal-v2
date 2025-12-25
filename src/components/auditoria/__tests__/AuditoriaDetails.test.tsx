867888095623c7742d17a64f28a334055f07fb62
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:45:26 -0300

    test: AuditoriaDetails

---
+++ src/components/auditoria/__tests__/AuditoriaDetails.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { AuditoriaDetails } from '../AuditoriaDetails';
+describe('AuditoriaDetails', () => { it('defined', () => { expect(AuditoriaDetails).toBeDefined(); }); });
