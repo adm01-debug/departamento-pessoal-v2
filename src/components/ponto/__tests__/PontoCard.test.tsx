0f96553172d9b123cb2667aa6989b4904208a63f
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:12:39 -0300

    test: add PontoCard test [250/3211]

---
+++ src/components/ponto/__tests__/PontoCard.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { PontoCard } from '../PontoCard';
+describe('PontoCard', () => { it('should be defined', () => { expect(PontoCard).toBeDefined(); }); });
