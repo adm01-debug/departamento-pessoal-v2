01a69ef9392a3c5f5468dfe573b021b55f04bb14
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:40:07 -0300

    test: useFerias

---
+++ src/hooks/__tests__/useFerias.test.ts
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { useFerias } from '../useFerias';
+describe('useFerias', () => { it('defined', () => { expect(useFerias).toBeDefined(); }); });
