88c48ceb8b1c66f0391a053f2d4c388f21bdc3b7
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:31:40 -0300

    test: useTableSort [444]

---
+++ src/hooks/__tests__/useTableSort.test.ts
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { useTableSort } from '../useTableSort';
+describe('useTableSort', () => { it('defined', () => { expect(useTableSort).toBeDefined(); }); });
