9e604cc4802ad4915d62a55b0b5fe6ece35137b2
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 20:55:31 -0300

    test: AccordionTrigger

---
+++ src/components/accordion/__tests__/AccordionTrigger.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { AccordionTrigger } from '../AccordionTrigger';
+describe('AccordionTrigger', () => { it('defined', () => { expect(AccordionTrigger).toBeDefined(); }); });
