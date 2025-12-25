10164427c1ca597d05af1b9a5f78552e5314a7de
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:28:26 -0300

    test: useSelection [410]

---
+++ src/hooks/__tests__/useSelection.test.ts
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { useSelection } from '../useSelection';
+describe('useSelection', () => { it('defined', () => { expect(useSelection).toBeDefined(); }); });
