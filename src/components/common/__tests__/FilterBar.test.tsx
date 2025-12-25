4af0296df81feed4be0727026aab10351956795f
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:09:55 -0300

    test: add FilterBar test [205/3211]

---
+++ src/components/common/__tests__/FilterBar.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { FilterBar } from '../FilterBar';
+describe('FilterBar', () => { it('should be defined', () => { expect(FilterBar).toBeDefined(); }); });
