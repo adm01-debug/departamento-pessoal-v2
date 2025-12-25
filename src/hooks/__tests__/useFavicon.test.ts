e1131b24152c050fdd25182e37e6ba8e6df571d2
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:22:12 -0300

    test: add useFavicon [345/3211]

---
+++ src/hooks/__tests__/useFavicon.test.ts
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { useFavicon } from '../useFavicon';
+describe('useFavicon', () => { it('should be defined', () => { expect(useFavicon).toBeDefined(); }); });
