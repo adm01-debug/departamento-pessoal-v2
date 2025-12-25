19268b7c960764ab34306cd7d63c43ac465ac32a
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:12:48 -0300

    test: add ESocialCard test [253/3211]

---
+++ src/components/esocial/__tests__/ESocialCard.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { ESocialCard } from '../ESocialCard';
+describe('ESocialCard', () => { it('should be defined', () => { expect(ESocialCard).toBeDefined(); }); });
