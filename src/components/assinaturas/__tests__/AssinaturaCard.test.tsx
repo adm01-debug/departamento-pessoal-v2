3e3a90a7815e01a633dcfd9cee7f1be8d912ff54
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:22:51 -0300

    test: add AssinaturaCard [361/3211]

---
+++ src/components/assinaturas/__tests__/AssinaturaCard.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { AssinaturaCard } from '../AssinaturaCard';
+describe('AssinaturaCard', () => { it('should be defined', () => { expect(AssinaturaCard).toBeDefined(); }); });
