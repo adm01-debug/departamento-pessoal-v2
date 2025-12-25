eadc80aa23a66b08bbd0489d7e0f667b2fb607e9
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:39:40 -0300

    test: DatePicker

---
+++ src/components/common/__tests__/DatePicker.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { DatePicker } from '../DatePicker';
+describe('DatePicker', () => { it('defined', () => { expect(DatePicker).toBeDefined(); }); });
