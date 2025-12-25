963937e8072f7fc5a4900d112ec4250bce691a8a
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:22:51 -0300

    test: add AssinaturaModal [361/3211]

---
+++ src/components/assinaturas/__tests__/AssinaturaModal.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { AssinaturaModal } from '../AssinaturaModal';
+describe('AssinaturaModal', () => { it('should be defined', () => { expect(AssinaturaModal).toBeDefined(); }); });
