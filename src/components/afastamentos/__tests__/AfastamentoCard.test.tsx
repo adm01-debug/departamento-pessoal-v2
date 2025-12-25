9562edd20601b55ab8444f4d7790de4591912566
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:12:29 -0300

    test: add AfastamentoCard test [244/3211]

---
+++ src/components/afastamentos/__tests__/AfastamentoCard.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { AfastamentoCard } from '../AfastamentoCard';
+describe('AfastamentoCard', () => { it('should be defined', () => { expect(AfastamentoCard).toBeDefined(); }); });
