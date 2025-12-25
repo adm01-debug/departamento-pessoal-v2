c23e31ef7c94653aaabe293c62e36ec7be36d97c
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:28:25 -0300

    test: useQueryParams [410]

---
+++ src/hooks/__tests__/useQueryParams.test.ts
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { useQueryParams } from '../useQueryParams';
+describe('useQueryParams', () => { it('defined', () => { expect(useQueryParams).toBeDefined(); }); });
