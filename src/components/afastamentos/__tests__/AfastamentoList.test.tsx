c612f4847cb956ad00847ffaaf1d29cb89030582
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:44:08 -0300

    test: AfastamentoList

---
+++ src/components/afastamentos/__tests__/AfastamentoList.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { AfastamentoList } from '../AfastamentoList';
+describe('AfastamentoList', () => { it('defined', () => { expect(AfastamentoList).toBeDefined(); }); });
