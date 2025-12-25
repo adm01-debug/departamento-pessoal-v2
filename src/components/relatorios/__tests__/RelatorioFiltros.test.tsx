ac0fad9285a9143894c072b64df84250aab1b1c9
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:12:58 -0300

    test: add RelatorioFiltros test [256/3211]

---
+++ src/components/relatorios/__tests__/RelatorioFiltros.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { RelatorioFiltros } from '../RelatorioFiltros';
+describe('RelatorioFiltros', () => { it('should be defined', () => { expect(RelatorioFiltros).toBeDefined(); }); });
