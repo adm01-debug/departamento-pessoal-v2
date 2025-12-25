41e9fcde4c274a2308d9753a1c7e70f494935048
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:45:44 -0300

    test: NotificationsContext

---
+++ src/contexts/__tests__/NotificationsContext.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { NotificationsContext } from '../NotificationsContext';
+describe('NotificationsContext', () => { it('defined', () => { expect(NotificationsContext).toBeDefined(); }); });
