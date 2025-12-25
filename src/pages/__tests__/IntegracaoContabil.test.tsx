59a1efea1b60fd436806f822f2d3742dc8e12415
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:42:10 -0300

    test: IntegracaoContabil page

---
+++ src/pages/__tests__/IntegracaoContabil.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import IntegracaoContabil from '../IntegracaoContabil';
+describe('IntegracaoContabil Page', () => { it('should be defined', () => { expect(IntegracaoContabil).toBeDefined(); }); });
