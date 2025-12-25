47e711cea3ac0f2bbf9ba4ff0758143aa2224d3d
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:44:52 -0300

    test: PontoTimeline

---
+++ src/components/ponto/__tests__/PontoTimeline.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { PontoTimeline } from '../PontoTimeline';
+describe('PontoTimeline', () => { it('defined', () => { expect(PontoTimeline).toBeDefined(); }); });
