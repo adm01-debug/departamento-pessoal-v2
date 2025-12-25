01afe30658e3b5f13d94a80b4b19cc26f38fd2aa
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:11:45 -0300

    test: add ColaboradorForm test [228/3211]

---
+++ src/components/colaboradores/__tests__/ColaboradorForm.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { ColaboradorForm } from '../ColaboradorForm';
+describe('ColaboradorForm', () => { it('should be defined', () => { expect(ColaboradorForm).toBeDefined(); }); });
