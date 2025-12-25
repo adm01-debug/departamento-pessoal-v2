bbf76a02603426e95b37e6d781b4733768fc55fa
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:12:06 -0300

    test: add ChecklistAdmissao test [235/3211]

---
+++ src/components/admissao/__tests__/ChecklistAdmissao.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { ChecklistAdmissao } from '../ChecklistAdmissao';
+describe('ChecklistAdmissao', () => { it('should be defined', () => { expect(ChecklistAdmissao).toBeDefined(); }); });
