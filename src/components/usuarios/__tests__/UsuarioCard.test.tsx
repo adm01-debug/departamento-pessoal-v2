b673659ca074b517fc1163b7fe4262e9daf35081
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:23:00 -0300

    test: add UsuarioCard [364/3211]

---
+++ src/components/usuarios/__tests__/UsuarioCard.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { UsuarioCard } from '../UsuarioCard';
+describe('UsuarioCard', () => { it('should be defined', () => { expect(UsuarioCard).toBeDefined(); }); });
