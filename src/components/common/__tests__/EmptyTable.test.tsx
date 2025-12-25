0d943ad588af619254cbf43d2d766c63fa48c210
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:39:42 -0300

    test: EmptyTable

---
+++ src/components/common/__tests__/EmptyTable.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { EmptyTable } from '../EmptyTable';
+describe('EmptyTable', () => { it('defined', () => { expect(EmptyTable).toBeDefined(); }); });
