73fc847a2bc02a5a9245e1af84af1778d7217e5b
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:12:14 -0300

    test: add FolhaResumo test [238/3211]

---
+++ src/components/folha/__tests__/FolhaResumo.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { FolhaResumo } from '../FolhaResumo';
+describe('FolhaResumo', () => { it('should be defined', () => { expect(FolhaResumo).toBeDefined(); }); });
