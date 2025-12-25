4f0034ad4a241005391b2e5326d5a8604e902072
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:23:14 -0300

    test: add NovoCargoModal [367/3211]

---
+++ src/components/cargos/__tests__/NovoCargoModal.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { NovoCargoModal } from '../NovoCargoModal';
+describe('NovoCargoModal', () => { it('should be defined', () => { expect(NovoCargoModal).toBeDefined(); }); });
