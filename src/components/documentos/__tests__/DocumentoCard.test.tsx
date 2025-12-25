286170b487806d9ec6b45bbb76e1dfb850b82dcf
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:13:07 -0300

    test: add DocumentoCard test [259/3211]

---
+++ src/components/documentos/__tests__/DocumentoCard.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { DocumentoCard } from '../DocumentoCard';
+describe('DocumentoCard', () => { it('should be defined', () => { expect(DocumentoCard).toBeDefined(); }); });
