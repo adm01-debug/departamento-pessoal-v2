7ba1b328862178a1107c5be5157741128c4d77ea
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 15:45:44 -0300

    feat(documentos): barrel [135/3211]

---
+++ src/components/documentos/index.ts
@@ -0,0 +1,7 @@
+export { DeclaracaoVinculo } from './DeclaracaoVinculo';
+export { DocumentoCard } from './DocumentoCard';
+export { DocumentoFilter } from './DocumentoFilter';
+export { DocumentoList } from './DocumentoList';
+export { DocumentoUpload } from './DocumentoUpload';
+export { DocumentoViewer } from './DocumentoViewer';
+export { InformeRendimentos } from './InformeRendimentos';
