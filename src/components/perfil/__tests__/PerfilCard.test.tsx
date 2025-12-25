5ed321ae043e2e496fad39fd74da6566350742c4
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:24:38 -0300

    test: PerfilCard [373]

---
+++ src/components/perfil/__tests__/PerfilCard.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { PerfilCard } from '../PerfilCard';
+describe('PerfilCard', () => { it('defined', () => { expect(PerfilCard).toBeDefined(); }); });
