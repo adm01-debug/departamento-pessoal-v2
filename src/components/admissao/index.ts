02aa78745f8912c5ef77179e8c2410d032642ef3
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 15:45:20 -0300

    feat(admissao): create barrel [128/3211]

---
+++ src/components/admissao/index.ts
@@ -0,0 +1,6 @@
+export { AdmissaoChecklistModal } from './AdmissaoChecklistModal';
+export { CalendarioAdmissoes } from './CalendarioAdmissoes';
+export { ChecklistAdmissao } from './ChecklistAdmissao';
+export { ContratacaoDigitalModal } from './ContratacaoDigitalModal';
+export { EditarAdmissaoModal } from './EditarAdmissaoModal';
+export { NovaAdmissaoModal } from './NovaAdmissaoModal';
