864c40c04b7265ef0d42c9a1b99b7f111c8a36dd
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:44:14 -0300

    test: FeriasTimeline

---
+++ src/components/ferias/__tests__/FeriasTimeline.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { FeriasTimeline } from '../FeriasTimeline';
+describe('FeriasTimeline', () => { it('defined', () => { expect(FeriasTimeline).toBeDefined(); }); });
