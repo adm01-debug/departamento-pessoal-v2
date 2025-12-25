2d9d0beb9d4c37b98d2722030ea0f68c3eba15b8
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:22:00 -0300

    test: add useInterval [340/3211]

---
+++ src/hooks/__tests__/useInterval.test.ts
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { useInterval } from '../useInterval';
+describe('useInterval', () => { it('should be defined', () => { expect(useInterval).toBeDefined(); }); });
