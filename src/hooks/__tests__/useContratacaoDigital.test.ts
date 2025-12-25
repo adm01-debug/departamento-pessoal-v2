eb5d5a273b09160f3e16f162e59cb37fbb46fcf5
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:42:28 -0300

    test: useContratacaoDigital

---
+++ src/hooks/__tests__/useContratacaoDigital.test.ts
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { useContratacaoDigital } from '../useContratacaoDigital';
+describe('useContratacaoDigital', () => { it('defined', () => { expect(useContratacaoDigital).toBeDefined(); }); });
