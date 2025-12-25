bf7a81f43a5da162e05d6c919bfe1ee96ca997e9
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:12:16 -0300

    test: add ProcessamentoFolha test [238/3211]

---
+++ src/components/folha/__tests__/ProcessamentoFolha.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { ProcessamentoFolha } from '../ProcessamentoFolha';
+describe('ProcessamentoFolha', () => { it('should be defined', () => { expect(ProcessamentoFolha).toBeDefined(); }); });
