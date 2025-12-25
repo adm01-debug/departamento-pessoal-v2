ff87dd82446427235c560276ca9bc3a9eea574dd
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:13:17 -0300

    test: add AuditoriaFiltros test [262/3211]

---
+++ src/components/auditoria/__tests__/AuditoriaFiltros.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { AuditoriaFiltros } from '../AuditoriaFiltros';
+describe('AuditoriaFiltros', () => { it('should be defined', () => { expect(AuditoriaFiltros).toBeDefined(); }); });
