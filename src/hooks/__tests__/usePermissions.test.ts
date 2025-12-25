72cdb416caf00337a9e33f247787fc0b6ca880bb
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:28:24 -0300

    test: usePermissions [410]

---
+++ src/hooks/__tests__/usePermissions.test.ts
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { usePermissions } from '../usePermissions';
+describe('usePermissions', () => { it('defined', () => { expect(usePermissions).toBeDefined(); }); });
