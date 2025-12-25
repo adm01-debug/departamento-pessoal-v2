00f8ed57249e65d90b2f79d5d3272204a01c17d5
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:42:33 -0300

    test: useIndicadoresDP

---
+++ src/hooks/__tests__/useIndicadoresDP.test.ts
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { useIndicadoresDP } from '../useIndicadoresDP';
+describe('useIndicadoresDP', () => { it('defined', () => { expect(useIndicadoresDP).toBeDefined(); }); });
