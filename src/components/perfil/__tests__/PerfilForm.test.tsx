135638c19cfcbf19dff22edfed091ef7a8cbc40a
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:24:38 -0300

    test: PerfilForm [373]

---
+++ src/components/perfil/__tests__/PerfilForm.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { PerfilForm } from '../PerfilForm';
+describe('PerfilForm', () => { it('defined', () => { expect(PerfilForm).toBeDefined(); }); });
