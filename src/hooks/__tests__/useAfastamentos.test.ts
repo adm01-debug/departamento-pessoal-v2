eb6ffc540f8fe916d66dcc17f2442234d1268620
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:40:04 -0300

    test: useAfastamentos

---
+++ src/hooks/__tests__/useAfastamentos.test.ts
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { useAfastamentos } from '../useAfastamentos';
+describe('useAfastamentos', () => { it('defined', () => { expect(useAfastamentos).toBeDefined(); }); });
