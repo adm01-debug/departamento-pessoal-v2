6ef767000ebbd91d79e7da2be5129d5916199783
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:24:39 -0300

    test: AlterarSenhaModal [373]

---
+++ src/components/perfil/__tests__/AlterarSenhaModal.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { AlterarSenhaModal } from '../AlterarSenhaModal';
+describe('AlterarSenhaModal', () => { it('defined', () => { expect(AlterarSenhaModal).toBeDefined(); }); });
