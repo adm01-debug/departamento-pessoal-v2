b2cd481a786002b67a24272ab539d1b1c5cf9fda
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:40:05 -0300

    test: useColaboradores

---
+++ src/hooks/__tests__/useColaboradores.test.ts
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { useColaboradores } from '../useColaboradores';
+describe('useColaboradores', () => { it('defined', () => { expect(useColaboradores).toBeDefined(); }); });
