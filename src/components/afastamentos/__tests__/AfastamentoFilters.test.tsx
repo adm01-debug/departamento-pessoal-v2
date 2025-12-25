6e6259aec98acbb833ff458d13f34ec3b9bf320b
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:44:08 -0300

    test: AfastamentoFilters

---
+++ src/components/afastamentos/__tests__/AfastamentoFilters.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { AfastamentoFilters } from '../AfastamentoFilters';
+describe('AfastamentoFilters', () => { it('defined', () => { expect(AfastamentoFilters).toBeDefined(); }); });
