740698b35bd19fb0d6a2a19530046ef37bbe7637
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:39:39 -0300

    test: Breadcrumb

---
+++ src/components/common/__tests__/Breadcrumb.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { Breadcrumb } from '../Breadcrumb';
+describe('Breadcrumb', () => { it('defined', () => { expect(Breadcrumb).toBeDefined(); }); });
