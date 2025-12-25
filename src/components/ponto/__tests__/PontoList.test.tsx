b7b15c500bc53230cb4bdb44419ebecf7be4634a
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:44:51 -0300

    test: PontoList

---
+++ src/components/ponto/__tests__/PontoList.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { PontoList } from '../PontoList';
+describe('PontoList', () => { it('defined', () => { expect(PontoList).toBeDefined(); }); });
