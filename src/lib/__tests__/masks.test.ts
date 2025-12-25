42a42b6e515c32920c117af00c1a24f1b07503c0
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:10:08 -0300

    test: add masks test [210/3211]

---
+++ src/lib/__tests__/masks.test.ts
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import * as masks from '../masks';
+describe('masks', () => { it('should export functions', () => { expect(masks).toBeDefined(); }); });
