c1dda3fa6ec45800d732a76bd52a36cc0fd8d6fb
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:44:49 -0300

    test: PontoDetails

---
+++ src/components/ponto/__tests__/PontoDetails.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { PontoDetails } from '../PontoDetails';
+describe('PontoDetails', () => { it('defined', () => { expect(PontoDetails).toBeDefined(); }); });
