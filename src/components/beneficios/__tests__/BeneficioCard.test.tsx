1ef067aa1228f6c9db2a942a460207315c05186e
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:12:28 -0300

    test: add BeneficioCard test [241/3211]

---
+++ src/components/beneficios/__tests__/BeneficioCard.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { BeneficioCard } from '../BeneficioCard';
+describe('BeneficioCard', () => { it('should be defined', () => { expect(BeneficioCard).toBeDefined(); }); });
