32892fb427cc8caec3a4a52aa66fd5844c41ffc3
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:12:15 -0300

    test: add HoleriteViewer test [238/3211]

---
+++ src/components/folha/__tests__/HoleriteViewer.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { HoleriteViewer } from '../HoleriteViewer';
+describe('HoleriteViewer', () => { it('should be defined', () => { expect(HoleriteViewer).toBeDefined(); }); });
