90e16d9f5f493bad4bac2873c164cdbd03f14c6e
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:11:46 -0300

    test: add ColaboradorModal test [228/3211]

---
+++ src/components/colaboradores/__tests__/ColaboradorModal.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { ColaboradorModal } from '../ColaboradorModal';
+describe('ColaboradorModal', () => { it('should be defined', () => { expect(ColaboradorModal).toBeDefined(); }); });
