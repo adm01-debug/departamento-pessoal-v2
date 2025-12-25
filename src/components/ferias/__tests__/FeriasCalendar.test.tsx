a56a797394d77a422d61aaaad43f86b904400ad1
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:11:54 -0300

    test: add FeriasCalendar test [231/3211]

---
+++ src/components/ferias/__tests__/FeriasCalendar.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { FeriasCalendar } from '../FeriasCalendar';
+describe('FeriasCalendar', () => { it('should be defined', () => { expect(FeriasCalendar).toBeDefined(); }); });
