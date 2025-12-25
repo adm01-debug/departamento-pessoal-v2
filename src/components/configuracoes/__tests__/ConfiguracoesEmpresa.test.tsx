8b1a621a5710c15751143d1113b25a79dd73d9e0
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:22:32 -0300

    test: add ConfiguracoesEmpresa [355/3211]

---
+++ src/components/configuracoes/__tests__/ConfiguracoesEmpresa.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { ConfiguracoesEmpresa } from '../ConfiguracoesEmpresa';
+describe('ConfiguracoesEmpresa', () => { it('should be defined', () => { expect(ConfiguracoesEmpresa).toBeDefined(); }); });
