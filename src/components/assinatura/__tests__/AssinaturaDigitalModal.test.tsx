1635bee8387ee7eb6e28715e0d1e104e286c2dbd
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 20:55:35 -0300

    test: AssinaturaDigitalModal

---
+++ src/components/assinatura/__tests__/AssinaturaDigitalModal.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { AssinaturaDigitalModal } from '../AssinaturaDigitalModal';
+describe('AssinaturaDigitalModal', () => { it('defined', () => { expect(AssinaturaDigitalModal).toBeDefined(); }); });
