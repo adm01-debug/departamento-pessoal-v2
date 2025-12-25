103c521a40ef3eb7ccb83318a8ae92b5e3c0f1f7
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:42:06 -0300

    test: Demissao page

---
+++ src/pages/__tests__/Demissao.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import Demissao from '../Demissao';
+describe('Demissao Page', () => { it('should be defined', () => { expect(Demissao).toBeDefined(); }); });
