63ad45dc2f60596068348302f27a0939ef4bfa38
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:23:15 -0300

    test: add DepartamentoCard [370/3211]

---
+++ src/components/departamentos/__tests__/DepartamentoCard.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { DepartamentoCard } from '../DepartamentoCard';
+describe('DepartamentoCard', () => { it('should be defined', () => { expect(DepartamentoCard).toBeDefined(); }); });
