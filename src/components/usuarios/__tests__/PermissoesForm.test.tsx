b8e22969d726d47fd263c6400c178563e6d0dfc2
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:23:01 -0300

    test: add PermissoesForm [364/3211]

---
+++ src/components/usuarios/__tests__/PermissoesForm.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { PermissoesForm } from '../PermissoesForm';
+describe('PermissoesForm', () => { it('should be defined', () => { expect(PermissoesForm).toBeDefined(); }); });
