216058196d6facc7e11973bb40fb87078ff67e9f
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:39:51 -0300

    test: SortableTable

---
+++ src/components/common/__tests__/SortableTable.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { SortableTable } from '../SortableTable';
+describe('SortableTable', () => { it('defined', () => { expect(SortableTable).toBeDefined(); }); });
