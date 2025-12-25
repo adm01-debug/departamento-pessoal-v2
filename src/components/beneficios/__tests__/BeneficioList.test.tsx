e2c8b5ef69d1b73e206596a67be03af20bd5945a
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:44:06 -0300

    test: BeneficioList

---
+++ src/components/beneficios/__tests__/BeneficioList.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { BeneficioList } from '../BeneficioList';
+describe('BeneficioList', () => { it('defined', () => { expect(BeneficioList).toBeDefined(); }); });
