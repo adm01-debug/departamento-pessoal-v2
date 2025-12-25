13e108e2803369b2163ecc112bb349f73fcd4683
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:22:22 -0300

    test: add useLazyComponent [350/3211]

---
+++ src/hooks/__tests__/useLazyComponent.test.ts
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { useLazyComponent } from '../useLazyComponent';
+describe('useLazyComponent', () => { it('should be defined', () => { expect(useLazyComponent).toBeDefined(); }); });
