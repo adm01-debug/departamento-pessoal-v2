ace7c3217d13c183174ce1a840ca339a4ccb4a53
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:11:07 -0300

    test: add DepartmentDistributionChart test [222/3211]

---
+++ src/components/dashboard/__tests__/DepartmentDistributionChart.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { DepartmentDistributionChart } from '../DepartmentDistributionChart';
+describe('DepartmentDistributionChart', () => { it('should be defined', () => { expect(DepartmentDistributionChart).toBeDefined(); }); });
