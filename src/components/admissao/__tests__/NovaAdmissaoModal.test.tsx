2209edcff5d74eea792c945eabaee4c021e74828
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:12:05 -0300

    test: add NovaAdmissaoModal test [235/3211]

---
+++ src/components/admissao/__tests__/NovaAdmissaoModal.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { NovaAdmissaoModal } from '../NovaAdmissaoModal';
+describe('NovaAdmissaoModal', () => { it('should be defined', () => { expect(NovaAdmissaoModal).toBeDefined(); }); });
