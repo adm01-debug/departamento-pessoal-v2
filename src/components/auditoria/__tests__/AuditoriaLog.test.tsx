64f6ec0a6a22610ddf28bcee797b7103e0d5e41a
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:13:16 -0300

    test: add AuditoriaLog test [262/3211]

---
+++ src/components/auditoria/__tests__/AuditoriaLog.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { AuditoriaLog } from '../AuditoriaLog';
+describe('AuditoriaLog', () => { it('should be defined', () => { expect(AuditoriaLog).toBeDefined(); }); });
