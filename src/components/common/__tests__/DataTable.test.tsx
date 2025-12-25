fade495a325dd1ff330b30960581a152dfb86c1f
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:09:56 -0300

    test: add DataTable test [205/3211]

---
+++ src/components/common/__tests__/DataTable.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { DataTable } from '../DataTable';
+describe('DataTable', () => { it('should be defined', () => { expect(DataTable).toBeDefined(); }); });
