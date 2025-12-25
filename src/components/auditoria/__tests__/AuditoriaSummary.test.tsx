093a89e68ee0ddf00911c3b6ff07705c857df80d
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:45:28 -0300

    test: AuditoriaSummary

---
+++ src/components/auditoria/__tests__/AuditoriaSummary.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { AuditoriaSummary } from '../AuditoriaSummary';
+describe('AuditoriaSummary', () => { it('defined', () => { expect(AuditoriaSummary).toBeDefined(); }); });
