9a845671996775d51ac8850c7b84284d856dea7f
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 15:45:12 -0300

    feat(colaboradores): create barrel export [127/3211]

---
+++ src/components/colaboradores/index.ts
@@ -0,0 +1,8 @@
+export { ColaboradorCard } from './ColaboradorCard';
+export { ColaboradorFilter } from './ColaboradorFilter';
+export { ColaboradorFormCompleto } from './ColaboradorFormCompleto';
+export { ColaboradorFormModal } from './ColaboradorFormModal';
+export { ColaboradorList } from './ColaboradorList';
+export { ColaboradorModal } from './ColaboradorModal';
+export { DocumentosColaborador } from './DocumentosColaborador';
+export { ImportacaoColaboradoresModal } from './ImportacaoColaboradoresModal';
