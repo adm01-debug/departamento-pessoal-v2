9bccaa33aaa1f2f126be346d49c977d539a912cd
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:39:52 -0300

    test: TableActions

---
+++ src/components/common/__tests__/TableActions.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { TableActions } from '../TableActions';
+describe('TableActions', () => { it('defined', () => { expect(TableActions).toBeDefined(); }); });
