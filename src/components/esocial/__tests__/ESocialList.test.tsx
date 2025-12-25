5266a5897e5453baa6131820f698621297f1b725
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:44:32 -0300

    test: ESocialList

---
+++ src/components/esocial/__tests__/ESocialList.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { ESocialList } from '../ESocialList';
+describe('ESocialList', () => { it('defined', () => { expect(ESocialList).toBeDefined(); }); });
