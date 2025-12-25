a563b21d67762d332b3e8ca2c07fb00c3811e977
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:16:50 -0300

    test: add apiHelpers test [297/3211]

---
+++ src/lib/__tests__/apiHelpers.test.ts
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import * as apiHelpers from '../apiHelpers';
+describe('apiHelpers', () => { it('should export functions', () => { expect(apiHelpers).toBeDefined(); }); });
