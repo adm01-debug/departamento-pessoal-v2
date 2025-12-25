0a880bd6e247d57eeab2dbae413f7e4c95828b3b
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:12:39 -0300

    test: add RegistroPontoModal test [250/3211]

---
+++ src/components/ponto/__tests__/RegistroPontoModal.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { RegistroPontoModal } from '../RegistroPontoModal';
+describe('RegistroPontoModal', () => { it('should be defined', () => { expect(RegistroPontoModal).toBeDefined(); }); });
