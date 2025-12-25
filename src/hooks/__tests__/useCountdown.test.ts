d14778506fae2eed0f41c6b114e92a30dd02d55b
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:33:49 -0300

    test: useCountdown [462]

---
+++ src/hooks/__tests__/useCountdown.test.ts
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { useCountdown } from '../useCountdown';
+describe('useCountdown', () => { it('defined', () => { expect(useCountdown).toBeDefined(); }); });
