cb10e136d2e1fe05c0adca5678bde7c2e10eda0a
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:42:14 -0300

    test: Organograma page

---
+++ src/pages/__tests__/Organograma.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import Organograma from '../Organograma';
+describe('Organograma Page', () => { it('should be defined', () => { expect(Organograma).toBeDefined(); }); });
