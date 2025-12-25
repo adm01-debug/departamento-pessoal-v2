77e8372b97a33c508be28f7a18b988380af72e5f
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:32:56 -0300

    test: ErrorMessage [454]

---
+++ src/components/common/__tests__/ErrorMessage.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { ErrorMessage } from '../ErrorMessage';
+describe('ErrorMessage', () => { it('defined', () => { expect(ErrorMessage).toBeDefined(); }); });
