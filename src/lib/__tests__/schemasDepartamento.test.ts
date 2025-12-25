9b7f7e867d3cc1a1b2b63e1c5c12530feeb2ca3d
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:42:41 -0300

    test: schemasDepartamento

---
+++ src/lib/__tests__/schemasDepartamento.test.ts
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import * as schemasDepartamento from '../schemasDepartamento';
+describe('schemasDepartamento', () => { it('should be defined', () => { expect(schemasDepartamento).toBeDefined(); }); });
