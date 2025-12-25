f3845cd9c62ec5e52a04c9740f066686a50a32f3
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:39:43 -0300

    test: ExportButton

---
+++ src/components/common/__tests__/ExportButton.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { ExportButton } from '../ExportButton';
+describe('ExportButton', () => { it('defined', () => { expect(ExportButton).toBeDefined(); }); });
