1da37315a299defd7dd5e27843c20979a8e93006
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:22:02 -0300

    test: add useAsync [340/3211]

---
+++ src/hooks/__tests__/useAsync.test.ts
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { useAsync } from '../useAsync';
+describe('useAsync', () => { it('should be defined', () => { expect(useAsync).toBeDefined(); }); });
