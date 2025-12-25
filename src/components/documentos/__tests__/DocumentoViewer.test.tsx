7e893436c6fb5ccde9dafaa2cef3375582ff99f6
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:13:08 -0300

    test: add DocumentoViewer test [259/3211]

---
+++ src/components/documentos/__tests__/DocumentoViewer.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { DocumentoViewer } from '../DocumentoViewer';
+describe('DocumentoViewer', () => { it('should be defined', () => { expect(DocumentoViewer).toBeDefined(); }); });
