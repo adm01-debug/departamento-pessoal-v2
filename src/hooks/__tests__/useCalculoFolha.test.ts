3a3989cf2dd0c60328a28b94159fa9e9a6259831
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:42:27 -0300

    test: useCalculoFolha

---
+++ src/hooks/__tests__/useCalculoFolha.test.ts
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { useCalculoFolha } from '../useCalculoFolha';
+describe('useCalculoFolha', () => { it('defined', () => { expect(useCalculoFolha).toBeDefined(); }); });
