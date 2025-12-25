8555df10ee75f4616584c740fcc65a1992dcd72c
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:42:43 -0300

    test: schemasDocumento

---
+++ src/lib/__tests__/schemasDocumento.test.ts
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import * as schemasDocumento from '../schemasDocumento';
+describe('schemasDocumento', () => { it('should be defined', () => { expect(schemasDocumento).toBeDefined(); }); });
