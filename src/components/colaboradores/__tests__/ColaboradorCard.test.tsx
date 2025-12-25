9c470f3004e931ead960760a2fae35a41c8216f1
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:11:44 -0300

    test: add ColaboradorCard test [228/3211]

---
+++ src/components/colaboradores/__tests__/ColaboradorCard.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { ColaboradorCard } from '../ColaboradorCard';
+describe('ColaboradorCard', () => { it('should be defined', () => { expect(ColaboradorCard).toBeDefined(); }); });
