17f3fd156dbfd617b65f3f06fd1e456613ce6208
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:35:21 -0300

    test: useCounter

---
+++ src/hooks/__tests__/useCounter.test.ts
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { useCounter } from '../useCounter';
+describe('useCounter', () => { it('defined', () => { expect(useCounter).toBeDefined(); }); });
