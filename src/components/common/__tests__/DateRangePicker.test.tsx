3786cc5c4ca285e2b982da50e19b4c1cfcf04284
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:39:41 -0300

    test: DateRangePicker

---
+++ src/components/common/__tests__/DateRangePicker.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { DateRangePicker } from '../DateRangePicker';
+describe('DateRangePicker', () => { it('defined', () => { expect(DateRangePicker).toBeDefined(); }); });
