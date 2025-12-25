b9bbb81bb74e2688068631692b403d8f57096524
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 20:55:30 -0300

    test: AccordionContainer

---
+++ src/components/accordion/__tests__/AccordionContainer.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { AccordionContainer } from '../AccordionContainer';
+describe('AccordionContainer', () => { it('defined', () => { expect(AccordionContainer).toBeDefined(); }); });
