9c7c12ba76f294b78c60fe713b56798e943d645c
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:39:45 -0300

    test: ImportButton

---
+++ src/components/common/__tests__/ImportButton.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { ImportButton } from '../ImportButton';
+describe('ImportButton', () => { it('defined', () => { expect(ImportButton).toBeDefined(); }); });
