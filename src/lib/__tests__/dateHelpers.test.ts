67ec51e2d3f95e97a6b80d8ade94669b1c2303f3
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:10:07 -0300

    test: add dateHelpers test [210/3211]

---
+++ src/lib/__tests__/dateHelpers.test.ts
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import * as dateHelpers from '../dateHelpers';
+describe('dateHelpers', () => { it('should export functions', () => { expect(dateHelpers).toBeDefined(); }); });
