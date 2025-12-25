af34684ff546845d6a6792a6dbcd4a45bbdf8148
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:39:44 -0300

    test: FileUpload

---
+++ src/components/common/__tests__/FileUpload.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { FileUpload } from '../FileUpload';
+describe('FileUpload', () => { it('defined', () => { expect(FileUpload).toBeDefined(); }); });
