25baf8e1c6eb1ce5c038ee1dd4f30d733e57d17d
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:42:42 -0300

    test: schemasDesligamento

---
+++ src/lib/__tests__/schemasDesligamento.test.ts
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import * as schemasDesligamento from '../schemasDesligamento';
+describe('schemasDesligamento', () => { it('should be defined', () => { expect(schemasDesligamento).toBeDefined(); }); });
