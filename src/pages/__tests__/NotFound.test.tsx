bb59325e9db431bb437c5c56067e38e36dcbea3d
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:42:11 -0300

    test: NotFound page

---
+++ src/pages/__tests__/NotFound.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import NotFound from '../NotFound';
+describe('NotFound Page', () => { it('should be defined', () => { expect(NotFound).toBeDefined(); }); });
