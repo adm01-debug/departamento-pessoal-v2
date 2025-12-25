ced1ab2558c82176a78197c7df4b17ea09748bc4
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 20:55:34 -0300

    test: AssinaturaCanvas

---
+++ src/components/assinatura/__tests__/AssinaturaCanvas.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { AssinaturaCanvas } from '../AssinaturaCanvas';
+describe('AssinaturaCanvas', () => { it('defined', () => { expect(AssinaturaCanvas).toBeDefined(); }); });
