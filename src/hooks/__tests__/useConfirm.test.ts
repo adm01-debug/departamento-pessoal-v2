c53f9885de80305165036c4028387f414f548bc5
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:28:28 -0300

    test: useConfirm [410]

---
+++ src/hooks/__tests__/useConfirm.test.ts
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { useConfirm } from '../useConfirm';
+describe('useConfirm', () => { it('defined', () => { expect(useConfirm).toBeDefined(); }); });
