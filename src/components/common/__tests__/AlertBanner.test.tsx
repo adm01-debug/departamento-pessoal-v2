530e1d7cc30beddb146b43ee54a386d7657b0b38
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:29:50 -0300

    test: AlertBanner [424]

---
+++ src/components/common/__tests__/AlertBanner.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { AlertBanner } from '../AlertBanner';
+describe('AlertBanner', () => { it('defined', () => { expect(AlertBanner).toBeDefined(); }); });
