30d21f2dd0e9bb96fa5e7a0a6e83758d304639dd
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:30:51 -0300

    test: useClickAway [436]

---
+++ src/hooks/__tests__/useClickAway.test.ts
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { useClickAway } from '../useClickAway';
+describe('useClickAway', () => { it('defined', () => { expect(useClickAway).toBeDefined(); }); });
