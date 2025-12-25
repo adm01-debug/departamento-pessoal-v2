c673fb104ff3c4dda7365beb24afaf8a6a342d2e
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:42:24 -0300

    test: useAuditoriaWrapper

---
+++ src/hooks/__tests__/useAuditoriaWrapper.test.ts
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { useAuditoriaWrapper } from '../useAuditoriaWrapper';
+describe('useAuditoriaWrapper', () => { it('defined', () => { expect(useAuditoriaWrapper).toBeDefined(); }); });
