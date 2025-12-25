f823bafe91453bb500fa37498ddc17ac4065653b
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:12:59 -0300

    test: add RelatorioExport test [256/3211]

---
+++ src/components/relatorios/__tests__/RelatorioExport.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { RelatorioExport } from '../RelatorioExport';
+describe('RelatorioExport', () => { it('should be defined', () => { expect(RelatorioExport).toBeDefined(); }); });
