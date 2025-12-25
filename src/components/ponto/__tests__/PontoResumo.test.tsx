a02a653952db9425c34951e86f4d384ea1a42aed
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:44:52 -0300

    test: PontoResumo

---
+++ src/components/ponto/__tests__/PontoResumo.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { PontoResumo } from '../PontoResumo';
+describe('PontoResumo', () => { it('defined', () => { expect(PontoResumo).toBeDefined(); }); });
