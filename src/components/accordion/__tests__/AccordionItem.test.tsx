d88014d810ba0c208c58f320209b02ce6785a020
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 20:55:31 -0300

    test: AccordionItem

---
+++ src/components/accordion/__tests__/AccordionItem.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { AccordionItem } from '../AccordionItem';
+describe('AccordionItem', () => { it('defined', () => { expect(AccordionItem).toBeDefined(); }); });
