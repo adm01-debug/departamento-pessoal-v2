ae5fca486b049d69a8931c5d2cd92c6d0521aa44
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:44:11 -0300

    test: FeriasSaldo

---
+++ src/components/ferias/__tests__/FeriasSaldo.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { FeriasSaldo } from '../FeriasSaldo';
+describe('FeriasSaldo', () => { it('defined', () => { expect(FeriasSaldo).toBeDefined(); }); });
