03d27426d586debc0bd4e480dd6789d4e05cb4dc
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:42:07 -0300

    test: Desligamento page

---
+++ src/pages/__tests__/Desligamento.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import Desligamento from '../Desligamento';
+describe('Desligamento Page', () => { it('should be defined', () => { expect(Desligamento).toBeDefined(); }); });
