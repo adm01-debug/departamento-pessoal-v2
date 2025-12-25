496b3daf7e0fc9717081881bc09ae33592be34e2
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 15:45:33 -0300

    feat(beneficios): barrel [131/3211]

---
+++ src/components/beneficios/index.ts
@@ -0,0 +1,4 @@
+export { BeneficioCard } from './BeneficioCard';
+export { BeneficioList } from './BeneficioList';
+export { BeneficioModal } from './BeneficioModal';
+export { ReciboValeTransporte } from './ReciboValeTransporte';
