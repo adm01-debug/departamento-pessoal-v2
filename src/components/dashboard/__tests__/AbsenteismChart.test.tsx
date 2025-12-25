74e1fb49d44b207a82fc16ed55f0f61ed5f6d2ee
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:11:06 -0300

    test: add AbsenteismChart test [222/3211]

---
+++ src/components/dashboard/__tests__/AbsenteismChart.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { AbsenteismChart } from '../AbsenteismChart';
+describe('AbsenteismChart', () => { it('should be defined', () => { expect(AbsenteismChart).toBeDefined(); }); });
