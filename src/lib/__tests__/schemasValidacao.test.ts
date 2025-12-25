597c9a331a7a889e06a0d9765b514a5c9d73b166
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:42:44 -0300

    test: schemasValidacao

---
+++ src/lib/__tests__/schemasValidacao.test.ts
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import * as schemasValidacao from '../schemasValidacao';
+describe('schemasValidacao', () => { it('should be defined', () => { expect(schemasValidacao).toBeDefined(); }); });
