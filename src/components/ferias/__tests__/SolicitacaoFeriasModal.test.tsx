0571491915befe1e9f53c6253eec0350158d1635
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:11:56 -0300

    test: add SolicitacaoFeriasModal test [231/3211]

---
+++ src/components/ferias/__tests__/SolicitacaoFeriasModal.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { SolicitacaoFeriasModal } from '../SolicitacaoFeriasModal';
+describe('SolicitacaoFeriasModal', () => { it('should be defined', () => { expect(SolicitacaoFeriasModal).toBeDefined(); }); });
