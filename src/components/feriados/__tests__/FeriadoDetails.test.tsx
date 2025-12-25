a6a953bbfe4b9f868ddff8492be35c187678aac0
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:45:40 -0300

    test: FeriadoDetails

---
+++ src/components/feriados/__tests__/FeriadoDetails.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { FeriadoDetails } from '../FeriadoDetails';
+describe('FeriadoDetails', () => { it('defined', () => { expect(FeriadoDetails).toBeDefined(); }); });
