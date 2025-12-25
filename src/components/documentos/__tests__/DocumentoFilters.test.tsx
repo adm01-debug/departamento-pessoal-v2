7840e41d741867b8454dea285d641b2b54e3ed1e
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:44:27 -0300

    test: DocumentoFilters

---
+++ src/components/documentos/__tests__/DocumentoFilters.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { DocumentoFilters } from '../DocumentoFilters';
+describe('DocumentoFilters', () => { it('defined', () => { expect(DocumentoFilters).toBeDefined(); }); });
