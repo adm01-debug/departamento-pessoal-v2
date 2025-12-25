ed8de898ad15f0786b9f0d5d698b69996f2662fb
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:45:42 -0300

    test: FeriadoCalendar

---
+++ src/components/feriados/__tests__/FeriadoCalendar.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { FeriadoCalendar } from '../FeriadoCalendar';
+describe('FeriadoCalendar', () => { it('defined', () => { expect(FeriadoCalendar).toBeDefined(); }); });
