058efba6c8745e1fc2cbe3ed173ebe59fac677b7
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:42:05 -0300

    test: Bitrix24Config page

---
+++ src/pages/__tests__/Bitrix24Config.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import Bitrix24Config from '../Bitrix24Config';
+describe('Bitrix24Config Page', () => { it('should be defined', () => { expect(Bitrix24Config).toBeDefined(); }); });
