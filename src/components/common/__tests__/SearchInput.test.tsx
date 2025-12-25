180e76c5a58582421650b934f8e3c7da1bf5f50f
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:09:53 -0300

    test: add SearchInput test [205/3211]

---
+++ src/components/common/__tests__/SearchInput.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { SearchInput } from '../SearchInput';
+describe('SearchInput', () => { it('should be defined', () => { expect(SearchInput).toBeDefined(); }); });
