72b6f83ccbc5248edfc84a3acd5bce58a194bc98
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:22:13 -0300

    test: add useCopyToClipboard [345/3211]

---
+++ src/hooks/__tests__/useCopyToClipboard.test.ts
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { useCopyToClipboard } from '../useCopyToClipboard';
+describe('useCopyToClipboard', () => { it('should be defined', () => { expect(useCopyToClipboard).toBeDefined(); }); });
