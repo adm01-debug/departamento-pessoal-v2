4ee3e44b17c4ed13612e9bb923e3682ec289d6c1
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:44:12 -0300

    test: FeriasStatus

---
+++ src/components/ferias/__tests__/FeriasStatus.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { FeriasStatus } from '../FeriasStatus';
+describe('FeriasStatus', () => { it('defined', () => { expect(FeriasStatus).toBeDefined(); }); });
