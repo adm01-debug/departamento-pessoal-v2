292d281a2d5626efe10c5b56a564e8b355014b0b
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:39:46 -0300

    test: LiveRegion

---
+++ src/components/common/__tests__/LiveRegion.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { LiveRegion } from '../LiveRegion';
+describe('LiveRegion', () => { it('defined', () => { expect(LiveRegion).toBeDefined(); }); });
