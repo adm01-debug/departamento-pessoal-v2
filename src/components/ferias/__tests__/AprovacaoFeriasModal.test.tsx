54bac6a3d3c608b36a943189fd1741521ab6ba87
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:44:14 -0300

    test: AprovacaoFeriasModal

---
+++ src/components/ferias/__tests__/AprovacaoFeriasModal.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { AprovacaoFeriasModal } from '../AprovacaoFeriasModal';
+describe('AprovacaoFeriasModal', () => { it('defined', () => { expect(AprovacaoFeriasModal).toBeDefined(); }); });
