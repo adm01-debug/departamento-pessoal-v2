e43345b3d81d5f6e2dc938bec9a519b7b4ec400e
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:22:32 -0300

    test: add ConfiguracoesGerais [355/3211]

---
+++ src/components/configuracoes/__tests__/ConfiguracoesGerais.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { ConfiguracoesGerais } from '../ConfiguracoesGerais';
+describe('ConfiguracoesGerais', () => { it('should be defined', () => { expect(ConfiguracoesGerais).toBeDefined(); }); });
