434c1e980a9cc69419b7e9a0fae10d71405ce851
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:30:49 -0300

    test: useEventListener [436]

---
+++ src/hooks/__tests__/useEventListener.test.ts
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { useEventListener } from '../useEventListener';
+describe('useEventListener', () => { it('defined', () => { expect(useEventListener).toBeDefined(); }); });
