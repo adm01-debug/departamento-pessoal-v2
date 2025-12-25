9b7bfb9eda42784b4630d43c078b630eaf97ceeb
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:35:21 -0300

    test: useSessionStorage

---
+++ src/hooks/__tests__/useSessionStorage.test.ts
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { useSessionStorage } from '../useSessionStorage';
+describe('useSessionStorage', () => { it('defined', () => { expect(useSessionStorage).toBeDefined(); }); });
