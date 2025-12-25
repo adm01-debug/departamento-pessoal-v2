833a50b354088a260d865bfc1e90c93372325f97
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 15:45:34 -0300

    feat(desligamento): barrel [132/3211]

---
+++ src/components/desligamento/index.ts
@@ -0,0 +1,5 @@
+export { CartaDesligamento } from './CartaDesligamento';
+export { ChecklistDesligamento } from './ChecklistDesligamento';
+export { DeclaracaoVinculo } from './DeclaracaoVinculo';
+export { DesligamentoChecklist } from './DesligamentoChecklist';
+export { DesligamentoModal } from './DesligamentoModal';
