2f5f96193ca4559ddcd7ea472c8fe8defa0b54fd
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:40:06 -0300

    test: useDocumentos

---
+++ src/hooks/__tests__/useDocumentos.test.ts
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { useDocumentos } from '../useDocumentos';
+describe('useDocumentos', () => { it('defined', () => { expect(useDocumentos).toBeDefined(); }); });
