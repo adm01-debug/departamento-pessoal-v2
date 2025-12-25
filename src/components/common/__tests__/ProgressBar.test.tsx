eeaa694c4151cdbdb757a3df88159f64675b64de
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:28:13 -0300

    test: ProgressBar [403]

---
+++ src/components/common/__tests__/ProgressBar.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { ProgressBar } from '../ProgressBar';
+describe('ProgressBar', () => { it('defined', () => { expect(ProgressBar).toBeDefined(); }); });
