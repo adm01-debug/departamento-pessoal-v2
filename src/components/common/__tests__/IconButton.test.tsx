63548efd7b6be070783c2116f16878e69bb2e599
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:29:49 -0300

    test: IconButton [424]

---
+++ src/components/common/__tests__/IconButton.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { IconButton } from '../IconButton';
+describe('IconButton', () => { it('defined', () => { expect(IconButton).toBeDefined(); }); });
