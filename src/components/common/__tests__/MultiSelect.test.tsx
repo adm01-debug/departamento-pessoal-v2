44971f3cc2860799e630ddf8ee4de917c2ccc1d5
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:39:48 -0300

    test: MultiSelect

---
+++ src/components/common/__tests__/MultiSelect.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { MultiSelect } from '../MultiSelect';
+describe('MultiSelect', () => { it('defined', () => { expect(MultiSelect).toBeDefined(); }); });
