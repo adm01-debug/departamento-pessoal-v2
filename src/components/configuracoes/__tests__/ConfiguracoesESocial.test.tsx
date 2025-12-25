bd2ffa01354eff8cf3191313260dec2c0afa4b00
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:22:33 -0300

    test: add ConfiguracoesESocial [355/3211]

---
+++ src/components/configuracoes/__tests__/ConfiguracoesESocial.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { ConfiguracoesESocial } from '../ConfiguracoesESocial';
+describe('ConfiguracoesESocial', () => { it('should be defined', () => { expect(ConfiguracoesESocial).toBeDefined(); }); });
