d17870017321b2ae20a9a17053f6422309347107
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:42:08 -0300

    test: Empresas page

---
+++ src/pages/__tests__/Empresas.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import Empresas from '../Empresas';
+describe('Empresas Page', () => { it('should be defined', () => { expect(Empresas).toBeDefined(); }); });
