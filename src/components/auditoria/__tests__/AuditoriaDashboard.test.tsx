46af5a20ab009f04e9defac62176f8eba0595e81
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:13:17 -0300

    test: add AuditoriaDashboard test [262/3211]

---
+++ src/components/auditoria/__tests__/AuditoriaDashboard.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { AuditoriaDashboard } from '../AuditoriaDashboard';
+describe('AuditoriaDashboard', () => { it('should be defined', () => { expect(AuditoriaDashboard).toBeDefined(); }); });
