4b61f0e487cd5097e65adb421d51c2d8846e19d1
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:43:36 -0300

    test: ColaboradorList

---
+++ src/components/colaboradores/__tests__/ColaboradorList.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { ColaboradorList } from '../ColaboradorList';
+describe('ColaboradorList', () => { it('defined', () => { expect(ColaboradorList).toBeDefined(); }); });
