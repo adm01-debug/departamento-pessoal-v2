0e6f18ef9c3da21467351d93733fd851bf4c79e4
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 20:55:32 -0300

    test: AlertContainer

---
+++ src/components/alert/__tests__/AlertContainer.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { AlertContainer } from '../AlertContainer';
+describe('AlertContainer', () => { it('defined', () => { expect(AlertContainer).toBeDefined(); }); });
