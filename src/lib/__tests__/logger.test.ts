a49190b1246bf0f1173a893cad5c44a1e5777cbb
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:17:00 -0300

    test: add logger test [301/3211]

---
+++ src/lib/__tests__/logger.test.ts
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import * as logger from '../logger';
+describe('logger', () => { it('should export functions', () => { expect(logger).toBeDefined(); }); });
