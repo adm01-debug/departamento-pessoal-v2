b774bee7a88849a385524e2d3fd5a4cc73f03ea3
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:12:58 -0300

    test: add RelatorioCard test [256/3211]

---
+++ src/components/relatorios/__tests__/RelatorioCard.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { RelatorioCard } from '../RelatorioCard';
+describe('RelatorioCard', () => { it('should be defined', () => { expect(RelatorioCard).toBeDefined(); }); });
