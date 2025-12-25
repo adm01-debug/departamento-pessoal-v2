d51d30385068773b1de887652b0105583c054c64
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:44:13 -0300

    test: FeriasSummary

---
+++ src/components/ferias/__tests__/FeriasSummary.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { FeriasSummary } from '../FeriasSummary';
+describe('FeriasSummary', () => { it('defined', () => { expect(FeriasSummary).toBeDefined(); }); });
