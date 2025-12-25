988540d327f99c42fbd970398a9503b6a8906cbc
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:43:37 -0300

    test: ColaboradorSidebar

---
+++ src/components/colaboradores/__tests__/ColaboradorSidebar.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { ColaboradorSidebar } from '../ColaboradorSidebar';
+describe('ColaboradorSidebar', () => { it('defined', () => { expect(ColaboradorSidebar).toBeDefined(); }); });
