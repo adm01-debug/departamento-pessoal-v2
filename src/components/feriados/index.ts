70961f90fef905b72325965f04fbd67ebf977f14
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 15:45:43 -0300

    feat(feriados): barrel [133/3211]

---
+++ src/components/feriados/index.ts
@@ -0,0 +1,4 @@
+export { CalendarioFeriados } from './CalendarioFeriados';
+export { FeriadoCard } from './FeriadoCard';
+export { FeriadoList } from './FeriadoList';
+export { FeriadoModal } from './FeriadoModal';
