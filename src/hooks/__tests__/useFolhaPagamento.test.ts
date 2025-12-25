0bac65a074e8f5258021c8af7835b848910e275f
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:40:07 -0300

    test: useFolhaPagamento

---
+++ src/hooks/__tests__/useFolhaPagamento.test.ts
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { useFolhaPagamento } from '../useFolhaPagamento';
+describe('useFolhaPagamento', () => { it('defined', () => { expect(useFolhaPagamento).toBeDefined(); }); });
