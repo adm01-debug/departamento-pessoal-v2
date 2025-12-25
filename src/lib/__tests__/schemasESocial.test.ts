6bf627f46bba6b8a2cafa5acc30f3b549a28bbbe
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:42:43 -0300

    test: schemasESocial

---
+++ src/lib/__tests__/schemasESocial.test.ts
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import * as schemasESocial from '../schemasESocial';
+describe('schemasESocial', () => { it('should be defined', () => { expect(schemasESocial).toBeDefined(); }); });
