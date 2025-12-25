93615da2d216caed639b7371844c4454c7a5e0ec
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:42:14 -0300

    test: PortalColaborador page

---
+++ src/pages/__tests__/PortalColaborador.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import PortalColaborador from '../PortalColaborador';
+describe('PortalColaborador Page', () => { it('should be defined', () => { expect(PortalColaborador).toBeDefined(); }); });
