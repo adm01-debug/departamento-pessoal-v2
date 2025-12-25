35b46cbc3007c1ee83d4aa941a51b30632fd83fc
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:22:42 -0300

    test: add NovoDesligamentoModal [358/3211]

---
+++ src/components/desligamentos/__tests__/NovoDesligamentoModal.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { NovoDesligamentoModal } from '../NovoDesligamentoModal';
+describe('NovoDesligamentoModal', () => { it('should be defined', () => { expect(NovoDesligamentoModal).toBeDefined(); }); });
