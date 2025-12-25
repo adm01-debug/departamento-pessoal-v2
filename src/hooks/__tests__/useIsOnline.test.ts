1fa3862e92539206c31a1a98e546b310a02f2972
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:30:50 -0300

    test: useIsOnline [436]

---
+++ src/hooks/__tests__/useIsOnline.test.ts
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { useIsOnline } from '../useIsOnline';
+describe('useIsOnline', () => { it('defined', () => { expect(useIsOnline).toBeDefined(); }); });
