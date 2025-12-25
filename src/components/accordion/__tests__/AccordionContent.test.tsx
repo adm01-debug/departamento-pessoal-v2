516ca0590aef8f03b857bbf9f8bb794fb09606e4
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 20:55:30 -0300

    test: AccordionContent

---
+++ src/components/accordion/__tests__/AccordionContent.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { AccordionContent } from '../AccordionContent';
+describe('AccordionContent', () => { it('defined', () => { expect(AccordionContent).toBeDefined(); }); });
