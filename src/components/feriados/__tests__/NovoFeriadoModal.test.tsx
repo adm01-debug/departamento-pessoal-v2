34c4f1804430755467cca960f7021fef837bd3a2
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 16:24:29 -0300

    test: NovoFeriadoModal [371]

---
+++ src/components/feriados/__tests__/NovoFeriadoModal.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect } from 'vitest';
+import { NovoFeriadoModal } from '../NovoFeriadoModal';
+describe('NovoFeriadoModal', () => { it('defined', () => { expect(NovoFeriadoModal).toBeDefined(); }); });
