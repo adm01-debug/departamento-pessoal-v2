a938ff765db61ea8bb8c67d3b40f7b43744af760
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 15:45:55 -0300

    feat(assinatura): barrel [138/3211]

---
+++ src/components/assinatura/index.ts
@@ -0,0 +1,4 @@
+export { AssinaturaCanvas } from './AssinaturaCanvas';
+export { AssinaturaDigitalModal } from './AssinaturaDigitalModal';
+export { AssinaturaPreview } from './AssinaturaPreview';
+export { AssinaturaStatus } from './AssinaturaStatus';
