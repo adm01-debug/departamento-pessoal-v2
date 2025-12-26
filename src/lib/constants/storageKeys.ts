c2490667dbcd0ae897c75b3158df92ff9cc77aad
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 21:27:14 -0300

    feat(constants): add storageKeys

---
+++ src/lib/constants/storageKeys.ts
@@ -0,0 +1,1 @@
+export const STORAGE_KEYS = { AUTH_TOKEN: 'auth_token', REFRESH_TOKEN: 'refresh_token', USER: 'user', THEME: 'theme', LANGUAGE: 'language', SIDEBAR_STATE: 'sidebar_state', RECENT_SEARCHES: 'recent_searches', FILTERS: 'filters', PREFERENCES: 'preferences' };
