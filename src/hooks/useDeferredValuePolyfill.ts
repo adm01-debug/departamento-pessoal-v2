ecf106045c3d8ba5a5feda8c310bb93025db9fc6
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 21:46:11 -0300

    feat(perf): add useDeferredValuePolyfill hook

---
+++ src/hooks/useDeferredValuePolyfill.ts
@@ -0,0 +1,9 @@
+import { useState, useEffect } from 'react';
+export function useDeferredValuePolyfill<T>(value: T, delay = 100): T {
+  const [deferredValue, setDeferredValue] = useState(value);
+  useEffect(() => {
+    const timer = setTimeout(() => setDeferredValue(value), delay);
+    return () => clearTimeout(timer);
+  }, [value, delay]);
+  return deferredValue;
+}
