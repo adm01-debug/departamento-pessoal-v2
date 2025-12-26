840781b2a58b4b46887f387e683597e5859cddb4
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 21:27:13 -0300

    feat(constants): add routes

---
+++ src/lib/constants/routes.ts
@@ -0,0 +1,1 @@
+export const ROUTES = { HOME: '/', DASHBOARD: '/dashboard', COLABORADORES: '/colaboradores', CARGOS: '/cargos', DEPARTAMENTOS: '/departamentos', FOLHA: '/folha', FERIAS: '/ferias', BENEFICIOS: '/beneficios', ADMISSOES: '/admissoes', DESLIGAMENTOS: '/desligamentos', RELATORIOS: '/relatorios', CONFIGURACOES: '/configuracoes', LOGIN: '/login', FORGOT_PASSWORD: '/forgot-password' };
