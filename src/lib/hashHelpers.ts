40ac12e59e5cb85f83e92e3b9e1969a034eecd78
adm01-debug <adm01@promobrindes.com.br>
2025-12-25 21:40:42 -0300

    feat(utils): add hashHelpers

---
+++ src/lib/hashHelpers.ts
@@ -0,0 +1,1 @@
+export const hashString = async (str: string) => { const encoder = new TextEncoder(); const data = encoder.encode(str); const hash = await crypto.subtle.digest('SHA-256', data); return Array.from(new Uint8Array(hash)).map(b => b.toString(16).padStart(2, '0')).join(''); };
