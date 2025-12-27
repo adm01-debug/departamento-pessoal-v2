28d1cd3fd7b7334df747e85db5d4dab2113b2ea2
adm01-debug <adm01@promobrindes.com.br>
2025-12-27 15:57:28 -0300

    feat: telefoneFormatter

---
+++ src/utils/telefoneFormatter.ts
@@ -0,0 +1,1 @@
+export const formatTelefone = (telefone: string): string => { const cleaned = telefone.replace(/\D/g, ''); if (cleaned.length === 11) return cleaned.replace(/(\d{2})(\d{5})(\d{4})/, '($1) $2-$3'); return cleaned.replace(/(\d{2})(\d{4})(\d{4})/, '($1) $2-$3'); };
