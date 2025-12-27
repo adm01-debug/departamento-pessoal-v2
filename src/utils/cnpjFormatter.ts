73dee1e9d80522b9783fb4beabc9615351c664f3
adm01-debug <adm01@promobrindes.com.br>
2025-12-27 15:57:27 -0300

    feat: cnpjFormatter

---
+++ src/utils/cnpjFormatter.ts
@@ -0,0 +1,1 @@
+export const formatCNPJ = (cnpj: string): string => { const cleaned = cnpj.replace(/\D/g, '').slice(0, 14); return cleaned.replace(/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/, '$1.$2.$3/$4-$5'); };
