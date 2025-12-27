f4ca582a858703615f4abcfdf39e1a2151b826aa
adm01-debug <adm01@promobrindes.com.br>
2025-12-27 16:06:56 -0300

    story: Toaster

---
+++ src/stories/Toaster.stories.tsx
@@ -0,0 +1,6 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Toaster } from '@/components/ui/toaster';
+const meta: Meta<typeof Toaster> = { title: 'UI/Toaster', component: Toaster };
+export default meta;
+type Story = StoryObj<typeof Toaster>;
+export const Default: Story = {};
