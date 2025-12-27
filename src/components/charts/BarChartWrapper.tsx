0629a836d6b2c71c113b813b54c8573951113aa4
adm01-debug <adm01@promobrindes.com.br>
2025-12-27 13:23:39 -0300

    feat: BarChartWrapper

---
+++ src/components/charts/BarChartWrapper.tsx
@@ -0,0 +1,5 @@
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+interface BarChartWrapperProps { data: any[]; dataKey: string; xAxisKey: string; color?: string; height?: number; }
+export function BarChartWrapper({ data, dataKey, xAxisKey, color = '#8884d8', height = 300 }: BarChartWrapperProps) {
+  return (<ResponsiveContainer width="100%" height={height}><BarChart data={data}><CartesianGrid strokeDasharray="3 3" /><XAxis dataKey={xAxisKey} /><YAxis /><Tooltip /><Bar dataKey={dataKey} fill={color} /></BarChart></ResponsiveContainer>);
+}
