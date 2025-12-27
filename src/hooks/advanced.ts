7f5f3c25406a5b2b56fe4de3ee7df880ea2c15f4
adm01-debug <adm01@promobrindes.com.br>
2025-12-27 13:20:31 -0300

    feat(hooks): add advanced hooks index

---
+++ src/hooks/advanced.ts
@@ -0,0 +1,24 @@
+// Observers
+export { useIntersectionObserver } from './useIntersectionObserver';
+export { useResizeObserver } from './useResizeObserver';
+export { useMutationObserver } from './useMutationObserver';
+// Events
+export { useEventListener } from './useEventListener';
+export { useClickOutside } from './useClickOutside';
+export { useHover } from './useHover';
+export { useFocus } from './useFocus';
+// State
+export { useToggle } from './useToggle';
+export { useCounter } from './useCounter';
+export { useArray } from './useArray';
+export { useMap } from './useMap';
+// Async
+export { useAsync } from './useAsync';
+export { useFetch } from './useFetch';
+export { useInterval } from './useInterval';
+export { useTimeout } from './useTimeout';
+// Form
+export { useInput } from './useInput';
+export { useSelect } from './useSelect';
+export { useCheckbox } from './useCheckbox';
+export { useRadio } from './useRadio';
