91d86f4eff6edd6aeba6e3ce6b0a85095b895d72
adm01-debug <adm01@promobrindes.com.br>
2025-12-27 15:57:22 -0300

    feat: cities

---
+++ src/constants/cities.ts
@@ -0,0 +1,1 @@
+export const CITIES_BR = [{ codigo: '5208707', nome: 'Goiânia', uf: 'GO' }, { codigo: '3550308', nome: 'São Paulo', uf: 'SP' }, { codigo: '3304557', nome: 'Rio de Janeiro', uf: 'RJ' }];
