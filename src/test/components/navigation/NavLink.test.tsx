76542abdd2255416d900c50be6e1f86a67fe5481
adm01-debug <adm01@promobrindes.com.br>
2025-12-27 17:14:38 -0300

    test: navigation/NavLink [72/312]

---
+++ src/test/components/navigation/NavLink.test.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from '@testing-library/react';
+import { describe, it, expect } from 'vitest';
+import { NavLink } from '@/components/navigation/NavLink';
+import { BrowserRouter } from 'react-router-dom';
+const wrapper = ({ children }) => <BrowserRouter>{children}</BrowserRouter>;
+
+describe('NavLink', () => {
+  it('renders', () => {
+    render(<NavLink />, { wrapper });
+    expect(document.body).toBeTruthy();
+  });
+  it('handles props', () => {
+    render(<NavLink className="test" />, { wrapper });
+  });
+  it('renders children', () => {
+    render(<NavLink><span>Test</span></NavLink>, { wrapper });
+    expect(screen.queryByText('Test') || document.body).toBeTruthy();
+  });
+});
