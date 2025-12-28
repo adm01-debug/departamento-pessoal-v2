6af5bde0c9089945218b8af6d3869f8ee003f970
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 14:14:07 -0300

    test: MultiSelect.test.tsx [V6-618]

---
+++ src/test/components/filters/MultiSelect.test.tsx
@@ -0,0 +1,4 @@
+import { render } from '@testing-library/react';
+import { describe, it } from 'vitest';
+import { MultiSelect } from '@/components/filters/MultiSelect';
+describe('MultiSelect', () => { it('renders', () => { render(<MultiSelect />); }); });
