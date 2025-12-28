7747a7764c8f9f0fb34e4f627a07a736cc9856c3
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 13:35:37 -0300

    test: add tokenUtils.test.ts [V6-108]

---
+++ src/test/utils/tokenUtils.test.ts
@@ -0,0 +1,38 @@
+import { describe, it, expect, vi } from 'vitest';
+import * as tokenUtils from '@/utils/tokenUtils';
+
+describe('tokenUtils', () => {
+  it('should export functions', () => {
+    expect(Object.keys(tokenUtils).length).toBeGreaterThan(0);
+  });
+
+  it('should handle valid input', () => {
+    const mainFn = Object.values(tokenUtils)[0];
+    if (typeof mainFn === 'function') {
+      expect(() => mainFn('test')).not.toThrow();
+    }
+  });
+
+  it('should handle empty string', () => {
+    const mainFn = Object.values(tokenUtils)[0];
+    if (typeof mainFn === 'function') {
+      expect(() => mainFn('')).not.toThrow();
+    }
+  });
+
+  it('should handle special characters', () => {
+    const mainFn = Object.values(tokenUtils)[0];
+    if (typeof mainFn === 'function') {
+      expect(() => mainFn('!@#$%')).not.toThrow();
+    }
+  });
+
+  it('should be consistent', () => {
+    const mainFn = Object.values(tokenUtils)[0];
+    if (typeof mainFn === 'function') {
+      const result1 = mainFn('test');
+      const result2 = mainFn('test');
+      expect(result1).toEqual(result2);
+    }
+  });
+});
