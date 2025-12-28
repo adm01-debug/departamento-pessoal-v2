b5d0402303675b7a31e336b236248eea87b8aae8
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 14:12:12 -0300

    test: TurnoConfig.test.tsx [V6-514]

---
+++ src/test/components/ponto/TurnoConfig.test.tsx
@@ -0,0 +1,4 @@
+import { render } from '@testing-library/react';
+import { describe, it } from 'vitest';
+import { TurnoConfig } from '@/components/ponto/TurnoConfig';
+describe('TurnoConfig', () => { it('renders', () => { render(<TurnoConfig />); }); });
