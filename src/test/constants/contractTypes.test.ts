b7a2b32bf610b986410e9ba87b2aa5fc9a697ff8
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 14:10:41 -0300

    test: contractTypes.test.ts [V6-442]

---
+++ src/test/constants/contractTypes.test.ts
@@ -0,0 +1,6 @@
+import { describe, it, expect } from 'vitest';
+import * as constants from '@/constants/contractTypes';
+describe('contractTypes', () => {
+  it('is defined', () => { expect(constants).toBeDefined(); });
+  it('exports', () => { expect(Object.keys(constants).length).toBeGreaterThan(0); });
+});
