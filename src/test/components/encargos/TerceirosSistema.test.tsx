533154d60b3ba0c35d4ced8f71a5df9dac801349
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 14:13:19 -0300

    test: TerceirosSistema.test.tsx [V6-571]

---
+++ src/test/components/encargos/TerceirosSistema.test.tsx
@@ -0,0 +1,4 @@
+import { render } from '@testing-library/react';
+import { describe, it } from 'vitest';
+import { TerceirosSistema } from '@/components/encargos/TerceirosSistema';
+describe('TerceirosSistema', () => { it('renders', () => { render(<TerceirosSistema />); }); });
