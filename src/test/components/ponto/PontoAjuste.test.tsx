35b52ed3b5a356ff3221382c7bb4aafb54102fce
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 14:12:08 -0300

    test: PontoAjuste.test.tsx [V6-507]

---
+++ src/test/components/ponto/PontoAjuste.test.tsx
@@ -0,0 +1,4 @@
+import { render } from '@testing-library/react';
+import { describe, it } from 'vitest';
+import { PontoAjuste } from '@/components/ponto/PontoAjuste';
+describe('PontoAjuste', () => { it('renders', () => { render(<PontoAjuste />); }); });
