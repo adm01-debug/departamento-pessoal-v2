24265d95f69ae04a108885d1bc9305955f0a57f0
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 14:13:09 -0300

    test: CargoCBO.test.tsx [V6-554]

---
+++ src/test/components/cargo/CargoCBO.test.tsx
@@ -0,0 +1,4 @@
+import { render } from '@testing-library/react';
+import { describe, it } from 'vitest';
+import { CargoCBO } from '@/components/cargo/CargoCBO';
+describe('CargoCBO', () => { it('renders', () => { render(<CargoCBO />); }); });
