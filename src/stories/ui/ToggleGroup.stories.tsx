e529b504f98c15e8c7af5323903415b937380266
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 14:09:45 -0300

    docs: ToggleGroup.stories.tsx [V6-395]

---
+++ src/stories/ui/ToggleGroup.stories.tsx
@@ -0,0 +1,6 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { ToggleGroup } from '@/components/ui/togglegroup';
+const meta: Meta<typeof ToggleGroup> = { title: 'UI/ToggleGroup', component: ToggleGroup, tags: ['autodocs'] };
+export default meta;
+type Story = StoryObj<typeof ToggleGroup>;
+export const Default: Story = { args: {} };
