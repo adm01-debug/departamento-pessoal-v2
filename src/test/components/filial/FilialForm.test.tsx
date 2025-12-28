0dc659054393ff38aeac4c22248fdb89d24eff97
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 14:13:12 -0300

    test: FilialForm.test.tsx [V6-559]

---
+++ src/test/components/filial/FilialForm.test.tsx
@@ -0,0 +1,4 @@
+import { render } from '@testing-library/react';
+import { describe, it } from 'vitest';
+import { FilialForm } from '@/components/filial/FilialForm';
+describe('FilialForm', () => { it('renders', () => { render(<FilialForm />); }); });
