1cd924183fd1b37d3a867280a936323e050c6d7e
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 13:35:34 -0300

    test: add retryHandler.test.ts [V6-102]

---
+++ src/test/utils/retryHandler.test.ts
@@ -0,0 +1,38 @@
+import { describe, it, expect, vi } from 'vitest';
+import * as retryHandler from '@/utils/retryHandler';
+
+describe('retryHandler', () => {
+  it('should export functions', () => {
+    expect(Object.keys(retryHandler).length).toBeGreaterThan(0);
+  });
+
+  it('should handle valid input', () => {
+    const mainFn = Object.values(retryHandler)[0];
+    if (typeof mainFn === 'function') {
+      expect(() => mainFn('test')).not.toThrow();
+    }
+  });
+
+  it('should handle empty string', () => {
+    const mainFn = Object.values(retryHandler)[0];
+    if (typeof mainFn === 'function') {
+      expect(() => mainFn('')).not.toThrow();
+    }
+  });
+
+  it('should handle special characters', () => {
+    const mainFn = Object.values(retryHandler)[0];
+    if (typeof mainFn === 'function') {
+      expect(() => mainFn('!@#$%')).not.toThrow();
+    }
+  });
+
+  it('should be consistent', () => {
+    const mainFn = Object.values(retryHandler)[0];
+    if (typeof mainFn === 'function') {
+      const result1 = mainFn('test');
+      const result2 = mainFn('test');
+      expect(result1).toEqual(result2);
+    }
+  });
+});
