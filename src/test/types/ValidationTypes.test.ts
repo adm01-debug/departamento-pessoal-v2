032851b7a3a434e2befb2cb57682166596c42bc9
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 14:11:14 -0300

    test: ValidationTypes.test.ts [V6-475]

---
+++ src/test/types/ValidationTypes.test.ts
@@ -0,0 +1,5 @@
+import { describe, it, expect } from 'vitest';
+import type * as Types from '@/types/ValidationTypes';
+describe('ValidationTypes', () => {
+  it('types valid', () => { expect(true).toBe(true); });
+});
