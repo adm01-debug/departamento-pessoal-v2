793664a83513e4d86473cf768d6318cebb0e8b70
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 14:12:08 -0300

    test: PontoAbono.test.tsx [V6-508]

---
+++ src/test/components/ponto/PontoAbono.test.tsx
@@ -0,0 +1,4 @@
+import { render } from '@testing-library/react';
+import { describe, it } from 'vitest';
+import { PontoAbono } from '@/components/ponto/PontoAbono';
+describe('PontoAbono', () => { it('renders', () => { render(<PontoAbono />); }); });
