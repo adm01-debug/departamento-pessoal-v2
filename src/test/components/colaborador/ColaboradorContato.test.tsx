c987587a8c0513cbca94ace21bd823dad5b92b28
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 14:12:50 -0300

    test: ColaboradorContato.test.tsx [V6-542]

---
+++ src/test/components/colaborador/ColaboradorContato.test.tsx
@@ -0,0 +1,4 @@
+import { render } from '@testing-library/react';
+import { describe, it } from 'vitest';
+import { ColaboradorContato } from '@/components/colaborador/ColaboradorContato';
+describe('ColaboradorContato', () => { it('renders', () => { render(<ColaboradorContato />); }); });
