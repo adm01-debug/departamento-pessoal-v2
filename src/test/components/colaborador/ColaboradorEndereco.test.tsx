6c19161dab007c22f15bd810f30d916ac73b32bc
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 14:12:51 -0300

    test: ColaboradorEndereco.test.tsx [V6-543]

---
+++ src/test/components/colaborador/ColaboradorEndereco.test.tsx
@@ -0,0 +1,4 @@
+import { render } from '@testing-library/react';
+import { describe, it } from 'vitest';
+import { ColaboradorEndereco } from '@/components/colaborador/ColaboradorEndereco';
+describe('ColaboradorEndereco', () => { it('renders', () => { render(<ColaboradorEndereco />); }); });
