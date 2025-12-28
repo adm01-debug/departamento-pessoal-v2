4edd6e58969ebdff8f5412c826974871cb46850a
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 14:12:37 -0300

    test: FeriasProporcionais.test.tsx [V6-534]

---
+++ src/test/components/rescisao/FeriasProporcionais.test.tsx
@@ -0,0 +1,4 @@
+import { render } from '@testing-library/react';
+import { describe, it } from 'vitest';
+import { FeriasProporcionais } from '@/components/rescisao/FeriasProporcionais';
+describe('FeriasProporcionais', () => { it('renders', () => { render(<FeriasProporcionais />); }); });
