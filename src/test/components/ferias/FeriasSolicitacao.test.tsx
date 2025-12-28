e96443a2fdcca6428acac36ab410077dccdba738
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 14:12:26 -0300

    test: FeriasSolicitacao.test.tsx [V6-516]

---
+++ src/test/components/ferias/FeriasSolicitacao.test.tsx
@@ -0,0 +1,4 @@
+import { render } from '@testing-library/react';
+import { describe, it } from 'vitest';
+import { FeriasSolicitacao } from '@/components/ferias/FeriasSolicitacao';
+describe('FeriasSolicitacao', () => { it('renders', () => { render(<FeriasSolicitacao />); }); });
