4df54efa2846929bf0e6864cae5c8567ccb1a187
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 14:12:36 -0300

    test: SaldoSalario.test.tsx [V6-533]

---
+++ src/test/components/rescisao/SaldoSalario.test.tsx
@@ -0,0 +1,4 @@
+import { render } from '@testing-library/react';
+import { describe, it } from 'vitest';
+import { SaldoSalario } from '@/components/rescisao/SaldoSalario';
+describe('SaldoSalario', () => { it('renders', () => { render(<SaldoSalario />); }); });
