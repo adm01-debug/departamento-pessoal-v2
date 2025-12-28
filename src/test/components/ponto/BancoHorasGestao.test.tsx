044173ed2ff1d5a5e841d2a1a894c348e1f9eb92
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 14:12:11 -0300

    test: BancoHorasGestao.test.tsx [V6-512]

---
+++ src/test/components/ponto/BancoHorasGestao.test.tsx
@@ -0,0 +1,4 @@
+import { render } from '@testing-library/react';
+import { describe, it } from 'vitest';
+import { BancoHorasGestao } from '@/components/ponto/BancoHorasGestao';
+describe('BancoHorasGestao', () => { it('renders', () => { render(<BancoHorasGestao />); }); });
