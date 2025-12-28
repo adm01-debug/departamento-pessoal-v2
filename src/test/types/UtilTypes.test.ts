d9e75420cbcabe8385e03bc4995bd0f2fdb6f12b
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 14:11:14 -0300

    test: UtilTypes.test.ts [V6-474]

---
+++ src/test/types/UtilTypes.test.ts
@@ -0,0 +1,5 @@
+import { describe, it, expect } from 'vitest';
+import type * as Types from '@/types/UtilTypes';
+describe('UtilTypes', () => {
+  it('types valid', () => { expect(true).toBe(true); });
+});
