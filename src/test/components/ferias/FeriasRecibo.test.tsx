36260dd8be111152643e1294027b07dbb00c674f
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 14:12:28 -0300

    test: FeriasRecibo.test.tsx [V6-519]

---
+++ src/test/components/ferias/FeriasRecibo.test.tsx
@@ -0,0 +1,4 @@
+import { render } from '@testing-library/react';
+import { describe, it } from 'vitest';
+import { FeriasRecibo } from '@/components/ferias/FeriasRecibo';
+describe('FeriasRecibo', () => { it('renders', () => { render(<FeriasRecibo />); }); });
