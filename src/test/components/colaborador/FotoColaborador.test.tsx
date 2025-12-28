e9834bee765c1fc062e207728d9efe0099b5da14
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 14:12:54 -0300

    test: FotoColaborador.test.tsx [V6-549]

---
+++ src/test/components/colaborador/FotoColaborador.test.tsx
@@ -0,0 +1,4 @@
+import { render } from '@testing-library/react';
+import { describe, it } from 'vitest';
+import { FotoColaborador } from '@/components/colaborador/FotoColaborador';
+describe('FotoColaborador', () => { it('renders', () => { render(<FotoColaborador />); }); });
