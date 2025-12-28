0c7607899960946c15b828eca9218f3b744a99e8
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 14:12:10 -0300

    test: HoraExtraAprovacao.test.tsx [V6-511]

---
+++ src/test/components/ponto/HoraExtraAprovacao.test.tsx
@@ -0,0 +1,4 @@
+import { render } from '@testing-library/react';
+import { describe, it } from 'vitest';
+import { HoraExtraAprovacao } from '@/components/ponto/HoraExtraAprovacao';
+describe('HoraExtraAprovacao', () => { it('renders', () => { render(<HoraExtraAprovacao />); }); });
