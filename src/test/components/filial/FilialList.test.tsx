3e908348b39acd7185af861a8e769ceb384d9400
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 14:13:12 -0300

    test: FilialList.test.tsx [V6-560]

---
+++ src/test/components/filial/FilialList.test.tsx
@@ -0,0 +1,4 @@
+import { render } from '@testing-library/react';
+import { describe, it } from 'vitest';
+import { FilialList } from '@/components/filial/FilialList';
+describe('FilialList', () => { it('renders', () => { render(<FilialList />); }); });
