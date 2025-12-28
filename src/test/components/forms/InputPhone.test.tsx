fb8045941617d5396d24024b81d6a94b3812cfde
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 14:13:37 -0300

    test: InputPhone.test.tsx [V6-583]

---
+++ src/test/components/forms/InputPhone.test.tsx
@@ -0,0 +1,4 @@
+import { render } from '@testing-library/react';
+import { describe, it } from 'vitest';
+import { InputPhone } from '@/components/forms/InputPhone';
+describe('InputPhone', () => { it('renders', () => { render(<InputPhone />); }); });
