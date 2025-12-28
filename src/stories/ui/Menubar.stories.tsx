299403ae851c0b79e3c0b2760d78febb65daff53
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 14:09:29 -0300

    docs: Menubar.stories.tsx [V6-380]

---
+++ src/stories/ui/Menubar.stories.tsx
@@ -0,0 +1,6 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Menubar } from '@/components/ui/menubar';
+const meta: Meta<typeof Menubar> = { title: 'UI/Menubar', component: Menubar, tags: ['autodocs'] };
+export default meta;
+type Story = StoryObj<typeof Menubar>;
+export const Default: Story = { args: {} };
