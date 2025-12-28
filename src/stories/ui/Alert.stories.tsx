6ff172053a9214f25967434177386ae9a14e5f70
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 14:09:21 -0300

    docs: Alert.stories.tsx [V6-366]

---
+++ src/stories/ui/Alert.stories.tsx
@@ -0,0 +1,6 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Alert } from '@/components/ui/alert';
+const meta: Meta<typeof Alert> = { title: 'UI/Alert', component: Alert, tags: ['autodocs'] };
+export default meta;
+type Story = StoryObj<typeof Alert>;
+export const Default: Story = { args: {} };
