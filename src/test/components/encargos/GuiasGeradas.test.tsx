3bf71b9575b23053829d35524eea2f9cc1082360
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 14:13:20 -0300

    test: GuiasGeradas.test.tsx [V6-573]

---
+++ src/test/components/encargos/GuiasGeradas.test.tsx
@@ -0,0 +1,4 @@
+import { render } from '@testing-library/react';
+import { describe, it } from 'vitest';
+import { GuiasGeradas } from '@/components/encargos/GuiasGeradas';
+describe('GuiasGeradas', () => { it('renders', () => { render(<GuiasGeradas />); }); });
