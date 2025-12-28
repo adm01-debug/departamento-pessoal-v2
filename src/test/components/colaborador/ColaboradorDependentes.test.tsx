c7bc5534ca7d718b3953c254b71fdb886fd3f148
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 14:12:49 -0300

    test: ColaboradorDependentes.test.tsx [V6-541]

---
+++ src/test/components/colaborador/ColaboradorDependentes.test.tsx
@@ -0,0 +1,4 @@
+import { render } from '@testing-library/react';
+import { describe, it } from 'vitest';
+import { ColaboradorDependentes } from '@/components/colaborador/ColaboradorDependentes';
+describe('ColaboradorDependentes', () => { it('renders', () => { render(<ColaboradorDependentes />); }); });
