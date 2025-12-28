2b2d5057631a7f6cd223dcb1d80ff19b1c215d30
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 14:12:52 -0300

    test: DadosProfissionais.test.tsx [V6-546]

---
+++ src/test/components/colaborador/DadosProfissionais.test.tsx
@@ -0,0 +1,4 @@
+import { render } from '@testing-library/react';
+import { describe, it } from 'vitest';
+import { DadosProfissionais } from '@/components/colaborador/DadosProfissionais';
+describe('DadosProfissionais', () => { it('renders', () => { render(<DadosProfissionais />); }); });
