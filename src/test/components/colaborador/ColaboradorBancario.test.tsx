646eb757988ee842a8865bab6388cdb4f65e3573
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 14:12:51 -0300

    test: ColaboradorBancario.test.tsx [V6-544]

---
+++ src/test/components/colaborador/ColaboradorBancario.test.tsx
@@ -0,0 +1,4 @@
+import { render } from '@testing-library/react';
+import { describe, it } from 'vitest';
+import { ColaboradorBancario } from '@/components/colaborador/ColaboradorBancario';
+describe('ColaboradorBancario', () => { it('renders', () => { render(<ColaboradorBancario />); }); });
