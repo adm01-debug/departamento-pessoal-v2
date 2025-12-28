b97aed527b3a1f11cc0cf0d5326c987ea725c69f
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 14:13:33 -0300

    test: FormValidation.test.tsx [V6-577]

---
+++ src/test/components/forms/FormValidation.test.tsx
@@ -0,0 +1,4 @@
+import { render } from '@testing-library/react';
+import { describe, it } from 'vitest';
+import { FormValidation } from '@/components/forms/FormValidation';
+describe('FormValidation', () => { it('renders', () => { render(<FormValidation />); }); });
