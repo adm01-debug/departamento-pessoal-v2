947626d354d2e4f9b7fe30374e6a42d4039c2e92
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 14:13:11 -0300

    test: FilialCard.test.tsx [V6-558]

---
+++ src/test/components/filial/FilialCard.test.tsx
@@ -0,0 +1,4 @@
+import { render } from '@testing-library/react';
+import { describe, it } from 'vitest';
+import { FilialCard } from '@/components/filial/FilialCard';
+describe('FilialCard', () => { it('renders', () => { render(<FilialCard />); }); });
