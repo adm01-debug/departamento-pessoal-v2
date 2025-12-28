37bdb5b04c3abceef8a9817e816991eb34122d36
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 14:13:32 -0300

    test: FormStepper.test.tsx [V6-576]

---
+++ src/test/components/forms/FormStepper.test.tsx
@@ -0,0 +1,4 @@
+import { render } from '@testing-library/react';
+import { describe, it } from 'vitest';
+import { FormStepper } from '@/components/forms/FormStepper';
+describe('FormStepper', () => { it('renders', () => { render(<FormStepper />); }); });
