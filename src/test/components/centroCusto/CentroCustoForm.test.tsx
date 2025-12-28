3a25f7ac1bd2b5cb7c05fcb1c69ca2dc64184c10
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 14:13:14 -0300

    test: CentroCustoForm.test.tsx [V6-562]

---
+++ src/test/components/centroCusto/CentroCustoForm.test.tsx
@@ -0,0 +1,4 @@
+import { render } from '@testing-library/react';
+import { describe, it } from 'vitest';
+import { CentroCustoForm } from '@/components/centroCusto/CentroCustoForm';
+describe('CentroCustoForm', () => { it('renders', () => { render(<CentroCustoForm />); }); });
