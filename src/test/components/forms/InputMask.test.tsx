ae66c288efe2be9ba305946e78f63ec04b6ab234
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 14:13:34 -0300

    test: InputMask.test.tsx [V6-578]

---
+++ src/test/components/forms/InputMask.test.tsx
@@ -0,0 +1,4 @@
+import { render } from '@testing-library/react';
+import { describe, it } from 'vitest';
+import { InputMask } from '@/components/forms/InputMask';
+describe('InputMask', () => { it('renders', () => { render(<InputMask />); }); });
