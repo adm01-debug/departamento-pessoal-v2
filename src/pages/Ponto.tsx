ec3a1e2edd33ad6e6b4f0988f556a2ee4f25d811
adm01-debug <adm01@promobrindes.com.br>
2025-12-29 20:01:18 -0300

    [QA-FIX-010] 🚨 CRITICAL: Corrigir erro sintático memo(function) em Ponto.tsx

--- src/pages/Ponto.tsx
+++ src/pages/Ponto.tsx
@@ -30,7 +30,7 @@ const statusConfig: Record<string, { label: string; bg: string; text: string; ic
 
 const diasSemana = ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'];
 
-export default memo(function) Ponto() {
+export default memo(function Ponto() {
   useEffect(() => { document.title = 'Ponto Eletrônico | DP System'; }, []);
 
   const [colaboradorId, setColaboradorId] = useState<string>('');
@@ -469,16 +469,3 @@ export default memo(function) Ponto() {
     </>
   );
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
