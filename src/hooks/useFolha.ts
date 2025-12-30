ecc1b1cf7d659a1fb2d9f6a0b163367a2c2e2a7b
adm01-debug <adm01@promobrindes.com.br>
2025-12-30 16:23:24 -0300

    [HOTFIX] useFolha.ts - Re-export de useFolhaPagamento

--- src/hooks/useFolha.ts
+++ src/hooks/useFolha.ts
@@ -1,4 +1,11 @@
 /**
  * @fileoverview Hook para gerenciamento de folha de pagamento
  * @module hooks/useFolha
+ * @version V8.5 - Implementação completa
  */
+
+// Re-export do hook principal
+export { useFolhaPagamento as useFolha, useFolhaPagamento } from './useFolhaPagamento';
+
+// Export padrão
+export { useFolhaPagamento as default } from './useFolhaPagamento';
