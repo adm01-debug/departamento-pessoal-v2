15ac92f6837680bf975b16af63a9858654d4973e
adm01-debug <adm01@promobrindes.com.br>
2026-01-02 08:44:32 -0300

    [DP-CRUD] Índice de Toolbars

---
+++ src/components/toolbars.ts
@@ -0,0 +1,10 @@
+// Índice de todas as Toolbars do DP System
+export { ColaboradoresToolbar } from './colaboradores/ColaboradoresToolbar';
+export { FeriasToolbar } from './ferias/FeriasToolbar';
+export { FolhaToolbar } from './folha/FolhaToolbar';
+export { AfastamentosToolbar } from './afastamentos/AfastamentosToolbar';
+export { CargosToolbar } from './cargos/CargosToolbar';
+export { DepartamentosToolbar } from './departamentos/DepartamentosToolbar';
+export { BeneficiosToolbar } from './beneficios/BeneficiosToolbar';
+export { DocumentosToolbar } from './documentos/DocumentosToolbar';
+export { PontoToolbar } from './ponto/PontoToolbar';
