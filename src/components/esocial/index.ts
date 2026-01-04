dd3b2c872283ffe55a3820c875b0909d032b48eb
adm01-debug <adm01@promobrindes.com.br>
2026-01-04 14:24:17 -0300

    [449/476] components/esocial/index

--- src/components/esocial/index.ts
+++ src/components/esocial/index.ts
@@ -1,3 +1,13 @@
-export { EventoESocialCard } from './EventoESocialCard';
-export { EventoESocialModal } from './EventoESocialModal';
-export { LoteESocialModal } from './LoteESocialModal';
+
+export { default as ESocialDashboard } from "./ESocialDashboard";
+export { default as ESocialCard } from "./ESocialCard";
+export { default as ESocialList } from "./ESocialList";
+export { default as ESocialFilters } from "./ESocialFilters";
+export { default as ESocialEventoForm } from "./ESocialEventoForm";
+export { default as ESocialLote } from "./ESocialLote";
+export { default as ESocialTimeline } from "./ESocialTimeline";
+export { default as S1200Remuneracao } from "./S1200Remuneracao";
+export { default as S1260ComercializacaoRural } from "./S1260ComercializacaoRural";
+export { default as S1270ContratacaoAvulsos } from "./S1270ContratacaoAvulsos";
+export { default as S1280InformacoesComplementares } from "./S1280InformacoesComplementares";
+export { default as S2400CDP } from "./S2400CDP";
