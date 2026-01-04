662e540e53633cb29a4e6468bd3a957ce0393ce1
adm01-debug <adm01@promobrindes.com.br>
2026-01-04 11:41:36 -0300

    [127/300] Checkbox

---
+++ src/components/ui-advanced/Checkbox.tsx
@@ -0,0 +1,10 @@
+class CheckboxClass {
+  async execute(params: any): Promise<any> { console.log("Checkbox executed", params); return { success: true }; }
+  async list(filters?: any): Promise<any[]> { return []; }
+  async getById(id: string): Promise<any | null> { return null; }
+  async create(data: any): Promise<any> { return { id: Date.now().toString(), ...data }; }
+  async update(id: string, data: any): Promise<any> { return { id, ...data }; }
+  async delete(id: string): Promise<boolean> { return true; }
+}
+export const Checkbox = new CheckboxClass();
+export default Checkbox;
