d8168796a16fd626e264693ffd20c9449820c2c5
adm01-debug <adm01@promobrindes.com.br>
2026-01-04 11:44:39 -0300

    [280/300] currencyConfig.ts

---
+++ src/constants/currencyConfig.ts
@@ -0,0 +1,10 @@
+export const currencyConfig = {
+  DEFAULT: "default",
+  ITEMS: [] as string[],
+  MAP: {} as Record<string, any>,
+  get(key: string): any { return this.MAP[key] || this.DEFAULT; },
+  has(key: string): boolean { return key in this.MAP; },
+  all(): string[] { return Object.keys(this.MAP); },
+} as const;
+export type currencyConfigType = keyof typeof currencyConfig;
+export default currencyConfig;
