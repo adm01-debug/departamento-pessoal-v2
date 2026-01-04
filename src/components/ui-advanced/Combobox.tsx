b8e26058d546cd7b03f2443bd48bac289f43b900
adm01-debug <adm01@promobrindes.com.br>
2026-01-04 11:41:37 -0300

    [129/300] Combobox

---
+++ src/components/ui-advanced/Combobox.tsx
@@ -0,0 +1,10 @@
+class ComboboxClass {
+  async execute(params: any): Promise<any> { console.log("Combobox executed", params); return { success: true }; }
+  async list(filters?: any): Promise<any[]> { return []; }
+  async getById(id: string): Promise<any | null> { return null; }
+  async create(data: any): Promise<any> { return { id: Date.now().toString(), ...data }; }
+  async update(id: string, data: any): Promise<any> { return { id, ...data }; }
+  async delete(id: string): Promise<boolean> { return true; }
+}
+export const Combobox = new ComboboxClass();
+export default Combobox;
