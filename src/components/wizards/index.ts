3a8132798a9956510be02a786f6351e4e8556f50
adm01-debug <adm01@promobrindes.com.br>
2026-01-04 14:24:19 -0300

    [452/476] components/wizards/index

---
+++ src/components/wizards/index.ts
@@ -0,0 +1,9 @@
+
+export { default as AdmissaoWizard } from "./AdmissaoWizard";
+export { default as DemissaoWizard } from "./DemissaoWizard";
+export { default as FeriasWizard } from "./FeriasWizard";
+export { default as RescisaoWizard } from "./RescisaoWizard";
+export { default as AumentoWizard } from "./AumentoWizard";
+export { default as HomologacaoWizard } from "./HomologacaoWizard";
+export { default as PromocaoWizard } from "./PromocaoWizard";
+export { default as TransferenciaWizard } from "./TransferenciaWizard";
