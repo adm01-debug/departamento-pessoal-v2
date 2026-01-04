9c5f9cd9fb88f6a449938d0896395d0e11ac38e5
adm01-debug <adm01@promobrindes.com.br>
2026-01-04 11:44:30 -0300

    [266/300] tableColumns.ts

---
+++ src/constants/tableColumns.ts
@@ -0,0 +1,10 @@
+export const tableColumns = {
+  DEFAULT: "default",
+  ITEMS: [] as string[],
+  MAP: {} as Record<string, any>,
+  get(key: string): any { return this.MAP[key] || this.DEFAULT; },
+  has(key: string): boolean { return key in this.MAP; },
+  all(): string[] { return Object.keys(this.MAP); },
+} as const;
+export type tableColumnsType = keyof typeof tableColumns;
+export default tableColumns;
