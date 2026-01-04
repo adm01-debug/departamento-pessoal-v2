5ecdd4baae72eaba12713e72056bc3352766a770
adm01-debug <adm01@promobrindes.com.br>
2026-01-04 11:44:35 -0300

    [274/300] eventNames.ts

---
+++ src/constants/eventNames.ts
@@ -0,0 +1,10 @@
+export const eventNames = {
+  DEFAULT: "default",
+  ITEMS: [] as string[],
+  MAP: {} as Record<string, any>,
+  get(key: string): any { return this.MAP[key] || this.DEFAULT; },
+  has(key: string): boolean { return key in this.MAP; },
+  all(): string[] { return Object.keys(this.MAP); },
+} as const;
+export type eventNamesType = keyof typeof eventNames;
+export default eventNames;
