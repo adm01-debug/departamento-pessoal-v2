351cd985da097e39faaeb41629a74aa300d6f822
adm01-debug <adm01@promobrindes.com.br>
2026-01-05 13:42:54 -0300

    [EXP-008]

---
+++ src/components/analytics/index.ts
@@ -0,0 +1,5 @@
+export * from "./HeadcountDepartamento";
+export * from "./DistribuicaoSalarial";
+export * from "./TurnoverChart";
+export * from "./EvolucaoCusto";
+export * from "./AbsenteismoChart";
