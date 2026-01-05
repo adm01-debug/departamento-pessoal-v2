1f3ae9eb630aa5d584edd38c40e803e1df4a7a20
adm01-debug <adm01@promobrindes.com.br>
2026-01-05 08:52:30 -0300

    [INDEX] lgpd

---
+++ src/utils/lgpd/index.ts
@@ -0,0 +1,3 @@
+export { default as anonimizarDados } from './anonimizacao';
+export { default as gerarRelatorioPortabilidade } from './portabilidade';
+export { default as executarExclusao } from './exclusaoDados';
