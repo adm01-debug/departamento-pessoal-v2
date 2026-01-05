a97945429aa6329660d1b2dcedd3a4b1f4363fb9
adm01-debug <adm01@promobrindes.com.br>
2026-01-05 08:52:28 -0300

    [INDEX] calculos

---
+++ src/utils/calculos/index.ts
@@ -0,0 +1,5 @@
+export { default as calculoHoraExtra } from './calculoHoraExtra';
+export { default as calculoMultaFGTS } from './calculoMultaFGTS';
+export { default as calculoSalarioFamilia } from './calculoSalarioFamilia';
+export { default as calculoProporcional } from './calculoProporcional';
+export { default as calculoMediaSalarial } from './calculoMediaSalarial';
