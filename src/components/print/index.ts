ec379fe6dbcf2423278c31805743688f97c1d679
adm01-debug <adm01@promobrindes.com.br>
2026-01-05 13:42:51 -0300

    [EXP-003]

---
+++ src/components/print/index.ts
@@ -0,0 +1,5 @@
+export * from "./HoleritePrint";
+export * from "./FichaRegistroPrint";
+export * from "./ReciboFeriasPrint";
+export * from "./TermoRescisaoPrint";
+export * from "./EspelhoPontoPrint";
