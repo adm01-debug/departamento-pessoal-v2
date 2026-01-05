38d685bacd269563b213896d8fadceb3e02e71e8
adm01-debug <adm01@promobrindes.com.br>
2026-01-05 19:31:31 -0300

    [FIX] Add layouts index

---
+++ src/layouts/index.ts
@@ -0,0 +1,6 @@
+export * from "./MainLayout";
+export * from "./AuthLayout";
+export * from "./DashboardLayout";
+export * from "./PrintLayout";
+export * from "./Sidebar";
+export * from "./Header";
