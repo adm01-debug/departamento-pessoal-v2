e9c8caacf42bfc47ac77c05cf9054f8de06cb660
adm01-debug <adm01@promobrindes.com.br>
2026-01-05 13:42:54 -0300

    [EXP-007]

---
+++ src/components/modals/index.ts
@@ -0,0 +1,5 @@
+export * from "./AdmissaoModal";
+export * from "./DemissaoModal";
+export * from "./FeriasProgramacaoModal";
+export * from "./LancamentoFolhaModal";
+export * from "./TratamentoPontoModal";
