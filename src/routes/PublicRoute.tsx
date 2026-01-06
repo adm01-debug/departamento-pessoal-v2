b4a3e62c673a1e053324e561ccfdac326505b96b
adm01-debug <adm01@promobrindes.com.br>
2026-01-06 18:39:42 -0300

    feat(817)

---
+++ src/routes/PublicRoute.tsx
@@ -0,0 +1,1 @@
+import React from'react';import{Navigate}from'react-router-dom';import{useAuth}from'@/contexts/AuthContext';interface Props{children:React.ReactNode;}export function PublicRoute({children}:Props){const{isAuthenticated}=useAuth();if(isAuthenticated){return<Navigate to="/"replace/>;}return<>{children}</>;}
