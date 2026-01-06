fae33bf9a68a09fd84a5ed7b33ea056eb9e6937c
adm01-debug <adm01@promobrindes.com.br>
2026-01-06 17:24:31 -0300

    feat(664)

--- src/components/ferias/index.ts
+++ src/components/ferias/index.ts
@@ -1,5 +1,1 @@
-export { CalendarioFerias } from './CalendarioFerias';
-export { FeriasCard } from './FeriasCard';
-export { FeriasList } from './FeriasList';
-export { FeriasModal } from './FeriasModal';
-export { WorkflowAprovacaoFerias } from './WorkflowAprovacaoFerias';
+export*from'./FeriasCalendario';
