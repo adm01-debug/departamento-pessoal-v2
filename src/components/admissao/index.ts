babb24ce7b2b905063b19839b9c9f087e30c7554
adm01-debug <adm01@promobrindes.com.br>
2026-01-06 17:25:51 -0300

    feat(674)

--- src/components/admissao/index.ts
+++ src/components/admissao/index.ts
@@ -1,7 +1,1 @@
-export { AdmissaoChecklistModal } from './AdmissaoChecklistModal';
-export { CalendarioAdmissoes } from './CalendarioAdmissoes';
-export { default as ChecklistAdmissao } from './ChecklistAdmissao';
-export { ContratacaoDigitalModal } from './ContratacaoDigitalModal';
-export { EditarAdmissaoModal } from './EditarAdmissaoModal';
-export { NovaAdmissaoModal } from './NovaAdmissaoModal';
-
+export*from'./AdmissaoStepper';export*from'./AdmissaoDocumentos';
