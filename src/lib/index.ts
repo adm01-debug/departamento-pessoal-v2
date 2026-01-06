908d533c1fd8886f4564dbebb7cf3d765f48a44a
adm01-debug <adm01@promobrindes.com.br>
2026-01-06 18:39:14 -0300

    feat(815)

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,1 @@
-export*from'./api';export*from'./queryClient';export*from'./axios';export*from'./utils';
+export{api}from'./api';export{axios}from'./axios';export{queryClient,invalidateQueries,prefetchQuery,setQueryData,getQueryData}from'./queryClient';export{cn}from'./utils';
