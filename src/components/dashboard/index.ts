62b445df24a4c12346c901167d26483ed68ad689
adm01-debug <adm01@promobrindes.com.br>
2026-01-06 17:25:48 -0300

    feat(671)

--- src/components/dashboard/index.ts
+++ src/components/dashboard/index.ts
@@ -1,10 +1,1 @@
-
-export { default as VacationCard } from "./VacationCard";
-export { default as PendingApprovalsCard } from "./PendingApprovalsCard";
-export { default as PayrollSummaryCard } from "./PayrollSummaryCard";
-export { default as FeriasVencidasCard } from "./FeriasVencidasCard";
-export { default as AbsenteismChart } from "./AbsenteismChart";
-export { default as HeadcountChart } from "./HeadcountChart";
-export { default as MetricCard } from "./MetricCard";
-export { default as TurnoverChart } from "./TurnoverChart";
-export { default as DepartmentDistributionChart } from "./DepartmentDistributionChart";
+export*from'./DashboardStats';
