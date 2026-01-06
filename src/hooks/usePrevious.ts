0a191ae58e05e7319b3a384fe97b6e0bf5440a7c
adm01-debug <adm01@promobrindes.com.br>
2026-01-06 18:44:20 -0300

    feat(870)

--- src/hooks/usePrevious.ts
+++ src/hooks/usePrevious.ts
@@ -1,1 +1,1 @@
-import{useRef,useEffect}from'react';export function usePrevious<T>(value:T):T|undefined{const ref=useRef<T>();useEffect(()=>{ref.current=value;},[value]);return ref.current;}
+import{useEffect,useRef}from'react';export function usePrevious<T>(value:T):T|undefined{const ref=useRef<T>();useEffect(()=>{ref.current=value;},[value]);return ref.current;}
