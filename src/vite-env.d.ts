58876b93937920053558ba2dcc59286df7a4da8b
adm01-debug <adm01@promobrindes.com.br>
2026-01-06 17:50:39 -0300

    feat(830)

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,3 +1,2 @@
 /// <reference types="vite/client" />
-interface ImportMetaEnv{readonly VITE_API_URL:string;readonly VITE_APP_NAME:string;}
-interface ImportMeta{readonly env:ImportMetaEnv;}
+interface ImportMetaEnv{readonly VITE_API_URL:string;readonly VITE_APP_NAME:string;readonly VITE_APP_VERSION:string;}interface ImportMeta{readonly env:ImportMetaEnv;}
