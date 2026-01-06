0157b46a4cda00633fb5e97682cb878f8d2405f7
adm01-debug <adm01@promobrindes.com.br>
2026-01-06 14:51:14 -0300

    feat(420)

---
+++ src/components/layout/DashboardLayout.tsx
@@ -0,0 +1,1 @@
+import React from'react';import{MainLayout}from'./MainLayout';interface Props{children:React.ReactNode;}export function DashboardLayout({children}:Props){return<MainLayout>{children}</MainLayout>;}
