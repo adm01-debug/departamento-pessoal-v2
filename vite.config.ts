c9a085000f4472217b94a30d8956865b1e829403
adm01-debug <adm01@promobrindes.com.br>
2026-01-07 07:27:04 -0300

    feat(897)

--- vite.config.ts
+++ vite.config.ts
@@ -1,1 +1,1 @@
-import{defineConfig}from'vite';import react from'@vitejs/plugin-react';import path from'path';export default defineConfig({plugins:[react()],resolve:{alias:{'@':path.resolve(__dirname,'./src')}},server:{port:3000,open:true},build:{outDir:'dist',sourcemap:true},preview:{port:4173}});
+import{defineConfig}from'vite';import react from'@vitejs/plugin-react';import path from'path';export default defineConfig({plugins:[react()],resolve:{alias:{'@':path.resolve(__dirname,'./src')}},server:{port:3000,open:true},build:{outDir:'dist',sourcemap:true}});
