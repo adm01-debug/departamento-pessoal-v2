3ebb84d5e09507ae34a8c985b78155056936f8ea
adm01-debug <adm01@promobrindes.com.br>
2026-01-07 07:28:10 -0300

    feat(914)

---
+++ cypress.config.ts
@@ -0,0 +1,1 @@
+import{defineConfig}from'cypress';export default defineConfig({e2e:{baseUrl:'http://localhost:3000',supportFile:false,specPattern:'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}'},component:{devServer:{framework:'react',bundler:'vite'}}});
