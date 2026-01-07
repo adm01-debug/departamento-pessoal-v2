bbb00662589aac8d9da68c4d9cba8c9d3421e453
adm01-debug <adm01@promobrindes.com.br>
2026-01-07 07:27:47 -0300

    feat(907)

---
+++ jest.config.js
@@ -0,0 +1,1 @@
+module.exports={preset:'ts-jest',testEnvironment:'jsdom',setupFilesAfterEnv:['<rootDir>/src/setupTests.ts'],moduleNameMapper:{'^@/(.*)$':'<rootDir>/src/$1'},collectCoverageFrom:['src/**/*.{ts,tsx}','!src/**/*.d.ts'],coverageThreshold:{global:{branches:70,functions:70,lines:70,statements:70}}};
