db12c77870074858551e0604361eced3114281a3
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:34:01 -0300

    feat(V15-056)

--- src/features/beneficios/index.ts
+++ src/features/beneficios/index.ts
@@ -1,1 +1,6 @@
-export*from'./api';export*from'./hooks';
+// V15-056: src/features/beneficios/index.ts
+export * from './api';
+export * from './hooks';
+export * from './types';
+export { BeneficiosProvider } from './BeneficiosProvider';
+export { useBeneficiosContext } from './BeneficiosContext';
