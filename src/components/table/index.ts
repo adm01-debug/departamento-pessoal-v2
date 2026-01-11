9e6be9fe84e31dce74c3afad9b2009b0c418a188
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:32:26 -0300

    feat(V15-029)

--- src/components/table/index.ts
+++ src/components/table/index.ts
@@ -1,3 +1,11 @@
-export * from './SimpleTable';
-export * from './SelectableRow';
-export * from './SortableHeader';
+// V15-029: src/components/table/index.ts
+export { Table } from './Table';
+export { TableHeader } from './TableHeader';
+export { TableBody } from './TableBody';
+export { TableRow } from './TableRow';
+export { TableCell } from './TableCell';
+export { TablePagination } from './TablePagination';
+export { TableSorting } from './TableSorting';
+export { TableFilter } from './TableFilter';
+export { useTable } from './hooks/useTable';
+export type { TableProps, Column, SortConfig } from './types';
