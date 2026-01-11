18db8c5416f46e21499d9abf4429ce781faabe0b
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:33:38 -0300

    feat(V15-047)

--- src/factories/index.ts
+++ src/factories/index.ts
@@ -1,1 +1,7 @@
-export{createColaborador}from'./colaboradorFactory';export{createFerias}from'./feriasFactory';
+// V15-047: src/factories/index.ts
+export { ComponentFactory } from './ComponentFactory';
+export { FormFactory } from './FormFactory';
+export { TableFactory } from './TableFactory';
+export { ModalFactory } from './ModalFactory';
+export { createComponent } from './createComponent';
+export type { FactoryConfig, FactoryResult } from './types';
