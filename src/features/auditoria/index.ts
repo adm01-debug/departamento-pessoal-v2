35b99af102cab98fcaab315b3e33b522beafab81
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:33:59 -0300

    feat(V15-053)

--- src/features/auditoria/index.ts
+++ src/features/auditoria/index.ts
@@ -1,1 +1,6 @@
-export*from'./api';export*from'./hooks';
+// V15-053: src/features/auditoria/index.ts
+export * from './api';
+export * from './hooks';
+export * from './types';
+export { AuditoriaProvider } from './AuditoriaProvider';
+export { useAuditoriaContext } from './AuditoriaContext';
