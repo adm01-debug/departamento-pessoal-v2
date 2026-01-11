5948cd21d969b2159091a8aa4aab7d92622a58d2
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:35:30 -0300

    feat(V15-086)

--- src/throttle/index.ts
+++ src/throttle/index.ts
@@ -1,1 +1,7 @@
-export{throttle,debounce}from'./throttle';
+// V15-086: src/throttle/index.ts
+export { throttle } from './throttle';
+export { debounce } from './debounce';
+export { rateLimit } from './rateLimit';
+export { useThrottle } from './hooks/useThrottle';
+export { useDebounce } from './hooks/useDebounce';
+export type { ThrottleConfig, DebounceConfig } from './types';
