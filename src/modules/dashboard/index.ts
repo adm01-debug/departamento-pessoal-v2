b280509a01e8408a1594717b8146ed8bffe7b7b7
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:34:56 -0300

    feat(V15-070)

--- src/modules/dashboard/index.ts
+++ src/modules/dashboard/index.ts
@@ -1,1 +1,6 @@
-export{DashboardStats}from'./DashboardStats';export{DashboardCharts}from'./DashboardCharts';
+// V15-070: src/modules/dashboard/index.ts
+export { DashboardModule } from './DashboardModule';
+export { DashboardContainer } from './DashboardContainer';
+export { useDashboard } from './hooks/useDashboard';
+export * from './components';
+export * from './types';
