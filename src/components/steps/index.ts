f945af0edf7fe878f28d507514d4e7fcaa0dac27
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:32:26 -0300

    feat(V15-028)

--- src/components/steps/index.ts
+++ src/components/steps/index.ts
@@ -1,2 +1,9 @@
-export * from './Steps';
-export * from './VerticalSteps';
+// V15-028: src/components/steps/index.ts
+export { Steps } from './Steps';
+export { Step } from './Step';
+export { StepIndicator } from './StepIndicator';
+export { StepContent } from './StepContent';
+export { StepNavigation } from './StepNavigation';
+export { Wizard } from './Wizard';
+export { useSteps } from './hooks/useSteps';
+export type { StepProps, StepStatus } from './types';
