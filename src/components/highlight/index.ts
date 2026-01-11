7f26d878199741b23c7995ad70bc19f649338293
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:32:02 -0300

    feat(V15-019)

--- src/components/highlight/index.ts
+++ src/components/highlight/index.ts
@@ -1,1 +1,7 @@
-export * from './HighlightText';
+// V15-019: src/components/highlight/index.ts
+export { Highlight } from './Highlight';
+export { HighlightText } from './HighlightText';
+export { HighlightCode } from './HighlightCode';
+export { SearchHighlight } from './SearchHighlight';
+export { MarkText } from './MarkText';
+export type { HighlightProps } from './types';
