12e91b45483d74767deeaeb38987207adbe90ae6
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:32:48 -0300

    feat(V15-031)

--- src/components/truncate/index.ts
+++ src/components/truncate/index.ts
@@ -1,1 +1,7 @@
-export * from './TruncateText';
+// V15-031: src/components/truncate/index.ts
+export { Truncate } from './Truncate';
+export { TruncateText } from './TruncateText';
+export { Ellipsis } from './Ellipsis';
+export { ReadMore } from './ReadMore';
+export { Clamp } from './Clamp';
+export type { TruncateProps } from './types';
