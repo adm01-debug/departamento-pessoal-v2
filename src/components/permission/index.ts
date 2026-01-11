0848b0368b4b1bc99e7428346ad39ee7083f5d3b
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:32:23 -0300

    feat(V15-025)

--- src/components/permission/index.ts
+++ src/components/permission/index.ts
@@ -1,3 +1,8 @@
-export * from './Can';
-export * from './RequireRole';
-export * from './Protected';
+// V15-025: src/components/permission/index.ts
+export { PermissionGuard } from './PermissionGuard';
+export { PermissionCheck } from './PermissionCheck';
+export { RoleGuard } from './RoleGuard';
+export { AccessControl } from './AccessControl';
+export { PermissionList } from './PermissionList';
+export { usePermission } from './hooks/usePermission';
+export type { Permission, Role } from './types';
