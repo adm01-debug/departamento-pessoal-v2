37fbe13726fefe880dcaaa301adfc374c160b8a5
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:34:04 -0300

    feat(V15-060)

--- src/features/departamentos/index.ts
+++ src/features/departamentos/index.ts
@@ -1,1 +1,6 @@
-export*from'./api';export*from'./hooks';
+// V15-060: src/features/departamentos/index.ts
+export * from './api';
+export * from './hooks';
+export * from './types';
+export { DepartamentosProvider } from './DepartamentosProvider';
+export { useDepartamentosContext } from './DepartamentosContext';
