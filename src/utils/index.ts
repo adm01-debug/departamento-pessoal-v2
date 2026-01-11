d498bbee241b5f982096a5df820eff0536d3f64c
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 12:38:54 -0300

    V15-386

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,10 +1,4 @@
-// V15-156: src/utils/index.ts
-export * from './array';
-export * from './object';
-export * from './string';
-export * from './number';
-export * from './dom';
-export * from './file';
-export * from './url';
-export * from './color';
-export * from './crypto';
+// V15-386
+export { cn } from './cn';
+export { storage } from './storage';
+export * from './download';
