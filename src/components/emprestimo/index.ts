6f8baf7bc2d7d0cfe4b4e9fa727ac6c1f0706db0
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:31:37 -0300

    feat(V15-015)

--- src/components/emprestimo/index.ts
+++ src/components/emprestimo/index.ts
@@ -1,1 +1,8 @@
-export * from "./EmprestimoCard";
+// V15-015: src/components/emprestimo/index.ts
+export { EmprestimoForm } from './EmprestimoForm';
+export { EmprestimoList } from './EmprestimoList';
+export { EmprestimoParcelas } from './EmprestimoParcelas';
+export { EmprestimoCalculo } from './EmprestimoCalculo';
+export { EmprestimoStatus } from './EmprestimoStatus';
+export { useEmprestimo } from './hooks/useEmprestimo';
+export type { Emprestimo, Parcela } from './types';
