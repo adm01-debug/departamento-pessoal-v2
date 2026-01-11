c5649cf6502d7d3f39d572719abe510d98ec0183
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:30:47 -0300

    feat(V15-002): Expand index exports

--- src/components/admissao/index.ts
+++ src/components/admissao/index.ts
@@ -1,1 +1,9 @@
-export*from'./AdmissaoStepper';export*from'./AdmissaoDocumentos';
+// V15-002: src/components/admissao/index.ts
+export { AdmissaoForm } from './AdmissaoForm';
+export { AdmissaoList } from './AdmissaoList';
+export { AdmissaoWizard } from './AdmissaoWizard';
+export { AdmissaoDocumentos } from './AdmissaoDocumentos';
+export { AdmissaoResumo } from './AdmissaoResumo';
+export { AdmissaoStatus } from './AdmissaoStatus';
+export { useAdmissao } from './hooks/useAdmissao';
+export type { AdmissaoData, AdmissaoFormProps } from './types';
