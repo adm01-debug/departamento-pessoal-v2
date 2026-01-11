e74a86490f7a238e3a9ad7fdc35f53bc66e132f6
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:33:11 -0300

    feat(V15-037)

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,1 +1,7 @@
-export*from'./endpoints';
+// V15-037: src/api/index.ts
+export { api } from './client';
+export { apiClient } from './apiClient';
+export { request, get, post, put, del } from './methods';
+export { useApi } from './hooks/useApi';
+export { ApiProvider } from './ApiProvider';
+export type { ApiConfig, ApiResponse, ApiError } from './types';
