ebfa67ec8872fa9dee5d65a776d47360d208eb52
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:34:59 -0300

    feat(V15-075)

--- src/observers/index.ts
+++ src/observers/index.ts
@@ -1,1 +1,6 @@
-export{setupNotificationObserver}from'./notificationObserver';
+// V15-075: src/observers/index.ts
+export { IntersectionObserver } from './IntersectionObserver';
+export { ResizeObserver } from './ResizeObserver';
+export { MutationObserver } from './MutationObserver';
+export { useObserver } from './hooks/useObserver';
+export type { ObserverConfig, ObserverCallback } from './types';
