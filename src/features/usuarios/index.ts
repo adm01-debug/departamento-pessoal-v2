b12b5358edc4b5bfe2dffcc79fe345d8e48a5a20
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:34:28 -0300

    feat(V15-069)

--- src/features/usuarios/index.ts
+++ src/features/usuarios/index.ts
@@ -1,1 +1,6 @@
-export*from'./api';export*from'./hooks';
+// V15-069: src/features/usuarios/index.ts
+export * from './api';
+export * from './hooks';
+export * from './types';
+export { UsuariosProvider } from './UsuariosProvider';
+export { useUsuariosContext } from './UsuariosContext';
