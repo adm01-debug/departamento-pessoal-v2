94f6c8f0718fc0c83054de132bed758ba143285e
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:32:04 -0300

    feat(V15-023)

--- src/components/page/index.ts
+++ src/components/page/index.ts
@@ -1,3 +1,9 @@
-export * from './PageHeader';
-export * from './PageContent';
-export * from './Section';
+// V15-023: src/components/page/index.ts
+export { Page } from './Page';
+export { PageHeader } from './PageHeader';
+export { PageContent } from './PageContent';
+export { PageFooter } from './PageFooter';
+export { PageTitle } from './PageTitle';
+export { PageActions } from './PageActions';
+export { PageBreadcrumb } from './PageBreadcrumb';
+export type { PageProps, PageLayout } from './types';
