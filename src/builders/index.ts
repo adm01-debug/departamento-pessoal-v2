a6365a92a20358e81f246fcbdbf419f06b7506cd
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:33:13 -0300

    feat(V15-039)

--- src/builders/index.ts
+++ src/builders/index.ts
@@ -1,1 +1,7 @@
-export{QueryBuilder}from'./queryBuilder';export{UrlBuilder}from'./urlBuilder';
+// V15-039: src/builders/index.ts
+export { QueryBuilder } from './QueryBuilder';
+export { FormBuilder } from './FormBuilder';
+export { TableBuilder } from './TableBuilder';
+export { ChartBuilder } from './ChartBuilder';
+export { ReportBuilder } from './ReportBuilder';
+export type { BuilderConfig, BuilderResult } from './types';
