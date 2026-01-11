c83664f3cab5ae12cf46175d390a18e7e59f546b
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:33:36 -0300

    feat(V15-045)

--- src/enums/index.ts
+++ src/enums/index.ts
@@ -1,1 +1,8 @@
-export*from'./status';export*from'./tipoContrato';export*from'./tipoBeneficio';
+// V15-045: src/enums/index.ts
+export { StatusColaborador } from './StatusColaborador';
+export { TipoContrato } from './TipoContrato';
+export { TipoAfastamento } from './TipoAfastamento';
+export { TipoBeneficio } from './TipoBeneficio';
+export { StatusFolha } from './StatusFolha';
+export { TipoEvento } from './TipoEvento';
+export { GrauParentesco } from './GrauParentesco';
