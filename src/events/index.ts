4470c5aea7304fa4e8291615613ee835fb41cb3a
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:33:37 -0300

    feat(V15-046)

--- src/events/index.ts
+++ src/events/index.ts
@@ -1,1 +1,6 @@
-export{eventBus}from'./eventBus';export{EVENTS}from'./events';
+// V15-046: src/events/index.ts
+export { EventEmitter } from './EventEmitter';
+export { EventBus } from './EventBus';
+export { subscribe, publish } from './pubsub';
+export { useEvent } from './hooks/useEvent';
+export type { EventHandler, EventPayload } from './types';
