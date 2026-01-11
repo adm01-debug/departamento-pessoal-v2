c334a4cb0f30ec413a5bbe916ed53686592fa3ec
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 11:24:35 -0300

    feat(V15-215)

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,1 +1,11 @@
-export{authService}from'./authService';export{colaboradorService}from'./colaboradorService';export{feriasService}from'./feriasService';export{folhaService}from'./folhaService';export{pontoService}from'./pontoService';export{beneficioService}from'./beneficioService';export{cargoService}from'./cargoService';export{departamentoService}from'./departamentoService';export{usuarioService}from'./usuarioService';export{empresaService}from'./empresaService';export{esocialService}from'./esocialService';export{relatorioService}from'./relatorioService';
+// V15-215: src/services/index.ts
+export { colaboradorService } from './colaboradorService';
+export { empresaService } from './empresaService';
+export { folhaService } from './folhaService';
+export { feriasService } from './feriasService';
+export { pontoService } from './pontoService';
+export { beneficioService } from './beneficioService';
+export { documentoService } from './documentoService';
+export { relatorioService } from './relatorioService';
+export { seedService } from './seedService';
+export { queueService, createQueue } from './queueService';
