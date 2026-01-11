06efbe89f4c059a7f70aff1e5d03d4bf8e481c12
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 13:35:47 -0300

    V15-500

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,14 +1,11 @@
-// V15-280: src/components/index.ts
+// V15-500 - Main Components Export
 export * from './ui';
-export * from './layout';
 export * from './forms';
-export * from './colaborador';
-export * from './folha';
-export * from './ferias';
-export * from './ponto';
-export * from './beneficio';
-export * from './dashboard';
 export * from './charts';
-export * from './relatorio';
-export * from './esocial';
-export * from './documento';
+export * from './layout';
+export * from './loading';
+export * from './empty';
+export * from './feedback';
+export * from './data-display';
+export * from './widgets';
+export * from './modals';
