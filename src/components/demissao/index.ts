a3ab87888ff346390969af4e4000cd0462e50e4c
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:31:14 -0300

    feat(V15-012): Expand index

--- src/components/demissao/index.ts
+++ src/components/demissao/index.ts
@@ -1,1 +1,9 @@
-export*from'./DemissaoResumo';
+// V15-012: src/components/demissao/index.ts
+export { DemissaoForm } from './DemissaoForm';
+export { DemissaoList } from './DemissaoList';
+export { DemissaoWizard } from './DemissaoWizard';
+export { DemissaoCalculo } from './DemissaoCalculo';
+export { DemissaoDocumentos } from './DemissaoDocumentos';
+export { RescisaoForm } from './RescisaoForm';
+export { useDemissao } from './hooks/useDemissao';
+export type { DemissaoData, TipoDemissao } from './types';
