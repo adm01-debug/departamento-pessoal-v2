01a1967f8f144df4a6f1432b9ce5c2d03d440d75
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:31:12 -0300

    feat(V15-009): Expand index

--- src/components/dashboard/index.ts
+++ src/components/dashboard/index.ts
@@ -1,1 +1,10 @@
-export*from'./DashboardStats';
+// V15-009: src/components/dashboard/index.ts
+export { DashboardHeader } from './DashboardHeader';
+export { DashboardGrid } from './DashboardGrid';
+export { DashboardCard } from './DashboardCard';
+export { DashboardWidget } from './DashboardWidget';
+export { DashboardChart } from './DashboardChart';
+export { DashboardStats } from './DashboardStats';
+export { DashboardKPI } from './DashboardKPI';
+export { QuickActions } from './QuickActions';
+export type { DashboardData, WidgetConfig } from './types';
