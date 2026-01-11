090af7d9efb02666e5d41864b61d03945ad65c3c
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:33:40 -0300

    feat(V15-051)

--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -1,1 +1,8 @@
-export{handlers}from'./handlers';export{server}from'./server';
+// V15-051: src/mocks/index.ts
+export { mockColaboradores } from './colaboradores';
+export { mockEmpresas } from './empresas';
+export { mockFolha } from './folha';
+export { mockFerias } from './ferias';
+export { mockPonto } from './ponto';
+export { setupMocks } from './setup';
+export type { MockConfig } from './types';
