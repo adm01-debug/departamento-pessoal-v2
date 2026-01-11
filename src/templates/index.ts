a653c8cf96027bf281e0e230b3469ba882b68c62
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:35:27 -0300

    feat(V15-081)

--- src/templates/index.ts
+++ src/templates/index.ts
@@ -1,1 +1,7 @@
-export*from'./email';export*from'./pdf';
+// V15-081: src/templates/index.ts
+export { EmailTemplate } from './EmailTemplate';
+export { PDFTemplate } from './PDFTemplate';
+export { ReportTemplate } from './ReportTemplate';
+export { ContractTemplate } from './ContractTemplate';
+export { HoleriteTemplate } from './HoleriteTemplate';
+export type { TemplateData, TemplateConfig } from './types';
