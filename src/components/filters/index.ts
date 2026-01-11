e5b8a57e1f35138b509cc3bfc1bb01101607140f
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:31:39 -0300

    feat(V15-018)

--- src/components/filters/index.ts
+++ src/components/filters/index.ts
@@ -1,1 +1,10 @@
-export*from'./ColaboradorFilter';export*from'./DateRangeFilter';
+// V15-018: src/components/filters/index.ts
+export { FilterBar } from './FilterBar';
+export { FilterPanel } from './FilterPanel';
+export { FilterChip } from './FilterChip';
+export { FilterGroup } from './FilterGroup';
+export { DateFilter } from './DateFilter';
+export { StatusFilter } from './StatusFilter';
+export { SearchFilter } from './SearchFilter';
+export { useFilters } from './hooks/useFilters';
+export type { FilterConfig, FilterValue } from './types';
