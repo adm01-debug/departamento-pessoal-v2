166bb79b1f9b038a5de84c9d01d53d58d0ef8adf
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:34:58 -0300

    feat(V15-073)

--- src/modules/folha/index.ts
+++ src/modules/folha/index.ts
@@ -1,1 +1,6 @@
-export{FolhaResumo}from'./FolhaResumo';export{FolhaList}from'./FolhaList';
+// V15-073: src/modules/folha/index.ts
+export { FolhaModule } from './FolhaModule';
+export { FolhaContainer } from './FolhaContainer';
+export { useFolha } from './hooks/useFolha';
+export * from './components';
+export * from './types';
