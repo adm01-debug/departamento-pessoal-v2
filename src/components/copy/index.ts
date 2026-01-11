318d0b51e97cf2d2c13066704c8ab72bc628595c
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:31:11 -0300

    feat(V15-008): Expand index

--- src/components/copy/index.ts
+++ src/components/copy/index.ts
@@ -1,3 +1,8 @@
-export * from './CopyButton';
-export * from './CopyableText';
-export * from './CodeBlock';
+// V15-008: src/components/copy/index.ts
+export { CopyButton } from './CopyButton';
+export { CopyText } from './CopyText';
+export { CopyCode } from './CopyCode';
+export { CopyToClipboard } from './CopyToClipboard';
+export { CopyField } from './CopyField';
+export { useCopy } from './hooks/useCopy';
+export type { CopyButtonProps, CopyResult } from './types';
