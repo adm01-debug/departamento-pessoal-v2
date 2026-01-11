6df4e3756db2daab067891d6d9c85cfc6d9964ae
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:35:29 -0300

    feat(V15-084)

--- src/test/index.ts
+++ src/test/index.ts
@@ -1,1 +1,8 @@
-export*from'./utils';export*from'./setup';export*from'./factories';export*from'./mocks';
+// V15-084: src/test/index.ts
+export * from './factories';
+export * from './mocks';
+export * from './utils';
+export { renderWithProviders } from './renderWithProviders';
+export { mockApi } from './mockApi';
+export { waitForElement } from './waitForElement';
+export type { TestConfig } from './types';
