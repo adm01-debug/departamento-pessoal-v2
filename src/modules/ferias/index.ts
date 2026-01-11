b49a7790ad59cdbb7f111aa6b4143fe26e812821
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:34:57 -0300

    feat(V15-072)

--- src/modules/ferias/index.ts
+++ src/modules/ferias/index.ts
@@ -1,1 +1,6 @@
-export{FeriasList}from'./FeriasList';export{FeriasCalendar}from'./FeriasCalendar';
+// V15-072: src/modules/ferias/index.ts
+export { FeriasModule } from './FeriasModule';
+export { FeriasContainer } from './FeriasContainer';
+export { useFerias } from './hooks/useFerias';
+export * from './components';
+export * from './types';
