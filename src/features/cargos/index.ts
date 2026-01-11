1dd1819028521e7b638022a79aa2f13bd1c45db4
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:34:02 -0300

    feat(V15-057)

--- src/features/cargos/index.ts
+++ src/features/cargos/index.ts
@@ -1,1 +1,6 @@
-export*from'./api';export*from'./hooks';
+// V15-057: src/features/cargos/index.ts
+export * from './api';
+export * from './hooks';
+export * from './types';
+export { CargosProvider } from './CargosProvider';
+export { useCargosContext } from './CargosContext';
