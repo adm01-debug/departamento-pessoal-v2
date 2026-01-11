645e41958614b9255f3621e9aaef4a4c46a93c2f
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:34:58 -0300

    feat(V15-074)

--- src/modules/ponto/index.ts
+++ src/modules/ponto/index.ts
@@ -1,1 +1,6 @@
-export{PontoRegistro}from'./PontoRegistro';export{PontoEspelho}from'./PontoEspelho';
+// V15-074: src/modules/ponto/index.ts
+export { PontoModule } from './PontoModule';
+export { PontoContainer } from './PontoContainer';
+export { usePonto } from './hooks/usePonto';
+export * from './components';
+export * from './types';
