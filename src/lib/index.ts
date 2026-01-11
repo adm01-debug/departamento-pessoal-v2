8daca261cd182eefd7a8d576b418ca9f6671f1a1
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 11:16:14 -0300

    feat(V15-158)

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,19 @@
-export{api}from'./api';export{axios}from'./axios';export{queryClient,invalidateQueries,prefetchQuery,setQueryData,getQueryData}from'./queryClient';export{cn}from'./utils';
+// V15-158: src/lib/index.ts
+export * from './abbreviate';
+export * from './capitalize';
+export * from './debounce';
+export * from './deepClone';
+export * from './format';
+export * from './groupBy';
+export * from './isEmpty';
+export * from './random';
+export * from './slugify';
+export * from './throttle';
+export * from './validators';
+export * from './date';
+export * from './masks';
+export * from './currency';
+export * from './storage';
+export * from './api';
+export * from './validation';
+export * from './utils';
