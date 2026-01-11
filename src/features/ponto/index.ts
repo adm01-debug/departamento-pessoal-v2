3b617441c60354fb7a2be4bb45100c567baf23af
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:34:27 -0300

    feat(V15-067)

--- src/features/ponto/index.ts
+++ src/features/ponto/index.ts
@@ -1,1 +1,6 @@
-export*from'./api';export*from'./hooks';
+// V15-067: src/features/ponto/index.ts
+export * from './api';
+export * from './hooks';
+export * from './types';
+export { PontoProvider } from './PontoProvider';
+export { usePontoContext } from './PontoContext';
