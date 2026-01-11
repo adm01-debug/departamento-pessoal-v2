fdc44591d2b7c29bb53506ebbb7d9b485604a578
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:31:38 -0300

    feat(V15-017)

--- src/components/file/index.ts
+++ src/components/file/index.ts
@@ -1,3 +1,9 @@
-export * from './FileIcon';
-export * from './FileCard';
-export * from './FileList';
+// V15-017: src/components/file/index.ts
+export { FileUpload } from './FileUpload';
+export { FileList } from './FileList';
+export { FilePreview } from './FilePreview';
+export { FileDownload } from './FileDownload';
+export { FileIcon } from './FileIcon';
+export { FileDrop } from './FileDrop';
+export { useFile } from './hooks/useFile';
+export type { FileData, FileType } from './types';
