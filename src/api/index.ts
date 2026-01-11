f6250274138d189d0a6c57ed89572aea825aad9e
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 11:59:52 -0300

    V15-292

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,3 @@
-// V15-037: src/api/index.ts
-export { api } from './client';
-export { apiClient } from './apiClient';
-export { request, get, post, put, del } from './methods';
-export { useApi } from './hooks/useApi';
-export { ApiProvider } from './ApiProvider';
-export type { ApiConfig, ApiResponse, ApiError } from './types';
+// V15-292
+export { api, apiRequest } from './client';
+export { ENDPOINTS } from './endpoints';
