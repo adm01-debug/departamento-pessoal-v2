b7ebf26bdafd2c8f6b321aa7dcef614ca7c2e8ee
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:34:03 -0300

    feat(V15-058)

--- src/features/colaboradores/index.ts
+++ src/features/colaboradores/index.ts
@@ -1,1 +1,6 @@
-export*from'./api';export*from'./hooks';
+// V15-058: src/features/colaboradores/index.ts
+export * from './api';
+export * from './hooks';
+export * from './types';
+export { ColaboradoresProvider } from './ColaboradoresProvider';
+export { useColaboradoresContext } from './ColaboradoresContext';
