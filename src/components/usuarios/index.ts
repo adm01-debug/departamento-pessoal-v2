2d730554254891cac4d9a656c1475c3d94826fbc
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:32:49 -0300

    feat(V15-033)

--- src/components/usuarios/index.ts
+++ src/components/usuarios/index.ts
@@ -1,1 +1,8 @@
-export*from'./UsuarioCard';
+// V15-033: src/components/usuarios/index.ts
+export { UsuarioForm } from './UsuarioForm';
+export { UsuarioList } from './UsuarioList';
+export { UsuarioCard } from './UsuarioCard';
+export { UsuarioPermissoes } from './UsuarioPermissoes';
+export { UsuarioStatus } from './UsuarioStatus';
+export { useUsuarios } from './hooks/useUsuarios';
+export type { Usuario, UsuarioFormData } from './types';
