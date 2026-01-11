ae5724bb5d700a44c61c158d9bc839f95a1237d0
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:31:14 -0300

    feat(V15-011): Expand index

--- src/components/date/index.ts
+++ src/components/date/index.ts
@@ -1,3 +1,10 @@
-export * from './RelativeDate';
-export * from './FormattedDate';
-export * from './Countdown';
+// V15-011: src/components/date/index.ts
+export { DatePicker } from './DatePicker';
+export { DateRangePicker } from './DateRangePicker';
+export { DateInput } from './DateInput';
+export { DateDisplay } from './DateDisplay';
+export { DateFormatter } from './DateFormatter';
+export { Calendar } from './Calendar';
+export { TimePicker } from './TimePicker';
+export { useDate } from './hooks/useDate';
+export type { DatePickerProps, DateRange } from './types';
