529c67122f03a8a76915653c3e954ff4d68cc30b
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:33:38 -0300

    feat(V15-048)

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -1,1 +1,7 @@
-export{logger}from'./logger';
+// V15-048: src/logger/index.ts
+export { logger } from './logger';
+export { Logger } from './Logger';
+export { ConsoleLogger } from './ConsoleLogger';
+export { FileLogger } from './FileLogger';
+export { useLogger } from './hooks/useLogger';
+export type { LogLevel, LogEntry, LogConfig } from './types';
