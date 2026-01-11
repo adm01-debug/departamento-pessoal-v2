c8218a1a5f5889382944ed21125c2eefc98fe2ee
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:35:28 -0300

    feat(V15-083)

--- src/test/factories/index.ts
+++ src/test/factories/index.ts
@@ -1,1 +1,8 @@
-export*from'./colaborador';export*from'./ferias';export*from'./folha';
+// V15-083: src/test/factories/index.ts
+export { createColaborador } from './colaborador';
+export { createEmpresa } from './empresa';
+export { createFolha } from './folha';
+export { createFerias } from './ferias';
+export { createPonto } from './ponto';
+export { createUser } from './user';
+export type { FactoryOptions } from './types';
