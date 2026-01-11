76753a82b1ae9da9e0f84c643555f77288977657
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:35:00 -0300

    feat(V15-077)

--- src/providers/index.ts
+++ src/providers/index.ts
@@ -1,1 +1,7 @@
-export{AppProvider}from'./AppProvider';
+// V15-077: src/providers/index.ts
+export { AppProvider } from './AppProvider';
+export { ThemeProvider } from './ThemeProvider';
+export { AuthProvider } from './AuthProvider';
+export { QueryProvider } from './QueryProvider';
+export { NotificationProvider } from './NotificationProvider';
+export { ToastProvider } from './ToastProvider';
