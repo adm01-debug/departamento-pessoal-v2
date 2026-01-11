e30931bf88bc95bc8f14215aac27e76afbb435ef
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:32:03 -0300

    feat(V15-021)

--- src/components/lotacao/index.ts
+++ src/components/lotacao/index.ts
@@ -1,1 +1,8 @@
-export * from "./LotacaoCard";
+// V15-021: src/components/lotacao/index.ts
+export { LotacaoForm } from './LotacaoForm';
+export { LotacaoList } from './LotacaoList';
+export { LotacaoHistorico } from './LotacaoHistorico';
+export { LotacaoTree } from './LotacaoTree';
+export { TransferenciaForm } from './TransferenciaForm';
+export { useLotacao } from './hooks/useLotacao';
+export type { Lotacao, Transferencia } from './types';
