5f4e1909c17721775b4c3369d450f8b3cdd73133
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:32:50 -0300

    feat(V15-034)

--- src/adapters/index.ts
+++ src/adapters/index.ts
@@ -1,1 +1,7 @@
-export*from'./apiAdapter';export*from'./storageAdapter';
+// V15-034: src/adapters/index.ts
+export { ApiAdapter } from './ApiAdapter';
+export { StorageAdapter } from './StorageAdapter';
+export { CacheAdapter } from './CacheAdapter';
+export { EventAdapter } from './EventAdapter';
+export { LogAdapter } from './LogAdapter';
+export type { AdapterConfig, AdapterResult } from './types';
