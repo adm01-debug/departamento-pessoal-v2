f638c62d1ccaaeb85ad69dcdbd92be47865a8ab8
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:33:39 -0300

    feat(V15-050)

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,1 +1,7 @@
-export{setupAuthMiddleware}from'./authMiddleware';
+// V15-050: src/middlewares/index.ts
+export { authMiddleware } from './auth';
+export { logMiddleware } from './log';
+export { cacheMiddleware } from './cache';
+export { errorMiddleware } from './error';
+export { validationMiddleware } from './validation';
+export type { MiddlewareConfig, MiddlewareNext } from './types';
