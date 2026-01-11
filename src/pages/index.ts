f7a52fa3e14b43cbbacd7a4a2333aaf05437b112
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 13:36:31 -0300

    V15-501

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,4 +1,4 @@
-// V15-230: src/pages/index.ts
+// V15-501 - Pages Export
 export { default as LoginPage } from './LoginPage';
 export { default as DashboardPage } from './DashboardPage';
 export { default as ColaboradoresPage } from './ColaboradoresPage';
@@ -9,4 +9,6 @@ export { default as FeriasPage } from './FeriasPage';
 export { default as PontoPage } from './PontoPage';
 export { default as BeneficiosPage } from './BeneficiosPage';
 export { default as RelatoriosPage } from './RelatoriosPage';
-export { default as LoadingPage } from './LoadingPage';
+export { default as ESocialPage } from './ESocialPage';
+export { default as ConfiguracoesPage } from './ConfiguracoesPage';
+export { default as NotFoundPage } from './NotFoundPage';
