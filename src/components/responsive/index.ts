3351deb54ee37cc96dda3ed3958598c39174c88a
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:32:25 -0300

    feat(V15-027)

--- src/components/responsive/index.ts
+++ src/components/responsive/index.ts
@@ -1,2 +1,8 @@
-export * from './Responsive';
-export * from './ShowOn';
+// V15-027: src/components/responsive/index.ts
+export { Responsive } from './Responsive';
+export { MobileOnly } from './MobileOnly';
+export { DesktopOnly } from './DesktopOnly';
+export { TabletOnly } from './TabletOnly';
+export { Hidden } from './Hidden';
+export { useResponsive } from './hooks/useResponsive';
+export type { Breakpoint } from './types';
