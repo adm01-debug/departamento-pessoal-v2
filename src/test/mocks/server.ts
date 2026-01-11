ba29e3d379c1dde75fe78fec2b9d612827d37da5
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 12:36:31 -0300

    V15-359

--- src/test/mocks/server.ts
+++ src/test/mocks/server.ts
@@ -1,1 +1,4 @@
-import{setupServer}from'msw/node';import{handlers}from'./handlers';export const server=setupServer(...handlers);
+// V15-359
+import { setupServer } from 'msw/node';
+import { handlers } from './handlers';
+export const server = setupServer(...handlers);
