706d0e00ef3a01267cf8982d1e81dbac02fb15fa
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:31:36 -0300

    feat(V15-014)

--- src/components/documentos/index.ts
+++ src/components/documentos/index.ts
@@ -1,1 +1,9 @@
-export*from'./DocumentoCard';
+// V15-014: src/components/documentos/index.ts
+export { DocumentoForm } from './DocumentoForm';
+export { DocumentoList } from './DocumentoList';
+export { DocumentoUpload } from './DocumentoUpload';
+export { DocumentoViewer } from './DocumentoViewer';
+export { DocumentoPreview } from './DocumentoPreview';
+export { DocumentoDownload } from './DocumentoDownload';
+export { useDocumentos } from './hooks/useDocumentos';
+export type { Documento, TipoDocumento } from './types';
