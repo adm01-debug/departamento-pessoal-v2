2f76323abc06c5992a81ce29b180c194145da548
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 13:28:25 -0300

    V15-463

--- src/components/ui/aspect-ratio.tsx
+++ src/components/ui/aspect-ratio.tsx
@@ -1,1 +1,4 @@
-import*as AspectRatioPrimitive from'@radix-ui/react-aspect-ratio';const AspectRatio=AspectRatioPrimitive.Root;export{AspectRatio};
+// V15-463
+import * as AspectRatioPrimitive from '@radix-ui/react-aspect-ratio';
+const AspectRatio = AspectRatioPrimitive.Root;
+export { AspectRatio };
