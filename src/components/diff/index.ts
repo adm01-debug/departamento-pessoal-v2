718a19ee3e3d92040236fd0a0cdf464e0206516b
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:31:35 -0300

    feat(V15-013)

--- src/components/diff/index.ts
+++ src/components/diff/index.ts
@@ -1,1 +1,8 @@
-export * from './DiffDisplay';
+// V15-013: src/components/diff/index.ts
+export { DiffViewer } from './DiffViewer';
+export { DiffInline } from './DiffInline';
+export { DiffSplit } from './DiffSplit';
+export { DiffHighlight } from './DiffHighlight';
+export { ChangeLog } from './ChangeLog';
+export { useDiff } from './hooks/useDiff';
+export type { DiffData, DiffChange } from './types';
