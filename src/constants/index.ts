7053d3b09637754a19580d262ab798e64e4df03f
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:33:14 -0300

    feat(V15-041)

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,9 @@
-export*from'./status';export*from'./messages';export*from'./tables';
+// V15-041: src/constants/index.ts
+export * from './api';
+export * from './routes';
+export * from './status';
+export * from './messages';
+export * from './validation';
+export * from './brasil';
+export * from './esocial';
+export * from './departamentoPessoal';
