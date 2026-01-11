b6868dbd52cd6e118c5f161445090e29f4df18b4
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:33:15 -0300

    feat(V15-043)

--- src/directives/index.ts
+++ src/directives/index.ts
@@ -1,1 +1,8 @@
-export{useClickOutsideDirective}from'./useClickOutside';export{useFocusTrap}from'./useFocusTrap';
+// V15-043: src/directives/index.ts
+export { clickOutside } from './clickOutside';
+export { focus } from './focus';
+export { scroll } from './scroll';
+export { resize } from './resize';
+export { tooltip } from './tooltip';
+export { ripple } from './ripple';
+export type { DirectiveBinding } from './types';
