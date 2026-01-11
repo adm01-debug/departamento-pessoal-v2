b1048ef0cc3ca41090b1218c5010dcc9c7292328
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:35:01 -0300

    feat(V15-078)

--- src/security/index.ts
+++ src/security/index.ts
@@ -1,1 +1,7 @@
-export*from'./sanitize';export*from'./xss';
+// V15-078: src/security/index.ts
+export { encrypt, decrypt } from './crypto';
+export { hashPassword, verifyPassword } from './password';
+export { generateToken, verifyToken } from './token';
+export { sanitize } from './sanitize';
+export { validatePermission } from './permissions';
+export type { SecurityConfig } from './types';
