03daa1ff5c7e727bcfd98506e5e770dca32f67fe
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:32:02 -0300

    feat(V15-020)

--- src/components/jornada/index.ts
+++ src/components/jornada/index.ts
@@ -1,1 +1,8 @@
-export * from "./JornadaCard";
+// V15-020: src/components/jornada/index.ts
+export { JornadaForm } from './JornadaForm';
+export { JornadaList } from './JornadaList';
+export { JornadaCalendario } from './JornadaCalendario';
+export { JornadaHorarios } from './JornadaHorarios';
+export { EscalaTrabalho } from './EscalaTrabalho';
+export { useJornada } from './hooks/useJornada';
+export type { Jornada, TipoJornada } from './types';
