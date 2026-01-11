ce3ad644b655098782b6cdae51a0e9a8607a54bd
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 13:27:41 -0300

    V15-458

--- src/components/ui/collapsible.tsx
+++ src/components/ui/collapsible.tsx
@@ -1,1 +1,6 @@
-import*as CollapsiblePrimitive from'@radix-ui/react-collapsible';const Collapsible=CollapsiblePrimitive.Root;const CollapsibleTrigger=CollapsiblePrimitive.CollapsibleTrigger;const CollapsibleContent=CollapsiblePrimitive.CollapsibleContent;export{Collapsible,CollapsibleTrigger,CollapsibleContent};
+// V15-458
+import * as CollapsiblePrimitive from '@radix-ui/react-collapsible';
+const Collapsible = CollapsiblePrimitive.Root;
+const CollapsibleTrigger = CollapsiblePrimitive.CollapsibleTrigger;
+const CollapsibleContent = CollapsiblePrimitive.CollapsibleContent;
+export { Collapsible, CollapsibleTrigger, CollapsibleContent };
