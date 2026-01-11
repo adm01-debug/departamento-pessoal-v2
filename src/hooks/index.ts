c10e74a1b7d467502c2e7f715953316e542575d9
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 11:16:13 -0300

    feat(V15-157)

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,12 +1,20 @@
-// Hooks index - Re-export all hooks
-export { useDebounce, useDebouncedCallback } from "./useDebounce";
-export { useLocalStorage } from "./useLocalStorage";
-export { useClickOutside, useClickOutsideMultiple } from "./useClickOutside";
-export { useMediaQuery, useBreakpoint, useIsMobile } from "./useMediaQuery";
-export { useClipboard } from "./useClipboard";
-export { useKeyboard, useHotkey, useEscapeKey, useEnterKey } from "./useKeyboard";
-export { useAsync, useAsyncCallback } from "./useAsync";
-export { useInterval, useIntervalControl, useTimeout } from "./useInterval";
-export { usePrevious, usePreviousDistinct, useHasChanged } from "./usePrevious";
-export { useToggle, useBoolean, useDisclosure } from "./useToggle";
-export { useForm } from "./useForm";
+// V15-157: src/hooks/index.ts
+export * from './useApi';
+export * from './useBattery';
+export * from './useCamera';
+export * from './useClipboard';
+export * from './useNetwork';
+export * from './useLocalStorage';
+export * from './useDebounce';
+export * from './useThrottle';
+export * from './useClickOutside';
+export * from './useMediaQuery';
+export * from './useToggle';
+export * from './usePrevious';
+export * from './useAsync';
+export * from './useFetch';
+export * from './useForm';
+export * from './useIntersectionObserver';
+export * from './useEventListener';
+export * from './useKeyPress';
+export * from './useWindowSize';
