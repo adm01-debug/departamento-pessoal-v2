046363d8a0aaaf58958a3a5ff544437070578260
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:32:50 -0300

    feat(V15-035)

--- src/analytics/index.ts
+++ src/analytics/index.ts
@@ -1,1 +1,7 @@
-export{analytics}from'./tracker';
+// V15-035: src/analytics/index.ts
+export { Analytics } from './Analytics';
+export { trackEvent } from './trackEvent';
+export { trackPage } from './trackPage';
+export { trackUser } from './trackUser';
+export { useAnalytics } from './hooks/useAnalytics';
+export type { AnalyticsEvent, AnalyticsConfig } from './types';
