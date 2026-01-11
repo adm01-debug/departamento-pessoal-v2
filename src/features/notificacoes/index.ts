ed7036a9382bb36ce77067c220cd7d3b7a690fe9
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:34:26 -0300

    feat(V15-066)

--- src/features/notificacoes/index.ts
+++ src/features/notificacoes/index.ts
@@ -1,1 +1,6 @@
-export*from'./api';export*from'./hooks';
+// V15-066: src/features/notificacoes/index.ts
+export * from './api';
+export * from './hooks';
+export * from './types';
+export { NotificacoesProvider } from './NotificacoesProvider';
+export { useNotificacoesContext } from './NotificacoesContext';
