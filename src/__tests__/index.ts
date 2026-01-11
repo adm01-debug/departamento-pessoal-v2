530988c1780a9829d2e82e62527a71a4b76c62fb
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:35:31 -0300

    feat(V15-087)

--- src/__tests__/index.ts
+++ src/__tests__/index.ts
@@ -1,1 +1,7 @@
-export*from'./utils';export*from'./setup';export*from'./mocks/handlers';
+// V15-087: src/__tests__/index.ts
+export * from './setup';
+export * from './utils';
+export * from './mocks';
+export { customRender as render } from './customRender';
+export { createTestStore } from './createTestStore';
+export type { TestContext } from './types';
