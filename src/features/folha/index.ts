da3a3555baa9cb6e1d14b954d39cb00ff6cb75bb
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 13:36:36 -0300

    V15-507

--- src/features/folha/index.ts
+++ src/features/folha/index.ts
@@ -1,6 +1,3 @@
-// V15-065: src/features/folha/index.ts
-export * from './api';
-export * from './hooks';
-export * from './types';
-export { FolhaProvider } from './FolhaProvider';
-export { useFolhaContext } from './FolhaContext';
+// V15-507
+export { FolhaResumo } from './FolhaResumo';
+export { FolhaCalcular } from './FolhaCalcular';
