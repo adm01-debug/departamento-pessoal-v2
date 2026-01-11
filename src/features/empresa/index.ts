b49968ac7616271f77702af3f3bee7b7365d5324
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:34:24 -0300

    feat(V15-062)

--- src/features/empresa/index.ts
+++ src/features/empresa/index.ts
@@ -1,1 +1,6 @@
-export*from'./api';export*from'./hooks';
+// V15-062: src/features/empresa/index.ts
+export * from './api';
+export * from './hooks';
+export * from './types';
+export { EmpresaProvider } from './EmpresaProvider';
+export { useEmpresaContext } from './EmpresaContext';
