963cd24577edb6de8773fc92ddb115d49a2a68a7
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:33:15 -0300

    feat(V15-042)

--- src/decorators/index.ts
+++ src/decorators/index.ts
@@ -1,1 +1,8 @@
-export{withAuth,withPermission}from'./withAuth';
+// V15-042: src/decorators/index.ts
+export { Log } from './Log';
+export { Memoize } from './Memoize';
+export { Debounce } from './Debounce';
+export { Throttle } from './Throttle';
+export { Validate } from './Validate';
+export { Cached } from './Cached';
+export type { DecoratorConfig } from './types';
