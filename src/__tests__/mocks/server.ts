b0231b3ce5da0d3b9b04d6417b80904f6219510d
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:36:12 -0300

    feat(V15-088)

--- src/__tests__/mocks/server.ts
+++ src/__tests__/mocks/server.ts
@@ -1,1 +1,19 @@
-export const server={listen:()=>{},close:()=>{},resetHandlers:()=>{}};
+// V15-088: src/__tests__/mocks/server.ts
+import { setupServer } from 'msw/node';
+import { handlers } from './handlers';
+
+export const server = setupServer(...handlers);
+
+export const setupTestServer = () => {
+  beforeAll(() => server.listen({ onUnhandledRequest: 'error' }));
+  afterEach(() => server.resetHandlers());
+  afterAll(() => server.close());
+};
+
+export const addHandler = (handler: any) => {
+  server.use(handler);
+};
+
+export const resetHandlers = () => {
+  server.resetHandlers();
+};
