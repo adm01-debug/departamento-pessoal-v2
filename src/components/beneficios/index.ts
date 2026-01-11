d4ba70bfedf44ca4d5af6f4d3ae58651b44093e7
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:30:48 -0300

    feat(V15-004): Expand index exports

--- src/components/beneficios/index.ts
+++ src/components/beneficios/index.ts
@@ -1,1 +1,9 @@
-export{BeneficioCard}from'./BeneficioCard';export{BeneficioForm}from'./BeneficioForm';
+// V15-004: src/components/beneficios/index.ts
+export { BeneficioForm } from './BeneficioForm';
+export { BeneficioList } from './BeneficioList';
+export { BeneficioCard } from './BeneficioCard';
+export { BeneficioDetails } from './BeneficioDetails';
+export { BeneficioSelector } from './BeneficioSelector';
+export { BeneficioValor } from './BeneficioValor';
+export { useBeneficios } from './hooks/useBeneficios';
+export type { Beneficio, BeneficioFormData } from './types';
