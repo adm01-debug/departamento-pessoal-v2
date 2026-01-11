bf50106559d9d5346eb04bf6530abc4b29597f84
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:32:05 -0300

    feat(V15-024)

--- src/components/performance/index.ts
+++ src/components/performance/index.ts
@@ -1,2 +1,7 @@
-export * from './LazyImage';
-export * from './LazyComponent';
+// V15-024: src/components/performance/index.ts
+export { PerformanceChart } from './PerformanceChart';
+export { PerformanceMetrics } from './PerformanceMetrics';
+export { PerformanceIndicator } from './PerformanceIndicator';
+export { PerformanceGauge } from './PerformanceGauge';
+export { LoadTime } from './LoadTime';
+export type { PerformanceData } from './types';
