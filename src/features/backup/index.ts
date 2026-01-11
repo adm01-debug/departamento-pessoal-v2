5cb76a3e80ae46a0da2e22cc0eb7841884b24903
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:34:01 -0300

    feat(V15-055)

--- src/features/backup/index.ts
+++ src/features/backup/index.ts
@@ -1,1 +1,6 @@
-export*from'./api';export*from'./hooks';
+// V15-055: src/features/backup/index.ts
+export * from './api';
+export * from './hooks';
+export * from './types';
+export { BackupProvider } from './BackupProvider';
+export { useBackupContext } from './BackupContext';
