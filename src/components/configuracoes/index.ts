8ac0bf7686fb70da827480e186cf47513259fefc
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:31:11 -0300

    feat(V15-007): Expand index

--- src/components/configuracoes/index.ts
+++ src/components/configuracoes/index.ts
@@ -1,1 +1,9 @@
-export*from'./ConfiguracaoSection';
+// V15-007: src/components/configuracoes/index.ts
+export { ConfigForm } from './ConfigForm';
+export { ConfigPanel } from './ConfigPanel';
+export { ConfigSection } from './ConfigSection';
+export { ConfigToggle } from './ConfigToggle';
+export { SystemConfig } from './SystemConfig';
+export { EmpresaConfig } from './EmpresaConfig';
+export { useConfig } from './hooks/useConfig';
+export type { ConfigData, ConfigOption } from './types';
