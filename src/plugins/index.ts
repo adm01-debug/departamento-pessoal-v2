ffca21f04b917ec93d67a19a63274052230e6b2b
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:35:00 -0300

    feat(V15-076)

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,1 +1,7 @@
-export*from'./masks';export{dayjs,formatDate,formatDateTime,fromNow,isBetweenDates}from'./dayjs';
+// V15-076: src/plugins/index.ts
+export { AnalyticsPlugin } from './analytics';
+export { LoggerPlugin } from './logger';
+export { CachePlugin } from './cache';
+export { ValidationPlugin } from './validation';
+export { registerPlugin } from './registry';
+export type { Plugin, PluginConfig } from './types';
