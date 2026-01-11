b4c750d446f46a8d093bfb6fea36cddca039d6d9
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:30:49 -0300

    feat(V15-005): Expand index exports

--- src/components/cargos/index.ts
+++ src/components/cargos/index.ts
@@ -1,1 +1,9 @@
-export*from'./CargoCard';
+// V15-005: src/components/cargos/index.ts
+export { CargoForm } from './CargoForm';
+export { CargoList } from './CargoList';
+export { CargoSelect } from './CargoSelect';
+export { CargoDetails } from './CargoDetails';
+export { CargoHierarquia } from './CargoHierarquia';
+export { CargoCBO } from './CargoCBO';
+export { useCargos } from './hooks/useCargos';
+export type { Cargo, CargoFormData, NivelCargo } from './types';
