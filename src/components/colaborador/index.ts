5ac49c46a0eee20f9bc2965b402d799f10a7e937
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:30:50 -0300

    feat(V15-006): Expand index exports

--- src/components/colaborador/index.ts
+++ src/components/colaborador/index.ts
@@ -1,1 +1,9 @@
-export*from'./ColaboradorCard';export*from'./ColaboradorList';
+// V15-006: src/components/colaborador/index.ts
+export { ColaboradorForm } from './ColaboradorForm';
+export { ColaboradorList } from './ColaboradorList';
+export { ColaboradorCard } from './ColaboradorCard';
+export { ColaboradorDetails } from './ColaboradorDetails';
+export { ColaboradorSearch } from './ColaboradorSearch';
+export { ColaboradorSelect } from './ColaboradorSelect';
+export { useColaborador } from './hooks/useColaborador';
+export type { Colaborador, ColaboradorFormData } from './types';
