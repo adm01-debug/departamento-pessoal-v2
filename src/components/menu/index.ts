5bc79df9b006909d52a6333ae422a7fa911e0d06
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:32:04 -0300

    feat(V15-022)

--- src/components/menu/index.ts
+++ src/components/menu/index.ts
@@ -1,3 +1,9 @@
-export * from './MenuItem';
-export * from './MenuGroup';
-export * from './MenuDivider';
+// V15-022: src/components/menu/index.ts
+export { Menu } from './Menu';
+export { MenuItem } from './MenuItem';
+export { MenuGroup } from './MenuGroup';
+export { MenuDivider } from './MenuDivider';
+export { ContextMenu } from './ContextMenu';
+export { DropdownMenu } from './DropdownMenu';
+export { NavigationMenu } from './NavigationMenu';
+export type { MenuProps, MenuItemProps } from './types';
