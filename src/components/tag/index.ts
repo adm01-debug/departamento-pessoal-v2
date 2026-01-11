3fc76cd6fd37fc671ba3211251c387a7e65ddb18
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:32:27 -0300

    feat(V15-030)

--- src/components/tag/index.ts
+++ src/components/tag/index.ts
@@ -1,2 +1,8 @@
-export * from './Tag';
-export * from './TagList';
+// V15-030: src/components/tag/index.ts
+export { Tag } from './Tag';
+export { TagList } from './TagList';
+export { TagInput } from './TagInput';
+export { TagGroup } from './TagGroup';
+export { Badge } from './Badge';
+export { Chip } from './Chip';
+export type { TagProps, TagVariant } from './types';
