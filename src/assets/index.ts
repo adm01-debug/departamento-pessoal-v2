035cb4eef8d8f1ba90686810bea85177c65f3c6d
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:33:12 -0300

    feat(V15-038)

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,1 +1,7 @@
-export{default as Logo}from'./logo.svg';
+// V15-038: src/assets/index.ts
+export { logo } from './images/logo';
+export { icons } from './icons';
+export { illustrations } from './illustrations';
+export { backgrounds } from './backgrounds';
+export { placeholders } from './placeholders';
+export type { AssetType, IconName } from './types';
