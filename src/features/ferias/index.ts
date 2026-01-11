86fb53ba96f21a99dbb1ebb571133d106b75621b
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:34:25 -0300

    feat(V15-064)

--- src/features/ferias/index.ts
+++ src/features/ferias/index.ts
@@ -1,1 +1,6 @@
-export*from'./api';export*from'./hooks';
+// V15-064: src/features/ferias/index.ts
+export * from './api';
+export * from './hooks';
+export * from './types';
+export { FeriasProvider } from './FeriasProvider';
+export { useFeriasContext } from './FeriasContext';
