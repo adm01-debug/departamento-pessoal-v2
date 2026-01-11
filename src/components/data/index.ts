3ed770fdf795b25c9adcce61274a1d478d3dce63
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:31:13 -0300

    feat(V15-010): Expand index

--- src/components/data/index.ts
+++ src/components/data/index.ts
@@ -1,3 +1,10 @@
-export * from './DataLabel';
-export * from './DataSection';
-export * from './DataEmpty';
+// V15-010: src/components/data/index.ts
+export { DataTable } from './DataTable';
+export { DataGrid } from './DataGrid';
+export { DataList } from './DataList';
+export { DataCard } from './DataCard';
+export { DataView } from './DataView';
+export { DataExport } from './DataExport';
+export { DataImport } from './DataImport';
+export { useData } from './hooks/useData';
+export type { DataTableProps, DataColumn } from './types';
