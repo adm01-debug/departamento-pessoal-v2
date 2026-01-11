bcd89bbef765da9b2c2b7b286510fd8d61b74ebf
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:30:47 -0300

    feat(V15-003): Expand index exports

--- src/components/avatar/index.ts
+++ src/components/avatar/index.ts
@@ -1,2 +1,8 @@
-export * from './UserAvatar';
-export * from './AvatarGroup';
+// V15-003: src/components/avatar/index.ts
+export { Avatar } from './Avatar';
+export { AvatarGroup } from './AvatarGroup';
+export { AvatarUpload } from './AvatarUpload';
+export { AvatarFallback } from './AvatarFallback';
+export { AvatarBadge } from './AvatarBadge';
+export { UserAvatar } from './UserAvatar';
+export type { AvatarProps, AvatarSize } from './types';
