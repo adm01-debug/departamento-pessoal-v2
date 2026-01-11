9b788c2dd589e3a176374c43fe7cabc77357cb10
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:35:26 -0300

    feat(V15-079)

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,1 +1,6 @@
-export*from'./slices/authSlice';export*from'./slices/uiSlice';
+// V15-079: src/store/index.ts
+export { store } from './store';
+export { useStore } from './hooks/useStore';
+export { createStore } from './createStore';
+export { Provider } from './Provider';
+export type { StoreState, StoreAction, StoreDispatch } from './types';
