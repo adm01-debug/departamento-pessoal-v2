86d0f95cc6b954b4e3544fa444aad00c45918b89
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:32:24 -0300

    feat(V15-026)

--- src/components/rating/index.ts
+++ src/components/rating/index.ts
@@ -1,2 +1,7 @@
-export * from './StarRating';
-export * from './ThumbsRating';
+// V15-026: src/components/rating/index.ts
+export { Rating } from './Rating';
+export { RatingStars } from './RatingStars';
+export { RatingDisplay } from './RatingDisplay';
+export { RatingInput } from './RatingInput';
+export { FeedbackRating } from './FeedbackRating';
+export type { RatingProps, RatingValue } from './types';
