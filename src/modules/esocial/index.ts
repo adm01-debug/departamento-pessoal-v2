63233ef57b9dc074a7bb3e9c3712ba439129d66c
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:34:56 -0300

    feat(V15-071)

--- src/modules/esocial/index.ts
+++ src/modules/esocial/index.ts
@@ -1,1 +1,6 @@
-export{EventosList}from'./EventosList';
+// V15-071: src/modules/esocial/index.ts
+export { ESocialModule } from './ESocialModule';
+export { ESocialContainer } from './ESocialContainer';
+export { useESocial } from './hooks/useESocial';
+export * from './components';
+export * from './types';
