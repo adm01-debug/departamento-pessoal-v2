f112f9fc66b93498d566b67a7e1579d2a808bcbd
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:32:48 -0300

    feat(V15-032)

--- src/components/user/index.ts
+++ src/components/user/index.ts
@@ -1,2 +1,9 @@
-export * from './UserInfo';
-export * from './UserMenu';
+// V15-032: src/components/user/index.ts
+export { UserCard } from './UserCard';
+export { UserProfile } from './UserProfile';
+export { UserAvatar } from './UserAvatar';
+export { UserMenu } from './UserMenu';
+export { UserList } from './UserList';
+export { UserSelect } from './UserSelect';
+export { useUser } from './hooks/useUser';
+export type { User, UserRole } from './types';
