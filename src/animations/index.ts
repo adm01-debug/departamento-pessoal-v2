52f5e139c255c240f75eec21adbfb2b1e13e7a36
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:32:51 -0300

    feat(V15-036)

--- src/animations/index.ts
+++ src/animations/index.ts
@@ -1,1 +1,7 @@
-export*from'./fadeIn';export*from'./scale';export*from'./slide';
+// V15-036: src/animations/index.ts
+export { fadeIn, fadeOut } from './fade';
+export { slideIn, slideOut } from './slide';
+export { scaleIn, scaleOut } from './scale';
+export { bounce, shake } from './effects';
+export { useAnimation } from './hooks/useAnimation';
+export type { AnimationConfig, AnimationType } from './types';
