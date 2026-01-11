63c27812c6420333d231fe5a3bd9d5e0497c981e
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:33:16 -0300

    feat(V15-044)

--- src/dto/index.ts
+++ src/dto/index.ts
@@ -1,1 +1,8 @@
-export*from'./colaborador.dto';export*from'./ferias.dto';export*from'./folha.dto';
+// V15-044: src/dto/index.ts
+export { ColaboradorDTO } from './ColaboradorDTO';
+export { EmpresaDTO } from './EmpresaDTO';
+export { FolhaDTO } from './FolhaDTO';
+export { FeriasDTO } from './FeriasDTO';
+export { PontoDTO } from './PontoDTO';
+export { BeneficioDTO } from './BeneficioDTO';
+export type { DTOBase, DTOMapper } from './types';
