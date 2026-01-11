2b2ec5b608e1e7de9ce6ccd133e224e5aada9e61
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 12:36:32 -0300

    V15-360

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,1 +1,9 @@
-import{defineConfig}from'vitest/config';import react from'@vitejs/plugin-react';import path from'path';export default defineConfig({plugins:[react()],test:{environment:'jsdom',globals:true,setupFiles:['./src/setupTests.ts']},resolve:{alias:{'@':path.resolve(__dirname,'./src')}}});
+// V15-360
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+export default defineConfig({
+  plugins: [react()],
+  test: { globals: true, environment: 'jsdom', setupFiles: ['./src/test/setup.ts'], include: ['src/**/*.{test,spec}.{ts,tsx}'], coverage: { reporter: ['text', 'json', 'html'], exclude: ['node_modules/', 'src/test/'] } },
+  resolve: { alias: { '@': path.resolve(__dirname, './src') } },
+});
