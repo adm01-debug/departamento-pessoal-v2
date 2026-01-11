a02e66ce5c725ddbabd8edd8d1752ed052417f79
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 10:35:28 -0300

    feat(V15-082)

--- src/templates/pdf/index.ts
+++ src/templates/pdf/index.ts
@@ -1,1 +1,8 @@
-export{holeriteTemplate}from'./holerite';
+// V15-082: src/templates/pdf/index.ts
+export { HoleritePDF } from './HoleritePDF';
+export { ContratoPDF } from './ContratoPDF';
+export { RescisaoPDF } from './RescisaoPDF';
+export { FeriasPDF } from './FeriasPDF';
+export { RelatorioPDF } from './RelatorioPDF';
+export { generatePDF } from './generator';
+export type { PDFConfig, PDFData } from './types';
