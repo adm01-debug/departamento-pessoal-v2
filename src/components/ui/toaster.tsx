bbe485878755f90c4ffb7d38c20ab7c7321465f9
adm01-debug <adm01@promobrindes.com.br>
2026-01-11 12:38:15 -0300

    V15-380

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -1,1 +1,14 @@
-import{useToast}from'@/hooks/useToast';import{Toast,ToastClose,ToastDescription,ToastProvider,ToastTitle,ToastViewport}from'./toast';export function Toaster(){const{toasts}=useToast();return(<ToastProvider>{toasts.map(function({id,title,description,action,...props}){return(<Toast key={id}{...props}><div className="grid gap-1">{title&&<ToastTitle>{title}</ToastTitle>}{description&&<ToastDescription>{description}</ToastDescription>}</div>{action}<ToastClose/></Toast>);})}<ToastViewport/></ToastProvider>);}
+// V15-380
+import { Toast, ToastClose, ToastDescription, ToastProvider, ToastTitle, ToastViewport } from '@/components/ui/toast';
+import { useToast } from '@/hooks/useToast';
+export function Toaster() {
+  const { toasts } = useToast();
+  return (
+    <ToastProvider>
+      {toasts.map(({ id, title, description, action, ...props }) => (
+        <Toast key={id} {...props}><div className="grid gap-1">{title && <ToastTitle>{title}</ToastTitle>}{description && <ToastDescription>{description}</ToastDescription>}</div>{action}<ToastClose /></Toast>
+      ))}
+      <ToastViewport />
+    </ToastProvider>
+  );
+}
