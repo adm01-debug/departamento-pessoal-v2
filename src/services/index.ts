697e73a04e65fd371246c4995a565fb481ac6784
adm01-debug <adm01@promobrindes.com.br>
2026-01-12 18:30:56 -0300

    V17.3-FIX: Atualiza services/index.ts com .real.ts exports

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,22 +1,84 @@
-// V15-400 - Services Index
-export { authService } from './authService';
-export { colaboradorService } from './colaboradorService';
-export { empresaService } from './empresaService';
-export { folhaService } from './folhaService';
-export { feriasService } from './feriasService';
-export { pontoService } from './pontoService';
-export { beneficioService } from './beneficioService';
-export { documentoService } from './documentoService';
-export { relatorioService } from './relatorioService';
-export { departamentoService } from './departamentoService';
-export { cargoService } from './cargoService';
-export { feriadoService } from './feriadoService';
-export { dependenteService } from './dependenteService';
-export { afastamentoService } from './afastamentoService';
-export { rubricaService } from './rubricaService';
-export { bancoHorasService } from './bancoHorasService';
-export { esocialService } from './esocialService';
-export { auditoriaService } from './auditoriaService';
-export { backupService } from './backupService';
-export { usuarioService } from './usuarioService';
-export { sindicatoService } from './sindicatoService';
+// V17.3: Index de Services - Real Implementations
+// Core
+export { afastamentoServiceReal } from "./afastamentoService.real";
+export { admissaoServiceReal } from "./admissaoService.real";
+export { demissaoServiceReal } from "./demissaoService.real";
+export { rescisaoServiceReal } from "./rescisaoService.real";
+export { contratoServiceReal } from "./contratoService.real";
+export { dependenteServiceReal } from "./dependenteService.real";
+export { documentoServiceReal } from "./documentoService.real";
+export { bancoHorasServiceReal } from "./bancoHorasService.real";
+export { horasExtrasServiceReal } from "./horasExtrasService.real";
+export { rubricaServiceReal } from "./rubricaService.real";
+export { jornadaServiceReal } from "./jornadaService.real";
+export { lotacaoServiceReal } from "./lotacaoService.real";
+
+// Folha
+export { folhaPagamentoServiceReal } from "./folhaPagamentoService.real";
+export { inssServiceReal } from "./inssService.real";
+export { irrfServiceReal } from "./irrfService.real";
+export { fgtsDigitalServiceReal } from "./fgtsDigitalService.real";
+export { decimo13ServiceReal } from "./decimo13Service.real";
+export { plrServiceReal } from "./plrService.real";
+export { encargosServiceReal } from "./encargosService.real";
+export { provisoesServiceReal } from "./provisoesService.real";
+export { guiasServiceReal } from "./guiasService.real";
+export { holeriteServiceReal } from "./holeriteService.real";
+
+// Governo
+export { esocialServiceReal } from "./esocialService.real";
+export { sefipServiceReal } from "./sefipService.real";
+export { cagedServiceReal } from "./cagedService.real";
+export { raisServiceReal } from "./raisService.real";
+export { dirfServiceReal } from "./dirfService.real";
+export { dctfwebServiceReal } from "./dctfwebService.real";
+export { reinfServiceReal } from "./reinfService.real";
+
+// Relatórios
+export { relatorioServiceReal } from "./relatorioService.real";
+export { exportServiceReal } from "./exportService.real";
+export { importServiceReal } from "./importService.real";
+export { dashboardServiceReal } from "./dashboardService.real";
+export { analyticsServiceReal } from "./analyticsService.real";
+export { kpiServiceReal } from "./kpiService.real";
+
+// Comunicação
+export { emailServiceReal } from "./emailService.real";
+export { smsServiceReal } from "./smsService.real";
+export { whatsappServiceReal } from "./whatsappService.real";
+export { pushServiceReal } from "./pushService.real";
+export { notificacaoServiceReal } from "./notificacaoService.real";
+
+// Segurança
+export { usuarioServiceReal } from "./usuarioService.real";
+export { permissaoServiceReal } from "./permissaoService.real";
+export { logServiceReal } from "./logService.real";
+export { backupServiceReal } from "./backupService.real";
+export { configServiceReal } from "./configService.real";
+export { auditServiceReal } from "./auditService.real";
+
+// Auxiliares
+export { healthServiceReal } from "./healthService.real";
+export { cacheServiceReal } from "./cacheService.real";
+export { storageServiceReal } from "./storageService.real";
+export { templateServiceReal } from "./templateService.real";
+export { webhookServiceReal } from "./webhookService.real";
+
+// Tabelas
+export { tabelaINSSServiceReal } from "./tabelaINSSService.real";
+export { tabelaIRRFServiceReal } from "./tabelaIRRFService.real";
+export { tabelaSalarioFamiliaServiceReal } from "./tabelaSalarioFamiliaService.real";
+export { cboServiceReal } from "./cboService.real";
+export { cnaeServiceReal } from "./cnaeService.real";
+export { municipioServiceReal } from "./municipioService.real";
+export { bancoServiceReal } from "./bancoService.real";
+export { dominioServiceReal } from "./dominioService.real";
+
+// Legacy exports (mantidos para compatibilidade)
+export { authService } from "./authService";
+export { colaboradorService } from "./colaboradorService";
+export { empresaService } from "./empresaService";
+export { folhaService } from "./folhaService";
+export { feriasService } from "./feriasService";
+export { pontoService } from "./pontoService";
+export { beneficioService } from "./beneficioService";
