f8a3bdafec298bc99544997bd633d3bfe99f923c
adm01-debug <adm01@promobrindes.com.br>
2026-01-12 18:31:24 -0300

    V17.3-FIX: Atualiza hooks/index.ts com .real.ts exports

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,12 +1,65 @@
-// V15-455
-export { useDebounce } from './useDebounce';
-export { useLocalStorage } from './useLocalStorage';
-export { useMediaQuery } from './useMediaQuery';
-export { useOnClickOutside } from './useOnClickOutside';
-export { usePagination } from './usePagination';
-export { useSort } from './useSort';
-export { useFilter } from './useFilter';
-export { useSelection } from './useSelection';
-export { useClipboard } from './useClipboard';
-export { useGeolocation } from './useGeolocation';
-export { useToast } from './useToast';
+// V17.3: Index de Hooks - Real Implementations
+// Core
+export { useAfastamentosReal } from "./useAfastamentos.real";
+export { useAdmissaoReal } from "./useAdmissao.real";
+export { useDemissaoReal } from "./useDemissao.real";
+export { useRescisaoReal } from "./useRescisao.real";
+export { useBancoHorasReal } from "./useBancoHoras.real";
+export { useHorasExtrasReal } from "./useHorasExtras.real";
+export { useDependentesReal } from "./useDependentes.real";
+export { useDocumentosReal } from "./useDocumentos.real";
+export { useContratosReal } from "./useContratos.real";
+export { useJornadasReal } from "./useJornadas.real";
+
+// Folha
+export { useFolhaPagamentoReal } from "./useFolhaPagamento.real";
+export { useHoleriteReal } from "./useHolerite.real";
+export { useEncargosReal } from "./useEncargos.real";
+export { useGuiasReal } from "./useGuias.real";
+export { useProvisoesReal } from "./useProvisoes.real";
+export { useRubricasReal } from "./useRubricas.real";
+export { useDecimo13Real } from "./useDecimo13.real";
+export { usePLRReal } from "./usePLR.real";
+export { useBeneficiosReal } from "./useBeneficios.real";
+export { useValesReal } from "./useVales.real";
+
+// eSocial
+export { useESocialReal } from "./useESocial.real";
+export { useESocialEnvioReal } from "./useESocialEnvio.real";
+export { useESocialConsultaReal } from "./useESocialConsulta.real";
+
+// Relatórios
+export { useRelatoriosReal } from "./useRelatorios.real";
+export { useExportReal } from "./useExport.real";
+export { useDashboardReal } from "./useDashboard.real";
+export { useIndicadoresReal } from "./useIndicadores.real";
+
+// UI/Sistema
+export { useNotificacoesReal } from "./useNotificacoes.real";
+export { usePermissoesReal } from "./usePermissoes.real";
+export { useUsuariosReal } from "./useUsuarios.real";
+export { useBackupReal } from "./useBackup.real";
+export { useConfigReal } from "./useConfig.real";
+export { useLogsReal } from "./useLogs.real";
+export { useHealthReal } from "./useHealth.real";
+
+// Tabelas/Auxiliares
+export { useLotacoesReal } from "./useLotacoes.real";
+export { useSindicatosReal } from "./useSindicatos.real";
+export { useFeriadosReal } from "./useFeriados.real";
+export { useEscalasReal } from "./useEscalas.real";
+export { useTurnosReal } from "./useTurnos.real";
+export { useFeriasReal } from "./useFerias.real";
+export { useColaboradoresReal } from "./useColaboradores.real";
+
+// Legacy (compatibilidade)
+export { useDebounce } from "./useDebounce";
+export { useLocalStorage } from "./useLocalStorage";
+export { useMediaQuery } from "./useMediaQuery";
+export { useOnClickOutside } from "./useOnClickOutside";
+export { usePagination } from "./usePagination";
+export { useSort } from "./useSort";
+export { useFilter } from "./useFilter";
+export { useSelection } from "./useSelection";
+export { useClipboard } from "./useClipboard";
+export { useToast } from "./useToast";
