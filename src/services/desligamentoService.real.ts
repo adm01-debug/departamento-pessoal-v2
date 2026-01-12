5cd84b8992b8e35ee3eda6e79695b3f49894c38f
adm01-debug <adm01@promobrindes.com.br>
2026-01-12 07:37:08 -0300

    V17-S094: DesligamentoService Real

---
+++ src/services/desligamentoService.real.ts
@@ -0,0 +1,4 @@
+// V17-S094: DesligamentoService Real - Alias para DemissaoService
+import { demissaoServiceReal } from './demissaoService.real';
+export const desligamentoServiceReal = demissaoServiceReal;
+export default desligamentoServiceReal;
