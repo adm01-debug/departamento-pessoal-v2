0264b44f947c8222ff929cc7bf4f7cad28807366
adm01-debug <adm01@promobrindes.com.br>
2026-01-12 18:31:45 -0300

    V17.3-FIX: Atualiza constants/index.ts com tabelas 2025

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,19 @@
-// V15-289
-export * from './status';
-export * from './tipos';
-export * from './tables';
+// V17.3: Index de Constantes - Completo
+// Tabelas Trabalhistas 2025 (PRIORIDADE)
+export * from "./tabelas.constants";
+export * from "./esocial.constants";
+export * from "./status.constants";
+
+// Outros
+export * from "./status";
+export * from "./tipos";
+export * from "./tables";
+export * from "./bancos";
+export * from "./cbo";
+export * from "./cnae";
+export * from "./estados";
+export * from "./feriados";
+export * from "./messages";
+export * from "./routes";
+export * from "./permissions";
+export * from "./queryKeys";
