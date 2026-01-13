54a9567038042f0036f25d953878979e7acff40c
adm01-debug <adm01@promobrindes.com.br>
2026-01-13 09:24:01 -0300

    QA-FIX: src/guards/index.ts

---
+++ src/guards/index.ts
@@ -0,0 +1,5 @@
+// Guards Index
+export { AuthGuard } from "./AuthGuard";
+export { RoleGuard } from "./RoleGuard";
+export { PermissionGuard } from "./PermissionGuard";
+export { AdminGuard } from "./AdminGuard";
