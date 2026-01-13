b8cce6a23f222b30fdf87cb14f8f334e8eaa43b6
adm01-debug <adm01@promobrindes.com.br>
2026-01-13 09:24:02 -0300

    QA-FIX: src/hoc/index.ts

---
+++ src/hoc/index.ts
@@ -0,0 +1,4 @@
+// HOC Index - Higher Order Components
+export { withMemo } from "./withMemo";
+export { default as withMemoDefault } from "./withMemo";
+export type { MemoOptions } from "./withMemo";
