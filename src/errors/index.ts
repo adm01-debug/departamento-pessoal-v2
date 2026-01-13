b262b982c0489d35557dd83edf0b3a6230ff804f
adm01-debug <adm01@promobrindes.com.br>
2026-01-13 09:24:00 -0300

    QA-FIX: src/errors/index.ts

---
+++ src/errors/index.ts
@@ -0,0 +1,4 @@
+// Errors Index
+export { ErrorBoundary } from "./ErrorBoundary";
+export { AppError, ValidationError, AuthError, NetworkError } from "./AppError";
+export type { ErrorType, ErrorSeverity } from "./types";
