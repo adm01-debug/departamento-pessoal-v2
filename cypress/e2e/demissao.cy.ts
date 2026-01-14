f54c05cddacdab32a0ea15c32ab6936bdfacb272
adm01-debug <adm01@promobrindes.com.br>
2026-01-13 21:34:49 -0300

    V18:demissao.cy.ts

---
+++ cypress/e2e/demissao.cy.ts
@@ -0,0 +1,1 @@
+describe('demissaoE2E',()=>{beforeEach(()=>cy.visit('/'));it('load',()=>cy.get('body').should('exist'));it('nav',()=>cy.get('nav').should('be.visible'));it('content',()=>cy.get('main').should('exist'))});
