91258fe8662256a7aa72c4a1bb6cda56a0290d74
adm01-debug <adm01@promobrindes.com.br>
2026-01-17 11:44:14 -0300

    V18-CLEAN: Remove test

--- src/components/assinatura/__tests__/AssinaturaCanvas.test.tsx
+++
@@ -1,3 +0,0 @@
-import { describe, it, expect } from 'vitest';
-import { AssinaturaCanvas } from '../AssinaturaCanvas';
-describe('AssinaturaCanvas', () => { it('defined', () => { expect(AssinaturaCanvas).toBeDefined(); }); });
