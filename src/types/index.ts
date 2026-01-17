f17672c5e62c9de8b556fdf8574aa58f311cbd8a
adm01-debug <adm01@promobrindes.com.br>
2026-01-17 10:32:39 -0300

    V18-FIX: Expandir types index com todos os tipos

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,10 +1,25 @@
-// V15-440
+// V18: Types Index - Exportações Centralizadas
+
+// Core Types
 export * from './colaborador';
 export * from './empresa';
 export * from './folha';
 export * from './ferias';
 export * from './ponto';
 export * from './beneficio';
+
+// Documentos e Relatórios
 export * from './documento';
 export * from './relatorio';
+
+// Comum
 export * from './common';
+
+// Adicionais (se existirem)
+export * from './admissao';
+export * from './afastamento';
+export * from './cargo';
+export * from './contrato';
+
+// Re-export de tipos do database
+export type { Database, Json } from './database';
