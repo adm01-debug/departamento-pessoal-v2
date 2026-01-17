23633d7807c9f13c0f332a8badcd4c7ea4f4d6d6
adm01-debug <adm01@promobrindes.com.br>
2026-01-17 10:57:28 -0300

    V18-FIX: Expandir utils index com todas exportações

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,28 @@
-// V15-386
+// V18: Utils Index - Exportações Centralizadas
+
+// Core Utils
 export { cn } from './cn';
 export { storage } from './storage';
 export * from './download';
+
+// Formatters
+export * from './formatters';
+
+// Validadores
+export * from './cpfValidator';
+export * from './cnpjValidator';
+export * from './pisValidator';
+export * from './emailValidator';
+export * from './telefoneValidator';
+export * from './cepValidator';
+export * from './ctpsValidator';
+export * from './rgValidator';
+
+// Date Utils
+export * from './dateUtils';
+
+// String Utils
+export * from './stringUtils';
+
+// Number Utils
+export * from './numberUtils';
