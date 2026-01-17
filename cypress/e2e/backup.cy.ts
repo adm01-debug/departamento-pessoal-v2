2e5c1b6fdba785a469a28101ef14bd290acaa200
adm01-debug <adm01@promobrindes.com.br>
2026-01-17 11:38:10 -0300

    V18-CLEAN: Remove test file

--- cypress/e2e/backup.cy.ts
+++
@@ -1,1 +0,0 @@
-describe('backupE2E',()=>{beforeEach(()=>cy.visit('/'));it('load',()=>cy.get('body').should('exist'));it('nav',()=>cy.get('nav').should('be.visible'));it('content',()=>cy.get('main').should('exist'))});
