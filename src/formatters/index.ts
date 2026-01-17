b0a1c21a36711586f3802956c37184917d2615d0
adm01-debug <adm01@promobrindes.com.br>
2026-01-17 10:24:57 -0300

    V18-FIX: Expandir formatters index com re-exports

--- src/formatters/index.ts
+++ src/formatters/index.ts
@@ -1,4 +1,12 @@
-// V15-318
+// V18: Formatters Index - Exporta todos os formatadores
 export * from './currency';
 export * from './date';
 export * from './phone';
+
+// Re-exportar dos validadores para conveniência
+export { formatCPF, cleanCPF } from '@/utils/cpfValidator';
+export { formatCNPJ, cleanCNPJ } from '@/utils/cnpjValidator';
+export { formatPIS, cleanPIS } from '@/utils/pisValidator';
+export { formatCEP, cleanCEP } from '@/utils/cepValidator';
+export { formatTelefone, cleanTelefone } from '@/utils/telefoneValidator';
+export { formatCTPS } from '@/utils/ctpsValidator';
