a94208fd42de72a029bb6fa6aa2b3a7cfbd6295e
adm01-debug <adm01@promobrindes.com.br>
2026-01-17 11:48:43 -0300

    V18-CLEAN: Remove test

--- src/components/configuracoes/__tests__/ConfiguracoesEmpresa.test.tsx
+++
@@ -1,3 +0,0 @@
-import { describe, it, expect } from 'vitest';
-import { ConfiguracoesEmpresa } from '../ConfiguracoesEmpresa';
-describe('ConfiguracoesEmpresa', () => { it('should be defined', () => { expect(ConfiguracoesEmpresa).toBeDefined(); }); });
