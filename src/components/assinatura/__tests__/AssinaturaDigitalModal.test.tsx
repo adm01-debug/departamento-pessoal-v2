1c0dea271bdaa2e4831ebbd25db03f7ef4fd6a1b
adm01-debug <adm01@promobrindes.com.br>
2026-01-17 11:44:15 -0300

    V18-CLEAN: Remove test

--- src/components/assinatura/__tests__/AssinaturaDigitalModal.test.tsx
+++
@@ -1,3 +0,0 @@
-import { describe, it, expect } from 'vitest';
-import { AssinaturaDigitalModal } from '../AssinaturaDigitalModal';
-describe('AssinaturaDigitalModal', () => { it('defined', () => { expect(AssinaturaDigitalModal).toBeDefined(); }); });
