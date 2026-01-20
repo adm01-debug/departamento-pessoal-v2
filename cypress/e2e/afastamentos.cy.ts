d308e8c322602b521f1eab61537797bb91f3816d
adm01-debug <adm01@promobrindes.com.br>
2026-01-20 16:09:53 -0300

    🔥 V18: Eliminar teste - afastamentos.cy.ts

--- cypress/e2e/afastamentos.cy.ts
+++
@@ -1,6 +0,0 @@
-// V18-E2E: Testes Afastamentos
-describe("Afastamentos", () => {
-  beforeEach(() => { cy.login(); cy.visit("/afastamentos"); });
-  it("deve carregar pagina", () => { cy.url().should("include", "/afastamentos"); });
-  it("deve exibir conteudo", () => { cy.get("body").should("be.visible"); });
-});
