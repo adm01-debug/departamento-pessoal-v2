e482b12e3da0b267dc32fdd62f0446b4d89f9724
adm01-debug <adm01@promobrindes.com.br>
2026-01-20 16:10:01 -0300

    🔥 V18: Eliminar teste - banco-horas.cy.ts

--- cypress/e2e/banco-horas.cy.ts
+++
@@ -1,6 +0,0 @@
-// V18-E2E: Testes BancoHoras
-describe("BancoHoras", () => {
-  beforeEach(() => { cy.login(); cy.visit("/banco-horas"); });
-  it("deve carregar pagina", () => { cy.url().should("include", "/banco-horas"); });
-  it("deve exibir conteudo", () => { cy.get("body").should("be.visible"); });
-});
