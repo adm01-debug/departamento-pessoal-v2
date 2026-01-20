7af56f115ae995c04e5dbc9af9764d81687abf73
adm01-debug <adm01@promobrindes.com.br>
2026-01-20 13:29:14 -0300

    V18-E2E: Demissao

--- cypress/e2e/demissao.cy.ts
+++ cypress/e2e/demissao.cy.ts
@@ -1,12 +1,6 @@
-// V18-E2E: Testes E2E demissao
-describe("demissao", () => {
+// V18-E2E: Testes Demissao
+describe("Demissao", () => {
   beforeEach(() => { cy.login(); cy.visit("/demissao"); });
-
-  it("deve carregar pagina", () => {
-    cy.url().should("include", "/demissao");
-  });
-
-  it("deve exibir componentes", () => {
-    cy.get("button").should("have.length.at.least", 1);
-  });
+  it("deve carregar pagina", () => { cy.url().should("include", "/demissao"); });
+  it("deve exibir conteudo", () => { cy.get("body").should("be.visible"); });
 });
