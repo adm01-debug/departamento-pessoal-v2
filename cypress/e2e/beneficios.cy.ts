4c52f0647dda82359139b2f6c2e5fed0f8977172
adm01-debug <adm01@promobrindes.com.br>
2026-01-20 16:10:04 -0300

    🔥 V18: Eliminar teste - beneficios.cy.ts

--- cypress/e2e/beneficios.cy.ts
+++
@@ -1,6 +0,0 @@
-// V18-E2E: Testes Beneficios
-describe("Beneficios", () => {
-  beforeEach(() => { cy.login(); cy.visit("/beneficios"); });
-  it("deve carregar pagina", () => { cy.url().should("include", "/beneficios"); });
-  it("deve exibir conteudo", () => { cy.get("body").should("be.visible"); });
-});
