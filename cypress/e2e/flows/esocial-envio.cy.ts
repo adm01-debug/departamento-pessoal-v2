ad3f594ba98971af948fb7228e889439ae7bcdc7
adm01-debug <adm01@promobrindes.com.br>
2026-01-20 13:44:42 -0300

    V19: E2E eSocial Envio

---
+++ cypress/e2e/flows/esocial-envio.cy.ts
@@ -0,0 +1,19 @@
+// V19: Teste E2E - eSocial Envio
+describe('eSocial Envio', () => {
+  beforeEach(() => {
+    cy.visit('/');
+    cy.login();
+  });
+
+  it('deve completar fluxo principal', () => {
+    cy.get('[data-testid="main-content"]').should('be.visible');
+  });
+
+  it('deve validar dados obrigatorios', () => {
+    cy.get('form').should('exist');
+  });
+
+  it('deve exibir feedback ao usuario', () => {
+    cy.get('[role="alert"]').should('not.exist');
+  });
+});
