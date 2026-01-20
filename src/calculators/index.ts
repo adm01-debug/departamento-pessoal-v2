e1ce5af244a6956d70cd3ef7c98f098ddaec00b3
adm01-debug <adm01@promobrindes.com.br>
2026-01-20 14:09:39 -0300

    V20-027: Index Calculadoras

--- src/calculators/index.ts
+++ src/calculators/index.ts
@@ -1,54 +1,21 @@
-// V18: Index de Calculadoras - Completo e Atualizado 2026
-// Todas as calculadoras usam TABELA_2026
+// V20-027: Index de Exports Calculadoras
+export * from './inss';
+export * from './irrf';
+export * from './fgts';
+export * from './ferias';
+export * from './rescisao';
+export * from './horasExtras';
+export * from './decimo13';
+export * from './adicionalNoturno';
+export * from './dsr';
+export * from './multaFGTS';
+export * from './jornada';
+export * from './contribuicaoSindical';
+export * from './seguroDesemprego';
+export * from './custosPessoal';
+export * from './mediaVariaveis';
+export * from './reflexos';
 
-// Core - Impostos e Contribuições
-export * from "./inss";
-export * from "./irrf";
-export * from "./fgts";
-
-// Férias e 13º
-export * from "./ferias";
-export * from "./decimo13";
-
-// Rescisão
-export * from "./rescisao";
-export * from "./avisoPrevio";
-export * from "./multaFGTS";
-
-// Adicionais
-export * from "./horasExtras";
-export * from "./adicionalNoturno";
-export * from "./adicionalPericulosidade";
-export * from "./adicionalInsalubridade";
-
-// Descontos e Benefícios
-export * from "./pensaoAlimenticia";
-export * from "./valeTransporte";
-export * from "./salarioFamilia";
-
-// Banco de Horas e DSR
-export * from "./bancoHoras";
-export * from "./dsr";
-
-// Médias e Provisões
-export * from "./medias";
-export * from "./provisoes";
-
-// Gratificações e Comissões
-export * from "./gratificacao";
-export * from "./comissao";
-export * from "./plr";
-
-// Afastamentos
-export * from "./salarioMaternidade";
-export * from "./auxilioDoenca";
-
-// Re-export constantes 2026 para conveniência
-export { 
-  SALARIO_MINIMO_2026,
-  TETO_INSS_2026,
-  TABELA_INSS_2026,
-  TABELA_IRRF_2026,
-  LIMITE_SALARIO_FAMILIA_2026,
-  VALOR_COTA_SALARIO_FAMILIA_2026
-} from "@/constants/tabelas.constants";
+// Re-export de constantes
+export { TRABALHISTAS_2026 } from '@/constants/trabalhistas2026';
+export { FERIADOS_2026 } from '@/constants/feriados2026';
