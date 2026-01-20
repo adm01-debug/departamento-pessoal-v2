7bfd4eeb3b57cb949063000764e237b86462c7ee
adm01-debug <adm01@promobrindes.com.br>
2026-01-20 13:29:20 -0300

    V18-E2E: Beneficios

--- cypress/e2e/beneficios.cy.ts
+++ cypress/e2e/beneficios.cy.ts
@@ -1,6 +1,6 @@
-// V18-E2E: Testes E2E beneficios
-describe("beneficios", () => {
+// V18-E2E: Testes Beneficios
+describe("Beneficios", () => {
   beforeEach(() => { cy.login(); cy.visit("/beneficios"); });
-  it("deve carregar", () => { cy.url().should("include", "/beneficios"); });
-  it("deve ter acoes", () => { cy.get("button").should("exist"); });
+  it("deve carregar pagina", () => { cy.url().should("include", "/beneficios"); });
+  it("deve exibir conteudo", () => { cy.get("body").should("be.visible"); });
 });
