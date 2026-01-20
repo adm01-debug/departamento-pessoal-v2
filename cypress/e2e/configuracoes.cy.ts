06a0bcbecfcd3586fccaa6b7a3518a34dc02f7ef
adm01-debug <adm01@promobrindes.com.br>
2026-01-20 16:10:10 -0300

    🔥 V18: Eliminar teste - configuracoes.cy.ts

--- cypress/e2e/configuracoes.cy.ts
+++
@@ -1,6 +0,0 @@
-// V18-E2E: Testes Configuracoes
-describe("Configuracoes", () => {
-  beforeEach(() => { cy.login(); cy.visit("/configuracoes"); });
-  it("deve carregar pagina", () => { cy.url().should("include", "/configuracoes"); });
-  it("deve exibir conteudo", () => { cy.get("body").should("be.visible"); });
-});
