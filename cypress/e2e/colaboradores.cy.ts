ba6c9bd676e2c68d95a16cc4140702e5ec26c546
adm01-debug <adm01@promobrindes.com.br>
2026-01-20 16:10:07 -0300

    🔥 V18: Eliminar teste - colaboradores.cy.ts

--- cypress/e2e/colaboradores.cy.ts
+++
@@ -1,7 +0,0 @@
-// V18-E2E-002: Testes E2E Colaboradores
-describe("Colaboradores", () => {
-  beforeEach(() => { cy.login(); cy.visit("/colaboradores"); });
-  it("deve carregar lista", () => { cy.get("table").should("be.visible"); });
-  it("deve abrir modal novo", () => { cy.contains("Novo").click(); cy.get("[role='dialog']").should("be.visible"); });
-  it("deve filtrar", () => { cy.get("input[placeholder*='Buscar']").type("Maria"); });
-});
